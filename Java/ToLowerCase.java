534e60969e49123640c9df8599640e7045c2f4d3
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-10 09:39:15 +0900

    709. To Lower Case

---
+++ Java/ToLowerCase.java
@@ -0,0 +1,18 @@
+public class ToLowerCase {
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+        System.out.println(toLowerCase("hello"));
+    }
+
+    private static String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if ('A' <= chars[i] && chars[i] <= 'Z') {
+                chars[i] += ' ';
+            }
+        }
+
+        return new String(chars);
+    }
+}
