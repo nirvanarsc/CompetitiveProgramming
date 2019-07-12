6e52fe15d54158930a755355b40b9e2291789473
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-12 12:10:44 +0900

    938. Range Sum of BST
    Replace tail recursion with iteration

--- Java/RangeBSTSum.java
+++ Java/RangeBSTSum.java
@@ -10,14 +10,16 @@ public class RangeBSTSum {
     }
 
     private int rangeSumBST(TreeNode root, int L, int R) {
-        if (root == null) {
-            return 0;
+        while (true) {
+            if (root == null) {
+                return 0;
+            }
+            if (L <= root.val && root.val <= R) {
+                return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+            }
+            if (root.val < L) {
+                root = root.right;
+            } else {root = root.left;}
         }
-        if (L <= root.val && root.val <= R) {
-            return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
-        }
-        if (root.val < L) {
-            return rangeSumBST(root.right, L, R);
-        } else return rangeSumBST(root.left, L, R);
     }
 }
