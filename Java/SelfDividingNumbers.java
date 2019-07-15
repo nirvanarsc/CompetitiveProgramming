b02ebe4fb20ecb7fca00378ffa732d4b1cec0eec
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-15 19:20:20 +0900

    728. Self Dividing Numbers

---
+++ Java/SelfDividingNumbers.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(selfDividingNumbers(1, 1000));
+        System.out.println(selfDividingNumbers(10, 100));
+    }
+
+    private static boolean checkSelfDividing(int num) {
+        for (int i = num; i > 0; i /= 10) {
+            int curr = i % 10;
+            if (curr == 0 || num % curr != 0) return false;
+        }
+        return true;
+    }
+
+    private static List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (checkSelfDividing(i)) res.add(i);
+        }
+        return res;
+    }
+}
