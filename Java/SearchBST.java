f5bee61e98fcc0f791fb736e83639bb98623c432
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-23 23:12:26 +0900

    700. Search in a Binary Search Tree
    combine ifs

--- Java/SearchBST.java
+++ Java/SearchBST.java
@@ -1,6 +1,7 @@
 import org.jetbrains.annotations.Nullable;
 
 public class SearchBST {
+
     private static class TreeNode {
         int val;
         TreeNode left;
@@ -10,10 +11,7 @@ public class SearchBST {
     }
 
     public @Nullable TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == val) {
+        if (root == null || root.val == val) {
             return root;
         }
         return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val);
