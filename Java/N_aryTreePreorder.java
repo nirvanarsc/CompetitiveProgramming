f043f487ac3edd7670594dc740946fdc90729b65
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-25 16:24:14 +0900

    589. N-ary Tree Preorder Traversal

--- Java/N_aryTreePreorder.java
+++ Java/N_aryTreePreorder.java
@@ -1,7 +1,11 @@
+import static java.util.Collections.singletonList;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class N_aryTreePreorder {
+    public List<Integer> list = new ArrayList<>();
 
     private static final class Node {
         public int val;
@@ -15,11 +19,37 @@ public class N_aryTreePreorder {
         }
     }
 
-    public List<Integer> preorder(Node root) {
-        final List<Integer> result = new ArrayList<>();
-        while (!root.children.isEmpty()) {
-
+    // Iterative
+    public List<Integer> preorder1(Node root) {
+        final List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
         }
-        return result;
+
+        final LinkedList<Node> stack = new LinkedList<>(singletonList(root));
+
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            list.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--) {
+                stack.push(root.children.get(i));
+            }
+        }
+
+        return list;
+    }
+
+    // Recursive
+    public List<Integer> preorder2(Node root) {
+        if (root == null) {
+            return list;
+        }
+
+        list.add(root.val);
+        for (Node node : root.children) {
+            preorder2(node);
+        }
+
+        return list;
     }
 }
