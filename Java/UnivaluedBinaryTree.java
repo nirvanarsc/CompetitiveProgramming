b94a20a9dd75bfe7b09d2cb83bd7dcec5e2d18b6
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-29 18:38:24 +0900

    965. Univalued Binary Tree

---
+++ Java/UnivaluedBinaryTree.java
@@ -0,0 +1,21 @@
+public class UnivaluedBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        return isNodeUnival(root, root.val);
+    }
+
+    public boolean isNodeUnival(TreeNode node, int val) {
+        if (node == null) {
+            return true;
+        }
+        return node.val == val && isNodeUnival(node.left, val) && isNodeUnival(node.right, val);
+    }
+}
