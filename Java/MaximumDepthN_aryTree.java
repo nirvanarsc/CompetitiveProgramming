e998981b64e5af69fb8f5607e69c41e709c37639
nirvanarsc <nirvana_rsc@yahoo.com>
2019-07-31 00:56:59 +0900

    559. Maximum Depth of N-ary Tree

---
+++ Java/MaximumDepthN_aryTree.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class MaximumDepthN_aryTree {
+
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        Node() {}
+
+        Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    // Recursive DFS
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxDepth = 0;
+        for (Node child : root.children) {
+            maxDepth = Math.max(maxDepth, maxDepth(child));
+        }
+        return maxDepth + 1;
+    }
+}
