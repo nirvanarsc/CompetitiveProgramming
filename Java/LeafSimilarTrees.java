18cea85474efc91a878a0059660b3dd10ae4cfb6
nirvanarsc <nirvana_rsc@yahoo.com>
2019-08-08 00:13:55 +0900

    872. Leaf-Similar Trees
    Early return

--- Java/LeafSimilarTrees.java
+++ Java/LeafSimilarTrees.java
@@ -16,6 +16,26 @@ public class LeafSimilarTrees {
         return dfs(root1).equals(dfs(root2));
     }
 
+    public boolean leafSimilar2(TreeNode root1, TreeNode root2) {
+        final Stack<TreeNode> s1 = new Stack<>();
+        final Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root1);
+        s2.push(root2);
+        while (!s1.empty() && !s2.empty()) {
+            if (dfs(s1) != dfs(s2)) { return false; }
+        }
+        return s1.empty() && s2.empty();
+    }
+
+    public int dfs(Stack<TreeNode> s) {
+        while (true) {
+            final TreeNode node = s.pop();
+            if (node.right != null) { s.push(node.right); }
+            if (node.left != null) { s.push(node.left); }
+            if (node.left == null && node.right == null) { return node.val; }
+        }
+    }
+
     List<Integer> dfs(TreeNode root) {
         final Stack<TreeNode> stack = new Stack<>();
         final List<Integer> res = new ArrayList<>();
