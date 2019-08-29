12b5547372f84d434dab42d6ca352f94b75f7d81
nirvanarsc <nirvana_rsc@yahoo.com>
2019-08-29 23:57:37 +0900

    888. Fair Candy Swap
    Improved

--- Java/FairCandySwap.java
+++ Java/FairCandySwap.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class FairCandySwap {
 
@@ -6,6 +8,7 @@ public final class FairCandySwap {
         final int[] a = { 21, 89, 90, 88, 54, 45, 67, 41, 57, 7 };
         final int[] b = { 55, 57, 22, 19, 44, 91, 100, 71, 33, 33 };
         System.out.println(Arrays.toString(fairCandySwap(a, b)));
+        System.out.println(Arrays.toString(fairCandySwap2(a, b)));
     }
 
     public static int[] fairCandySwap(int[] a, int[] b) {
@@ -25,6 +28,23 @@ public final class FairCandySwap {
         }
 
         return res;
+    }
+
+    public static int[] fairCandySwap2(int[] a, int[] b) {
+        int diff = 0;
+        final Set<Integer> set = new HashSet<>();
+        for (int i : a) {
+            diff += i;
+            set.add(i);
+        }
+        for (int i : b) {
+            diff -= i;
+        }
+        diff /= 2;
+        for (int i : b) {
+            if (set.remove(i + diff)) { return new int[] { i + diff, i }; }
+        }
+        return new int[0];
     }
 
     private FairCandySwap() {}
