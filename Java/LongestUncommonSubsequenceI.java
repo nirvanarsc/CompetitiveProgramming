f87059a0f10bdc4c2b0b8a5b581ee36d6852a700
nirvanarsc <nirvana_rsc@yahoo.com>
2019-08-30 01:27:20 +0900

    521. Longest Uncommon Subsequence I

---
+++ Java/LongestUncommonSubsequenceI.java
@@ -0,0 +1,10 @@
+public class LongestUncommonSubsequenceI {
+
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+
+        return Math.max(a.length(), b.length());
+    }
+}
