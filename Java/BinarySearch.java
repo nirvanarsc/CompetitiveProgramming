0143f363dc80dc21ba4d1a9a3c30ea9f36c97d80
nirvanarsc <nirvana_rsc@yahoo.com>
2019-10-05 21:21:49 +0900

    704. Binary Search

---
+++ Java/BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            final int mid = (low + high) >>> 1;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
