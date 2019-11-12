4811df825f73c629b2dd8c39a10308487ceac141
nirvanarsc <nirvana_rsc@yahoo.com>
2019-11-12 12:17:57 +0100

    410. Split Array Largest Sum
    nit

--- Java/SplitArrayLargestSum.java
+++ Java/SplitArrayLargestSum.java
@@ -26,11 +26,10 @@ public final class SplitArrayLargestSum {
         int pieces = 1;
         int largest = 0;
         for (int i : arr) {
-            if (i + largest > mid) {
+            largest += i;
+            if (largest > mid) {
                 largest = i;
                 pieces++;
-            } else {
-                largest += i;
             }
         }
         return pieces;
@@ -38,6 +37,8 @@ public final class SplitArrayLargestSum {
 
     public static void main(String[] args) {
         System.out.println(splitArray(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(splitArray(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(splitArray(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
     }
 
     private SplitArrayLargestSum() {}
