ffbe02e3280756d962aae2e3ab3d34c83f4c4830
nirvanarsc <nirvana_rsc@yahoo.com>
2019-11-25 00:42:25 +0900

    1265. Print Immutable Linked List in Reverse

---
+++ Java/ReversePrintImmutableLinkedList.java
@@ -0,0 +1,48 @@
+import org.jetbrains.annotations.Nullable;
+
+public final class ReversePrintImmutableLinkedList {
+
+    public static class ListNode<T> {
+        public T data;
+        public @Nullable ListNode<T> next;
+
+        public ListNode(T data, @Nullable ListNode<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    // Can trade-off space for more loops (but still linear)
+    // https://stackoverflow.com/questions/41542257/reverse-print-an-immutable-linked-list-with-less-than-on-space/41575182#41575182
+    private static void reversePrint(ListNode<Integer> start, ListNode<Integer> end) {
+        if (start == end) {
+            return;
+        }
+
+        ListNode<Integer> slow;
+        ListNode<Integer> fast;
+        slow = fast = start;
+
+        while (fast.next != end && fast.next.next != end) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        reversePrint(slow.next, end);
+        System.out.println(slow.data);
+        reversePrint(start, slow);
+    }
+
+    public static void main(String[] args) {
+        ListNode<Integer> t = new ListNode<>(1, null);
+        final ListNode<Integer> head = t;
+        for (int i = 2; i <= 10; i++) {
+            t.next = new ListNode<>(i, null);
+            t = t.next;
+        }
+
+        reversePrint(head, null);
+    }
+
+    private ReversePrintImmutableLinkedList() {}
+}
