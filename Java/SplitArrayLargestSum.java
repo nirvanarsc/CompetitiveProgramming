7624b398beed18a9523f4fac5ee5117627683034
nirvanarsc <nirvana_rsc@yahoo.com>
2019-11-30 19:49:45 +0900

    410. Split Array Largest Sum
    Bottom up DP for both minMax and maxMin versions

--- Java/SplitArrayLargestSum.java
+++ Java/SplitArrayLargestSum.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public final class SplitArrayLargestSum {
 
-    public static int splitArray(int[] nums, int m) {
+    public static int minimizeMaximum(int[] nums, int m) {
         int max = 0;
         int sum = 0;
         for (int i : nums) {
@@ -22,7 +24,7 @@ public final class SplitArrayLargestSum {
         return low;
     }
 
-    public static int minSweetness(int[] chocolate, int m) {
+    public static int maximizeMinimum(int[] chocolate, int m) {
         int min = Integer.MAX_VALUE;
         int sum = 0;
         for (int i : chocolate) {
@@ -68,63 +70,96 @@ public final class SplitArrayLargestSum {
         return peopleCount;
     }
 
-    // TODO Write bottom-up DP
-    public static int splitArrayDP(int[] nums, int m) {
+    public static int maxMinR(int[] chocolate, int m) {
+        return findMaxMinOrMinMaxPartitionR(chocolate, m, true);
+    }
+
+    public static int maxMin(int[] chocolate, int m) {
+        return findMaxMinOrMinMaxPartition(chocolate, m, true);
+    }
+
+    public static int minMaxR(int[] chocolate, int m) {
+        return findMaxMinOrMinMaxPartitionR(chocolate, m, false);
+    }
+
+    public static int minMax(int[] chocolate, int m) {
+        return findMaxMinOrMinMaxPartition(chocolate, m, false);
+    }
+
+    // Top down DP
+    public static int findMaxMinOrMinMaxPartitionR(int[] nums, int m, boolean maxMin) {
         final int[][] dp = new int[nums.length][m + 1];
         final int[] sum = new int[nums.length];
         for (int i = nums.length - 1; i >= 0; i--) {
             sum[i] = (i == nums.length - 1 ? 0 : sum[i + 1]) + nums[i];
         }
-        return calc(nums, 0, m, sum, dp);
+        return recurse(nums, 0, m, sum, dp, maxMin);
     }
 
-    private static int calc(int[] nums, int start, int m, int[] sums, int[][] dp) {
+    private static int recurse(int[] nums, int start, int m, int[] sums, int[][] dp, boolean maxMin) {
         if (m == 1) {
             return sums[start];
         }
         if (dp[start][m] > 0) {
             return dp[start][m];
         }
-        int min = Integer.MAX_VALUE, sum = 0;
+        int min = maxMin ? 0 : Integer.MAX_VALUE, sum = 0;
         for (int i = start; i <= nums.length - m; i++) {
             sum += nums[i];
-            min = Math.min(Math.max(sum, calc(nums, i + 1, m - 1, sums, dp)), min);
+            min = maxMin ? Math.max(Math.min(sum, recurse(nums, i + 1, m - 1, sums, dp, true)), min)
+                         : Math.min(Math.max(sum, recurse(nums, i + 1, m - 1, sums, dp, false)), min);
         }
         dp[start][m] = min;
         return dp[start][m];
     }
 
-    public static int findMaxMinPartition(int[] nums, int m) {
-        final int[][] dp = new int[nums.length + 1][m + 1];
-        dp[0][0] = Integer.MAX_VALUE;
-        for (int n = 1; n <= nums.length; n++) {
-            for (int k = 1; k <= m; k++) {
-                int curr = 0;
-                for (int j = n - 1; j >= k - 1; j--) {
-                    curr += nums[j];
-                    dp[n][k] = Math.max(dp[n][k], Math.min(dp[j][k - 1], curr));
+    // Bottom up DP
+    public static int findMaxMinOrMinMaxPartition(int[] nums, int m, boolean maxMin) {
+        final int[] sums = new int[nums.length + 1];
+        final int[][] dp = new int[m + 1][nums.length + 1];
+        for (int i = 1; i <= nums.length; ++i) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int[] row : dp) {
+            Arrays.fill(row, maxMin ? 0 : Integer.MAX_VALUE);
+        }
+        dp[0][0] = maxMin ? Integer.MAX_VALUE : 0;
+        for (int row = 1; row <= m; row++) {
+            for (int col = 1; col <= nums.length; col++) {
+                for (int k = row - 1; k < col; k++) {
+                    dp[row][col] = maxMin
+                                   ? Math.max(dp[row][col], Math.min(dp[row - 1][k], sums[col] - sums[k]))
+                                   : Math.min(dp[row][col], Math.max(dp[row - 1][k], sums[col] - sums[k]));
                 }
             }
         }
-        return dp[nums.length][m];
+        return dp[m][nums.length];
     }
 
     public static void main(String[] args) {
-        System.out.println(splitArray(new int[] { 7, 2, 5, 10, 8 }, 2));
-        System.out.println(splitArray(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
-        System.out.println(splitArray(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
+        System.out.println(minimizeMaximum(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(minimizeMaximum(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(minimizeMaximum(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
         System.out.println();
-        System.out.println(splitArrayDP(new int[] { 7, 2, 5, 10, 8 }, 2));
-        System.out.println(splitArrayDP(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
-        System.out.println(splitArrayDP(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
+        System.out.println(maximizeMinimum(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(maximizeMinimum(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(maximizeMinimum(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
         System.out.println();
-        System.out.println(minSweetness(new int[] { 7, 2, 5, 10, 8 }, 2));
-        System.out.println(minSweetness(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
-        System.out.println(minSweetness(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
+        System.out.println(minMaxR(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(minMaxR(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(minMaxR(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
         System.out.println();
-        System.out.println(findMaxMinPartition(new int[] { 7, 2, 5, 10, 8 }, 2));
-        System.out.println(findMaxMinPartition(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
-        System.out.println(findMaxMinPartition(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
+        System.out.println(maxMinR(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(maxMinR(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(maxMinR(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
+        System.out.println();
+        System.out.println(minMax(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(minMax(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(minMax(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
+        System.out.println();
+        System.out.println(maxMin(new int[] { 7, 2, 5, 10, 8 }, 2));
+        System.out.println(maxMin(new int[] { 6, 3, 2, 8, 7, 5 }, 3));
+        System.out.println(maxMin(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 6));
     }
 
     private SplitArrayLargestSum() {}
