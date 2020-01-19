17906b5496f2c50f2833a4dc2237e6e8baf7f4ca
nirvanarsc <nirvana_rsc@yahoo.com>
2020-01-19 14:59:22 +0900

    1323. Maximum 69 Number 1324. Print Words Vertically 1325. Delete Leaves With a Given Value 1326. Minimum Number of Taps to Open to Water a Garden

---
+++ Java/weekly_172/P_1326.java
@@ -0,0 +1,23 @@
+package weekly_172;
+
+import java.util.Arrays;
+
+public class P_1326 {
+
+    public int minTaps(int n, int[] ranges) {
+        final int[] dp = new int[n + 1];
+        final int MAX_VALUE = (int) 1e9;
+        Arrays.fill(dp, MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i <= n; ++i) {
+            final int left = Math.max(0, i - ranges[i]);
+            final int right = Math.min(n, i + ranges[i]);
+            int t = MAX_VALUE;
+            for (int x = left; x <= right; ++x) {
+                t = Math.min(t, dp[x]);
+            }
+            dp[right] = Math.min(dp[right], t + 1);
+        }
+        return dp[n] < MAX_VALUE ? dp[n] : -1;
+    }
+}
