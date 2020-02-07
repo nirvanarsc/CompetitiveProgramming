4e2d7ee7f1c5ca701c0bea2bfff005fc104a0e3e
nirvanarsc <nirvana_rsc@yahoo.com>
2020-02-07 22:07:58 +0900

    887. Super Egg Drop

--- Java/hard/P_887.java
+++ Java/hard/P_887.java
@@ -24,4 +24,33 @@ public class P_887 {
         }
         return i;
     }
+
+    public int superEggDropTopDown(int k, int n) {
+        return recurse(k, n, new Integer[k + 1][n + 1]);
+    }
+
+    private static int recurse(int k, int n, Integer[][] dp) {
+        if (n <= 1) {
+            return n;
+        }
+        if (k == 1) {
+            return n;
+        }
+        if (dp[k][n] != null) {
+            return dp[k][n];
+        }
+        int low = 1, high = n, result = n;
+        while (low < high) {
+            final int mid = low + high >>> 1;
+            final int left = recurse(k - 1, mid - 1, dp);
+            final int right = recurse(k, n - mid, dp);
+            if (left <= right) {
+                result = Math.min(result, Math.max(left, right) + 1);
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return dp[k][n] = result;
+    }
 }
