0d546ef9957c1279beb8bad2f6d1f5f511df064f
nirvanarsc <nirvana_rsc@yahoo.com>
2020-02-17 23:07:27 +0900

    1160. Find Words That Can Be Formed by Characters 1161. Maximum Level Sum of a Binary Tree 1162. As Far from Land as Possible 1163. Last Substring in Lexicographical Order 1192. Critical Connections in a Network

---
+++ Java/weekly_150/P_1161.java
@@ -0,0 +1,36 @@
+package weekly_150;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import utils.DataStructures.TreeNode;
+
+public class P_1161 {
+
+    public int maxLevelSum(TreeNode root) {
+        int maxSum = Integer.MIN_VALUE;
+        int maxLevel = 0;
+        int level = 0;
+        final Deque<TreeNode> q = new ArrayDeque<>();
+        q.offerLast(root);
+        while (!q.isEmpty()) {
+            level++;
+            int levelSum = 0;
+            for (int i = q.size(); i > 0; i--) {
+                final TreeNode curr = q.removeFirst();
+                levelSum += curr.val;
+                if (curr.left != null) {
+                    q.offerLast(curr.left);
+                }
+                if (curr.right != null) {
+                    q.offerLast(curr.right);
+                }
+            }
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                maxLevel = level;
+            }
+        }
+        return maxLevel;
+    }
+}
---
+++ Java/weekly_150/P_1163.java
@@ -0,0 +1,25 @@
+package weekly_150;
+
+public class P_1163 {
+
+    // https://cp-algorithms.com/string/lyndon_factorization.html
+    // https://en.wikipedia.org/wiki/Lexicographically_minimal_string_rotation
+    public String lastSubstring(String s) {
+        int i = 0;
+        int j = 1;
+        int offset = 0;
+        final int len = s.length();
+        while (i + offset < len && j + offset < len) {
+            final char c = s.charAt(i + offset);
+            final char d = s.charAt(j + offset);
+            if (c == d) {
+                ++offset;
+            } else {
+                if (c < d) { i += offset + 1; } else { j += offset + 1; }
+                if (i == j) { ++i; }
+                offset = 0;
+            }
+        }
+        return s.substring(Math.min(i, j));
+    }
+}
