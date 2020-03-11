c6e8675531090722da0a7954e7c505a5fb9be782
nirvanarsc <nirvana_rsc@yahoo.com>
2020-03-11 20:04:58 +0900

    928. Minimize Malware Spread II 927. Three Equal Parts 926. Flip String to Monotone Increasing 925. Long Pressed Name 924. Minimize Malware Spread 923. 3Sum With Multiplicity 922. Sort Array By Parity II 921. Minimum Add to Make Parentheses Valid

---
+++ Java/weekly_contests/weekly_106/P_922.java
@@ -0,0 +1,39 @@
+package weekly_contests.weekly_106;
+
+@SuppressWarnings("MethodParameterNamingConvention")
+public class P_922 {
+
+    public int[] sortArrayByParityII(int[] A) {
+        final int[] map = new int[1001];
+        for (int num : A) { map[num]++; }
+        int even = 0;
+        int odd = 1;
+        for (int i = 0; i < A.length; i++) {
+            if (i % 2 == 0) {
+                while (even % 2 != 0 || map[even] == 0) { even++; }
+                A[i] = even;
+                map[even]--;
+            } else {
+                while (odd % 2 == 0 || map[odd] == 0) { odd++; }
+                A[i] = odd;
+                map[odd]--;
+            }
+        }
+        return A;
+    }
+
+    public int[] sortArrayByParity(int[] A) {
+        int odd = 1;
+        for (int i = 0; i < A.length; i += 2) {
+            if (A[i] % 2 != 0) {
+                while (A[odd] % 2 != 0) {
+                    odd += 2;
+                }
+                final int tmp = A[i];
+                A[i] = A[odd];
+                A[odd] = tmp;
+            }
+        }
+        return A;
+    }
+}
