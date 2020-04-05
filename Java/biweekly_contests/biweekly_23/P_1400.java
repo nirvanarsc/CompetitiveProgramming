8bd8c50911618eed87ab89b3fdc2e9beb3ecd0f7
nirvanarsc <nirvana_rsc@yahoo.com>
2020-04-05 18:37:36 +0900

    1406. Stone Game III 1405. Longest Happy String 1404. Number of Steps to Reduce a Number in Binary Representation to One 1403. Minimum Subsequence in Non-Increasing Order 1402. Reducing Dishes 1401. Circle and Rectangle Overlapping 1400. Construct K Palindrome Strings 1399. Count Largest Group

---
+++ Java/biweekly_contests/biweekly_23/P_1400.java
@@ -0,0 +1,18 @@
+package biweekly_contests.biweekly_23;
+
+public class P_1400 {
+
+    public boolean canConstruct(String s, int k) {
+        final int [] map = new int[26];
+        for(char c: s.toCharArray()) {
+            map[c-'a']++;
+        }
+        int odd = 0;
+        for(int i=0; i<26; i++) {
+            if(map[i] % 2 != 0) {
+                odd++;
+            }
+        }
+        return s.length() >= k && odd <= k;
+    }
+}
---
+++ Java/biweekly_contests/biweekly_23/P_1401.java
@@ -0,0 +1,10 @@
+package biweekly_contests.biweekly_23;
+
+public class P_1401 {
+
+    public boolean checkOverlap(int radius, int x_center, int y_center, int x1, int y1, int x2, int y2) {
+        final int deltaX = x_center - Math.max(x1, Math.min(x_center, x2));
+        final int deltaY = y_center - Math.max(y1, Math.min(y_center, y2));
+        return (deltaX * deltaX + deltaY * deltaY) <= (radius * radius);
+    }
+}
