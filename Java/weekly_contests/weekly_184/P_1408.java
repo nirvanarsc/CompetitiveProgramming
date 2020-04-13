3d1d39745632830fc8a42e6acdfc0e103c013c9c
nirvanarsc <nirvana_rsc@yahoo.com>
2020-04-13 21:43:29 +0900

    Weekly Contest 184

---
+++ Java/weekly_contests/weekly_184/P_1408.java
@@ -0,0 +1,23 @@
+package weekly_contests.weekly_184;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class P_1408 {
+
+    public List<String> stringMatching(String[] words) {
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        final List<String> ret = new ArrayList<>();
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[j].contains(words[i])) {
+                    ret.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+}
