cffb4be3c655ce872a38c600e9227574235f5d37
nirvanarsc <nirvana_rsc@yahoo.com>
2020-04-22 23:37:20 +0900

    Weekly Contest 47

--- Java/weekly_contests/weekly_48/P_672.java
+++ Java/weekly_contests/weekly_48/P_672.java
@@ -1,26 +1,29 @@
 package weekly_contests.weekly_48;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class P_672 {
 
-    @SuppressWarnings("PointlessBitwiseExpression")
     public int flipLights(int n, int m) {
-        final Set<Integer> seen = new HashSet<>();
-        n = Math.min(n, 6);
-        final int shift = Math.max(0, 6 - n);
-        for (int candidate = 0; candidate < 16; ++candidate) {
-            final int bits = Integer.bitCount(candidate);
-            if (bits % 2 == m % 2 && bits <= m) {
-                int lights = 0;
-                if (((candidate >> 0) & 1) > 0) { lights ^= 0b111111 >> shift; }
-                if (((candidate >> 1) & 1) > 0) { lights ^= 0b010101 >> shift; }
-                if (((candidate >> 2) & 1) > 0) { lights ^= 0b101010 >> shift; }
-                if (((candidate >> 3) & 1) > 0) { lights ^= 0b100100 >> shift; }
-                seen.add(lights);
+        n = n <= 6 ? n : n % 6 + 6;
+        final int flipAll = 0b11111111111 >> (11 - n);
+        final int flipOdd = 0b10101010101 >> (11 - n);
+        final int flipEven = 0b01010101010 >> (11 - n);
+        final int flip3rd = 0b10010010010 >> (11 - n);
+        Set<Integer> q = new HashSet<>(Collections.singleton((1 << n) - 1));
+        Set<Integer> q2 = new HashSet<>();
+        for (int round = 1; round <= m; round++) {
+            for (int state : q) {
+                q2.add(state ^ flipAll);
+                q2.add(state ^ flipOdd);
+                q2.add(state ^ flipEven);
+                q2.add(state ^ flip3rd);
             }
+            q = q2;
+            q2 = new HashSet<>();
         }
-        return seen.size();
+        return q.size();
     }
 }
