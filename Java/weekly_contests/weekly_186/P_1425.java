0dcf52eb166109e6356f13a241e2ef19e8e9a9b2
nirvanarsc <nirvana_rsc@yahoo.com>
2020-04-27 00:26:06 +0900

    Weekly Contest 186 Nits

--- Java/weekly_contests/weekly_186/P_1425.java
+++ Java/weekly_contests/weekly_186/P_1425.java
@@ -5,11 +5,10 @@ import utils.IntervalSegmentTree;
 public class P_1425 {
 
     public int constrainedSubsetSum(int[] nums, int k) {
-        final int n = nums.length;
         final IntervalSegmentTree st = new IntervalSegmentTree(0, (int) 1e9, 0, "MAX");
         int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= n; i++) {
-            final int next = Math.max(0, st.query(st, Math.max(0, i - k), i)) + nums[i - 1];
+        for (int i = 1; i <= nums.length; i++) {
+            final int next = st.query(st, Math.max(0, i - k), i) + nums[i - 1];
             max = Math.max(max, next);
             st.update(st, i, i, next);
         }
