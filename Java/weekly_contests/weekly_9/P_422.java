93189c333ede6735d32fde1587fcee63ee3817f6
nirvanarsc <nirvana_rsc@yahoo.com>
2020-05-16 20:34:05 +0900

    Weekly Contest 9

---
+++ Java/weekly_contests/weekly_9/P_422.java
@@ -0,0 +1,22 @@
+package weekly_contests.weekly_9;
+
+import java.util.List;
+
+public class P_422 {
+
+    public boolean validWordSquare(List<String> words) {
+        for (int i = 0; i < words.size(); i++) {
+            int j = i;
+            while (j < Math.max(words.size(), words.get(i).length())) {
+                final char left = (j >= words.size() || i >= words.get(j).length()) ? '*'
+                                                                                    : words.get(j).charAt(i);
+                final char right = j >= words.get(i).length() ? '*' : words.get(i).charAt(j);
+                if (left != right) {
+                    return false;
+                }
+                j++;
+            }
+        }
+        return true;
+    }
+}
