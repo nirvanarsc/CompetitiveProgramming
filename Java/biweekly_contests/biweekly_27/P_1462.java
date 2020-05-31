ff0344e95f4992db1d188bf0af8fd286a72dc851
nirvanarsc <nirvana_rsc@yahoo.com>
2020-05-31 15:34:54 +0900

    Biweekly 27 & Weekly 191

---
+++ Java/biweekly_contests/biweekly_27/P_1462.java
@@ -0,0 +1,28 @@
+package biweekly_contests.biweekly_27;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_1462 {
+
+    // Floyd-Warshall O(n^3)
+    // Similar to https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/discuss/491446/
+    public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
+        final boolean[][] g = new boolean[n][n];
+        for (int[] p : prerequisites) {
+            g[p[0]][p[1]] = true;
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    g[i][j] |= g[i][k] & g[k][j];
+                }
+            }
+        }
+        final List<Boolean> res = new ArrayList<>();
+        for (int[] q : queries) {
+            res.add(g[q[0]][q[1]]);
+        }
+        return res;
+    }
+}
---
+++ Java/biweekly_contests/biweekly_27/P_1463.java
@@ -0,0 +1,33 @@
+package biweekly_contests.biweekly_27;
+
+public class P_1463 {
+
+    private static final int[][] DIRS = {
+            { 0, -1 }, { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, 0 }, { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }
+    };
+
+    public int cherryPickup(int[][] grid) {
+        final int n = grid.length;
+        final int m = grid[0].length;
+        return dfs(grid, 0, 0, m - 1, new Integer[n][m][m]);
+    }
+
+    private static int dfs(int[][] grid, int level, int r1, int r2, Integer[][][] dp) {
+        if (level == grid.length) {
+            return 0;
+        }
+        if (dp[level][r1][r2] != null) {
+            return dp[level][r1][r2];
+        }
+        final int cherry = r1 == r2 ? grid[level][r1] : grid[level][r1] + grid[level][r2];
+        int max = 0;
+        for (int[] dir : DIRS) {
+            final int nextR1 = r1 + dir[0];
+            final int nextR2 = r2 + dir[1];
+            if (nextR1 >= 0 && nextR1 < grid[0].length && nextR2 >= 0 && nextR2 < grid[0].length) {
+                max = Math.max(max, dfs(grid, level + 1, nextR1, nextR2, dp));
+            }
+        }
+        return dp[level][r1][r2] = cherry + max;
+    }
+}
