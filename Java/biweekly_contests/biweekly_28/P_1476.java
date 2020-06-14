e7bec2cffd5748c76759929fe0924af41d84d5de
nirvanarsc <nirvana_rsc@yahoo.com>
2020-06-14 19:57:51 +0900

    Weekly Contest 193 & Biweekly contest 28

---
+++ Java/biweekly_contests/biweekly_28/P_1476.java
@@ -0,0 +1,25 @@
+package biweekly_contests.biweekly_28;
+
+public class P_1476 {
+
+    public static class SubrectangleQueries {
+
+        int[][] rect;
+
+        public SubrectangleQueries(int[][] rectangle) {
+            rect = rectangle;
+        }
+
+        public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+            for (int i = row1; i <= row2; i++) {
+                for (int j = col1; j <= col2; j++) {
+                    rect[i][j] = newValue;
+                }
+            }
+        }
+
+        public int getValue(int row, int col) {
+            return rect[row][col];
+        }
+    }
+}
---
+++ Java/biweekly_contests/biweekly_28/P_1478.java
@@ -0,0 +1,35 @@
+package biweekly_contests.biweekly_28;
+
+import java.util.Arrays;
+
+public class P_1478 {
+
+    private static final int MAX = (int) 1e9;
+
+    public int minDistance(int[] houses, int k) {
+        Arrays.sort(houses);
+        return solve(houses, k, 0, 0, new Integer[houses.length][k]);
+    }
+
+    public int solve(int[] houses, int k, int pos, int curK, Integer[][] dp) {
+        if (pos == houses.length) {
+            return curK == k ? 0 : MAX;
+        }
+        if (curK == k) {
+            return MAX;
+        }
+        if (dp[pos][curK] != null) {
+            return dp[pos][curK];
+        }
+        int res = MAX;
+        for (int i = pos; i < houses.length; i++) {
+            final int median = houses[(i + pos) / 2];
+            int cost = 0;
+            for (int j = pos; j <= i; j++) {
+                cost += Math.abs(median - houses[j]);
+            }
+            res = Math.min(res, solve(houses, k, i + 1, curK + 1, dp) + cost);
+        }
+        return dp[pos][curK] = res;
+    }
+}
