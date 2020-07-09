0ceae43e08fabe9c1de750e92f63081c3c70c028
nirvanarsc <nirvana_rsc@yahoo.com>
2020-07-09 22:19:22 +0900

    Running on empty - food review! part 40

--- Java/weekly_contests/weekly_46/P_662.java
+++ Java/weekly_contests/weekly_46/P_662.java
@@ -1,5 +1,8 @@
 package weekly_contests.weekly_46;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +10,37 @@ import utils.DataStructures.TreeNode;
 
 public class P_662 {
 
+    private static class Pair {
+        TreeNode node;
+        int d;
+
+        Pair(TreeNode node, int d) {
+            this.node = node;
+            this.d = d;
+        }
+    }
+
     public int widthOfBinaryTree(TreeNode root) {
+        int res = 1;
+        final Deque<Pair> q = new ArrayDeque<>(Collections.singleton(new Pair(root, 0)));
+        while (!q.isEmpty()) {
+            if (q.size() >= 2) {
+                res = Math.max(res, q.getLast().d - q.getFirst().d + 1);
+            }
+            for (int size = q.size(); size > 0; size--) {
+                final Pair curr = q.removeFirst();
+                if (curr.node.left != null) {
+                    q.offerLast(new Pair(curr.node.left, (curr.d << 1) + 1));
+                }
+                if (curr.node.right != null) {
+                    q.offerLast(new Pair(curr.node.right, (curr.d << 1) + 2));
+                }
+            }
+        }
+        return res;
+    }
+
+    public int widthOfBinaryTreeDFS(TreeNode root) {
         final int[] ans = { 0 };
         dfs(root, 0, 0, new HashMap<>(), ans);
         return ans[0];
