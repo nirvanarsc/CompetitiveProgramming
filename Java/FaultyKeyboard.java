c0e434b0816c5a958d7b6d4e882d2b1500b574f0
nirvanarsc <nirvana_rsc@yahoo.com>
2020-08-10 17:31:11 +0900

    Running on empty - food review! part 59

--- Java/FaultyKeyboard.java
+++ Java/FaultyKeyboard.java
@@ -1,78 +1,72 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // https://leetcode.com/discuss/interview-question/643158/google-phone-faulty-keyboard
-public class FaultyKeyboard {
+// https://leetcode.com/discuss/interview-experience/778039/google-phone-interview-rejected
+public final class FaultyKeyboard {
 
     static class Trie {
-        Trie[] children = new Trie[52];
-        boolean isWord;
+        Map<Character, Trie> children = new HashMap<>();
+        String word;
     }
 
-    List<String> answer = new ArrayList<>();
-    Trie root;
-    String input;
-    int longestWord;
-
-    private void insert(String str) {
-        Trie curr = root;
-        for (char c : str.toCharArray()) {
-            final int id = c <= 'Z' ? c - 'A' : c - 'a' + 26;
-            if (curr.children[id] == null) {
-                curr.children[id] = new Trie();
+    private static void dfs(String s, int i, Trie root, Trie curr,
+                            List<String> path,
+                            List<List<String>> result) {
+        if (i == s.length()) {
+            if (curr.word != null) {
+                path.add(curr.word);
+                result.add(new ArrayList<>(path));
+                path.remove(path.size() - 1);
             }
-            curr = curr.children[id];
-        }
-        curr.isWord = true;
-    }
-
-    private List<Integer> search(int start, int end) {
-        Trie curr = root;
-        final List<Integer> res = new ArrayList<>();
-        for (int i = start; i < end; i++) {
-            final int id;
-            if (input.charAt(i) == ' ') {
-                id = 'e' - 'a' + 26;
-            } else if (input.charAt(i) <= 'Z') {
-                id = input.charAt(i) - 'A';
-            } else {
-                id = input.charAt(i) - 'a' + 26;
-            }
-            if (curr.children[id] == null) {
-                return res;
-            }
-            curr = curr.children[id];
-            if (curr.isWord) {
-                res.add(i);
-            }
-        }
-        return res;
-    }
-
-    private void backTrack(int start, int end, String curr) {
-        if (start == end) {
-            answer.add(curr);
             return;
         }
-        backTrack(start + 1, end, curr + input.charAt(start));
-        for (int jump : search(start, Math.min(end, start + longestWord))) {
-            final String newStr = input.substring(start, jump + 1).replace(' ', 'e');
-            backTrack(jump + 1, end, curr + newStr);
+        final char c = s.charAt(i);
+        if (curr.children.containsKey(c)) {
+            dfs(s, i + 1, root, curr.children.get(c), path, result);
+        } else if (c == ' ') {
+            if (curr.word != null) {
+                path.add(curr.word);
+                dfs(s, i + 1, root, root, path, result);
+                path.remove(path.size() - 1);
+            }
+            if (curr.children.containsKey('e')) {
+                dfs(s, i + 1, root, curr.children.get('e'), path, result);
+            }
         }
+    }
+
+    private static Trie buildTrie(List<String> dict) {
+        final Trie root = new Trie();
+        for (String word : dict) {
+            insert(root, word);
+        }
+        return root;
+    }
+
+    private static void insert(Trie root, String word) {
+        Trie curr = root;
+        for (char c : word.toCharArray()) {
+            curr.children.putIfAbsent(c, new Trie());
+            curr = curr.children.get(c);
+        }
+        curr.word = word;
+    }
+
+    private static List<List<String>> faultyKeyboard(String sentence, List<String> dictionary) {
+        final List<List<String>> result = new ArrayList<>();
+        final Trie root = buildTrie(dictionary);
+        dfs(sentence, 0, root, root, new ArrayList<>(), result);
+        return result;
     }
 
     public static void main(String[] args) {
-        final FaultyKeyboard faultyKeyboard = new FaultyKeyboard();
-        faultyKeyboard.root = new Trie();
-        faultyKeyboard.input = "I lik   to   xplor   univ rs ";
-        for (String w : Arrays.asList("like", "explore", "toe", "universe", "rse")) {
-            faultyKeyboard.insert(w);
-            faultyKeyboard.longestWord = Math.max(faultyKeyboard.longestWord, w.length());
-        }
-        faultyKeyboard.backTrack(0, faultyKeyboard.input.length(), "");
-        for (String ans : faultyKeyboard.answer) {
-            System.out.println(ans);
-        }
+        System.out.println(faultyKeyboard("can s r n ",
+                                          Arrays.asList("can", "canes", "serene", "rene", "sam")));
+        System.out.println(faultyKeyboard("I lik  to  xplor  univ rs ",
+                                          Arrays.asList("I", "like", "explore", "to", "universe", "rse")));
     }
 }
