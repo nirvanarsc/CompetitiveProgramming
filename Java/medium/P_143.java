52a15d040bcb27fd87ab0d5f471d12ce90ebc61f
nirvanarsc <nirvana_rsc@yahoo.com>
2020-08-20 22:05:21 +0900

    Running on empty - food review! part 68

--- Java/medium/P_143.java
+++ Java/medium/P_143.java
@@ -2,40 +2,38 @@ package medium;
 
 import utils.DataStructures.ListNode;
 
+@SuppressWarnings({ "TailRecursion", "ConstantConditions" })
 public class P_143 {
 
     public void reorderList(ListNode head) {
         final ListNode dummy = new ListNode(-1);
         dummy.next = head;
+        ListNode upper = head;
         ListNode slow = dummy;
         ListNode fast = dummy;
-
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        final ListNode lower = slow.next;
+        ListNode lower = slow.next;
         slow.next = null;
-        ListNode head2 = reverse(lower, null);
-
-        while (head != null && head2 != null) {
-            final ListNode tmp1 = head.next;
-            final ListNode tmp2 = head2.next;
-            head2.next = head.next;
-            head.next = head2;
-            head = tmp1;
-            head2 = tmp2;
+        lower = reverse(lower, null);
+        while (lower != null) {
+            final ListNode t1 = upper.next;
+            final ListNode t2 = lower.next;
+            upper.next = lower;
+            lower.next = t1;
+            upper = t1;
+            lower = t2;
         }
     }
 
-    private static ListNode reverse(ListNode head, ListNode newHead) {
+    private static ListNode reverse(ListNode head, ListNode newTail) {
         if (head == null) {
-            return newHead;
+            return newTail;
         }
-
         final ListNode next = head.next;
-        head.next = newHead;
+        head.next = newTail;
         return reverse(next, head);
     }
 }
--- Java/medium/P_150.java
+++ Java/medium/P_150.java
@@ -1,16 +1,16 @@
 package medium;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Set;
 
-public final class P_150 {
+public class P_150 {
 
-    public static int evalRPN(String[] tokens) {
+    public int evalRPN(String[] tokens) {
         final Set<String> operations = new HashSet<>(Arrays.asList("+", "/", "*", "-"));
-        final Deque<Integer> stack = new LinkedList<>();
+        final Deque<Integer> stack = new ArrayDeque<>();
         for (String s : tokens) {
             if (operations.contains(s)) {
                 final int right = stack.removeFirst();
@@ -35,13 +35,4 @@ public final class P_150 {
 
         return stack.removeFirst();
     }
-
-    public static void main(String[] args) {
-        System.out.println(evalRPN(new String[] { "2", "1", "+", "3", "*" }));
-        System.out.println(evalRPN(new String[] { "4", "13", "5", "/", "+" }));
-        System.out.println(
-                evalRPN(new String[] { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" }));
-    }
-
-    private P_150() {}
 }
