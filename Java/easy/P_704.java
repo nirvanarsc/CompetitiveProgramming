ccb7a3d9f81432ae5b3479cd1a6eb531c6fad081
nirvanarsc <nirvana_rsc@yahoo.com>
2020-08-26 21:34:06 +0900

    Running on empty - food review! part 73

--- Java/easy/P_704.java
+++ Java/easy/P_704.java
@@ -1,18 +1,19 @@
-public class BinarySearch {
+package easy;
+
+public class P_704 {
 
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        while (low <= high) {
-            final int mid = (low + high) >>> 1;
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            final int mid = (lo + hi) >>> 1;
             if (nums[mid] < target) {
-                low = mid + 1;
+                lo = mid + 1;
             } else if (nums[mid] == target) {
                 return mid;
             } else {
-                high = mid - 1;
+                hi = mid - 1;
             }
         }
-
         return -1;
     }
 }
