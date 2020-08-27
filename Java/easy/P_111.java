b6b169a9a3317378d112cc9d5bf270bfcaf422b3
nirvanarsc <nirvana_rsc@yahoo.com>
2020-08-27 23:38:17 +0900

    Running on empty - food review! part 74

--- Java/easy/P_111.java
+++ Java/easy/P_111.java
@@ -1,55 +1,43 @@
 package easy;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 
 import utils.DataStructures.TreeNode;
 
 public class P_111 {
 
     public int minDepth(TreeNode root) {
-        return getDepth(root, 0);
+        return dfs(root);
     }
 
-    public int getDepth(TreeNode root, int depth) {
+    public int dfs(TreeNode root) {
         if (root == null) {
-            return depth;
+            return 0;
         }
-
-        final int left = getDepth(root.left, depth);
-        final int right = getDepth(root.right, depth);
-
+        final int left = dfs(root.left);
+        final int right = dfs(root.right);
         if (left == 0 || right == 0) {
             return left + right + 1;
         }
-
         return 1 + Math.min(left, right);
     }
 
     public int minDepthBFS(TreeNode root) {
-        final Deque<TreeNode> queue = new LinkedList<>();
-        int depth = 0;
+        final Deque<TreeNode> queue = new ArrayDeque<>();
         if (root != null) {
             queue.offerLast(root);
-            depth++;
         }
-        while (!queue.isEmpty()) {
-            int level = queue.size();
-            while (level-- > 0) {
+        for (int depth = 1; !queue.isEmpty(); depth++) {
+            for (int size = queue.size(); size > 0; size--) {
                 final TreeNode curr = queue.removeFirst();
                 if (curr.left == null && curr.right == null) {
                     return depth;
                 }
-                if (curr.right != null) {
-                    queue.offerLast(curr.right);
-                }
-                if (curr.left != null) {
-                    queue.offerLast(curr.left);
-                }
+                if (curr.right != null) { queue.offerLast(curr.right); }
+                if (curr.left != null) { queue.offerLast(curr.left); }
             }
-            depth++;
         }
-
-        return depth;
+        return 0;
     }
 }
--- Java/weekly_contests/weekly_164/P_1269.java
+++ Java/weekly_contests/weekly_164/P_1269.java
@@ -5,50 +5,41 @@ public class P_1269 {
     private static final int MOD = (int) (1e9 + 7);
 
     public int numWays(int steps, int arrLen) {
-        arrLen = Math.min(arrLen, steps);
-
-        final int[][] dp = new int[steps + 1][arrLen];
-
-        dp[1][0] = 1;
-        dp[1][1] = 1;
-
-        for (int step = 2; step <= steps; step++) {
-            for (int pos = 0; pos < arrLen; pos++) {
+        final int n = Math.min(arrLen, steps);
+        final int[][] dp = new int[steps + 1][n];
+        dp[0][0] = 1;
+        for (int step = 1; step <= steps; step++) {
+            for (int pos = 0; pos < Math.min(n, 2 * step); pos++) {
                 final int inPlace = dp[step - 1][pos];
                 final int left = pos - 1 >= 0 ? dp[step - 1][pos - 1] : 0;
-                final int right = pos + 1 < arrLen ? dp[step - 1][pos + 1] : 0;
-                dp[step][pos] += left;
-                dp[step][pos] %= MOD;
-                dp[step][pos] += right;
-                dp[step][pos] %= MOD;
-                dp[step][pos] += inPlace;
-                dp[step][pos] %= MOD;
+                final int right = pos + 1 < n ? dp[step - 1][pos + 1] : 0;
+                dp[step][pos] = (dp[step][pos] + left) % MOD;
+                dp[step][pos] = (dp[step][pos] + right) % MOD;
+                dp[step][pos] = (dp[step][pos] + inPlace) % MOD;
             }
         }
         return dp[steps][0];
     }
 
     public int numWaysTopDown(int steps, int arrLen) {
-        return recurse(0, steps, arrLen, new Integer[steps + 1][Math.min(steps, arrLen)]);
+        final int n = Math.min(steps, arrLen);
+        return dfs(0, steps, n, new Integer[steps + 1][n]);
     }
 
-    private static int recurse(int pos, int steps, int len, Integer[][] dp) {
-        if (steps == 0 && pos == 0) {
-            return 1;
+    private static int dfs(int idx, int steps, int n, Integer[][] dp) {
+        if (steps == 0) {
+            return idx == 0 ? 1 : 0;
         }
-        if (pos < 0 || pos == len || steps == 0 || pos > steps) {
+        if (idx < 0 || idx == n || idx > steps) {
             return 0;
         }
-        if (dp[steps][pos] != null) {
-            return dp[steps][pos];
+        if (dp[steps][idx] != null) {
+            return dp[steps][idx];
         }
         int res = 0;
-        res += recurse(pos, steps - 1, len, dp);
-        res %= MOD;
-        res += recurse(pos - 1, steps - 1, len, dp);
-        res %= MOD;
-        res += recurse(pos + 1, steps - 1, len, dp);
-        res %= MOD;
-        return dp[steps][pos] = res;
+        res = (res + dfs(idx, steps - 1, n, dp)) % MOD;
+        res = (res + dfs(idx - 1, steps - 1, n, dp)) % MOD;
+        res = (res + dfs(idx + 1, steps - 1, n, dp)) % MOD;
+        return dp[steps][idx] = res;
     }
 }
