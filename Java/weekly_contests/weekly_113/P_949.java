81a8e904ce9a69499713300ecb248984b35fe1b3
nirvanarsc <nirvana_rsc@yahoo.com>
2020-09-01 23:43:22 +0900

    Running on empty - food review! part 78

--- Java/weekly_contests/weekly_113/P_949.java
+++ Java/weekly_contests/weekly_113/P_949.java
@@ -2,7 +2,7 @@ package weekly_contests.weekly_113;
 
 public class P_949 {
 
-    public String largestTimeFromDigits(int[] ints) {
+    public String largestTimeFromDigitsIterative(int[] ints) {
         String ans = "";
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
@@ -20,5 +20,42 @@ public class P_949 {
             }
         }
         return ans;
+    }
+
+    public String largestTimeFromDigits(int[] ints) {
+        final String[] res = { "" };
+        final char[] chars = new char[4];
+        for (int i = 0; i < ints.length; i++) {
+            chars[i] = (char) ('0' + ints[i]);
+        }
+        dfs(0, res, chars);
+        return res[0].isEmpty() ? res[0] : res[0].substring(0, 2) + ':' + res[0].substring(2);
+    }
+
+    private static void dfs(int idx, String[] res, char[] curr) {
+        if (idx == curr.length) {
+            final String word = new String(curr);
+            if (isValidAndGreater(word, res[0])) {
+                res[0] = word;
+            }
+        }
+        for (int i = idx; i < curr.length; i++) {
+            swap(curr, i, idx);
+            dfs(idx + 1, res, curr);
+            swap(curr, i, idx);
+        }
+    }
+
+    private static void swap(char[] arr, int i, int j) {
+        final char t = arr[j];
+        arr[j] = arr[i];
+        arr[i] = t;
+    }
+
+    private static boolean isValidAndGreater(String left, String right) {
+        if ("24".compareTo(left.substring(0, 2)) > 0 && "60".compareTo(left.substring(2)) > 0) {
+            return right.isEmpty() || left.compareTo(right) > 0;
+        }
+        return false;
     }
 }
--- Java/weekly_contests/weekly_72/P_784.java
+++ Java/weekly_contests/weekly_72/P_784.java
@@ -1,27 +1,45 @@
 package weekly_contests.weekly_72;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.List;
 
+@SuppressWarnings("MethodParameterNamingConvention")
 public class P_784 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
     public List<String> letterCasePermutation(String S) {
         final List<String> res = new ArrayList<>();
-        helper(S.toCharArray(), 0, res);
+        dfs(S.toCharArray(), 0, res);
         return res;
     }
 
-    public void helper(char[] chars, int pos, List<String> res) {
-        if (pos == chars.length) {
+    private static void dfs(char[] chars, int idx, List<String> res) {
+        if (idx == chars.length) {
             res.add(new String(chars));
             return;
         }
-        if (Character.isLetter(chars[pos])) {
-            chars[pos] = Character.toLowerCase(chars[pos]);
-            helper(chars, pos + 1, res);
-            chars[pos] = Character.toUpperCase(chars[pos]);
+        if (Character.isLetter(chars[idx])) {
+            chars[idx] = Character.toLowerCase(chars[idx]);
+            dfs(chars, idx + 1, res);
+            chars[idx] = Character.toUpperCase(chars[idx]);
         }
-        helper(chars, pos + 1, res);
+        dfs(chars, idx + 1, res);
+    }
+
+    public List<String> letterCasePermutationBFS(String S) {
+        final Deque<String> q = new ArrayDeque<>(Collections.singletonList(S));
+        for (int i = 0; i < S.length(); i++) {
+            final char c = S.charAt(i);
+            if (Character.isLetter(c)) {
+                for (int size = q.size(); size > 0; size--) {
+                    final String curr = q.removeFirst();
+                    q.offerLast(curr.substring(0, i) + Character.toLowerCase(c) + curr.substring(i + 1));
+                    q.offerLast(curr.substring(0, i) + Character.toUpperCase(c) + curr.substring(i + 1));
+                }
+            }
+        }
+        return new ArrayList<>(q);
     }
 }
