1cc3a1284ed6617d4596c724f256259fb575da5d
nirvanarsc <nirvana_rsc@yahoo.com>
2020-09-13 17:21:44 +0900

    Weekly 206

---
+++ Java/weekly_contests/weekly_206/P_1584.java
@@ -0,0 +1,78 @@
+package weekly_contests.weekly_206;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class P_1584 {
+
+    public static class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+        private int count;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            final int rootP = find(p);
+            final int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+            }
+            count--;
+        }
+
+        public int count() { return count; }
+
+        public int[] size() { return size; }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        final List<int[]> weighted = new ArrayList<>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                final int w = dist(points[i], points[j]);
+                weighted.add(new int[] { i, j, w });
+            }
+        }
+        weighted.sort(Comparator.comparingInt(a -> a[2]));
+        final UnionFind uf = new UnionFind(points.length);
+        int res = 0;
+        for (int[] pp : weighted) {
+            if (uf.find(pp[0]) != uf.find(pp[1])) {
+                uf.union(pp[0], pp[1]);
+                res += pp[2];
+            }
+        }
+        return res;
+    }
+
+    private static int dist(int[] l, int[] r) {
+        return Math.abs(l[0] - r[0]) + Math.abs(l[1] - r[1]);
+    }
+}
---
+++ Java/weekly_contests/weekly_206/P_1585.java
@@ -0,0 +1,49 @@
+package weekly_contests.weekly_206;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class P_1585 {
+
+    public boolean isTransformable(String sX, String tX) {
+        if (sX.length() != tX.length()) {
+            return false;
+        }
+        final char[] sSorted = sX.toCharArray();
+        final char[] tSorted = tX.toCharArray();
+        Arrays.sort(sSorted);
+        Arrays.sort(tSorted);
+        if (!Arrays.equals(sSorted, tSorted)) {
+            return false;
+        }
+        final int n = sX.length();
+        final char[] s = sX.toCharArray();
+        final char[] t = tX.toCharArray();
+        final Map<Integer, Deque<Integer>> positions = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            positions.put(i, new ArrayDeque<>());
+        }
+        for (int i = 0; i < n; i++) {
+            positions.get(t[i] - '0').addLast(i);
+        }
+        final int[] mapTo = new int[n];
+        for (int i = 0; i < n; i++) {
+            mapTo[i] = positions.get(s[i] - '0').removeFirst();
+        }
+        final int[] lBound = new int[10];
+        for (int i = 0; i < n; i++) {
+            final int val = s[i] - '0';
+            final int to = mapTo[i];
+            if (to < lBound[val]) {
+                return false;
+            }
+            for (int j = val + 1; j < 10; j++) {
+                lBound[j] = Math.max(lBound[j], to);
+            }
+        }
+        return true;
+    }
+}
