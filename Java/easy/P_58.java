3a3ea896ec8989185a3fe9c227cb95c3ab4d1f47
nirvanarsc <nirvana_rsc@yahoo.com>
2020-09-15 21:33:24 +0900

    Running on empty - food review! part 90

--- Java/easy/P_58.java
+++ Java/easy/P_58.java
@@ -3,15 +3,17 @@ package easy;
 public class P_58 {
 
     public int lengthOfLastWord(String s) {
-        int res = 0;
-        s = s.trim();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') {
-                return res;
-            }
-            res++;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
-
-        return res;
+        if (i == -1) {
+            return 0;
+        }
+        int j = i - 1;
+        while (j >= 0 && s.charAt(j) != ' ') {
+            j--;
+        }
+        return i - j;
     }
 }
