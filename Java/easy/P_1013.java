a9caeaf6ce761c4b8247da5c4b793557e76c2521
nirvanarsc <nirvana_rsc@yahoo.com>
2020-09-23 21:46:24 +0900

    Running on empty - food review! part 97

---
+++ Java/easy/P_1013.java
@@ -0,0 +1,26 @@
+package easy;
+
+@SuppressWarnings("MethodParameterNamingConvention")
+public class P_1013 {
+
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum = 0;
+        for (int num : A) {
+            sum += num;
+        }
+        if (sum % 3 != 0) {
+            return false;
+        }
+        final int target = sum / 3;
+        int counter = 0;
+        sum = 0;
+        for (int num : A) {
+            sum += num;
+            if (sum == target) {
+                sum = 0;
+                counter++;
+            }
+        }
+        return counter >= 3;
+    }
+}
