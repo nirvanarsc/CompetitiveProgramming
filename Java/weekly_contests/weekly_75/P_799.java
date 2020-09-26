43bdba0e5c9b72203eef909b0ce0ecb8799bcefc
nirvanarsc <nirvana_rsc@yahoo.com>
2020-09-26 17:39:32 +0900

    Running on empty - food review! part 100

--- Java/weekly_contests/weekly_75/P_799.java
+++ Java/weekly_contests/weekly_75/P_799.java
@@ -1,20 +1,17 @@
 package weekly_contests.weekly_75;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
-import utils.DataStructures.UnionFind;
-
 public class P_799 {
 
     public double champagneTower(int poured, int query_row, int query_glass) {
-        final double[] dp = new double[101];
+        final double[] dp = new double[query_row + 1];
         dp[0] = poured;
         for (int row = 1; row <= query_row; row++) {
-            for (int i = row; i >= 0; i--) {
-                dp[i + 1] += dp[i] = Math.max(0.0, (dp[i] - 1) / 2);
+            for (int col = row; col >= 0; col--) {
+                double res = Math.max(0, dp[col] - 1) / 2;
+                if (col > 0) {
+                    res += Math.max(0, dp[col - 1] - 1) / 2;
+                }
+                dp[col] = res;
             }
         }
         return Math.min(dp[query_glass], 1.0);
