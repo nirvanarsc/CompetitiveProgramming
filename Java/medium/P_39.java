96730ca0f8e1c102d332f975b6c90d9a1374c56b
nirvanarsc <nirvana_rsc@yahoo.com>
2020-10-02 23:20:42 +0900

    Running on empty - food review! part 105

--- Java/medium/P_39.java
+++ Java/medium/P_39.java
@@ -9,22 +9,17 @@ public class P_39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         final List<List<Integer>> res = new ArrayList<>();
-        dfs(candidates, target, 0, new ArrayList<>(), res);
+        dfs(candidates, target, 0, res, new ArrayList<>());
         return res;
     }
 
-    private static void dfs(int[] candidates, int target, int idx, List<Integer> curr,
-                            List<List<Integer>> res) {
+    private static void dfs(int[] cand, int target, int idx, List<List<Integer>> res, List<Integer> curr) {
         if (target == 0) {
             res.add(new ArrayList<>(curr));
-            return;
         }
-        for (int i = idx; i < candidates.length; i++) {
-            if (target < candidates[i]) {
-                break;
-            }
-            curr.add(candidates[i]);
-            dfs(candidates, target - candidates[i], i, curr, res);
+        for (int i = idx; i < cand.length && target >= cand[idx]; i++) {
+            curr.add(cand[i]);
+            dfs(cand, target - cand[i], i, res, curr);
             curr.remove(curr.size() - 1);
         }
     }
