e007286a7af94efcbcc0fffbcab5e82bde2ed18e
nirvanarsc <nirvana_rsc@yahoo.com>
2020-10-05 22:39:31 +0900

    Running on empty - food review! part 107

--- Java/weekly_contests/weekly_128/P_1009.java
+++ Java/weekly_contests/weekly_128/P_1009.java
@@ -1,21 +1,19 @@
 package weekly_contests.weekly_128;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@SuppressWarnings("MethodParameterNamingConvention")
 public class P_1009 {
 
     public int bitwiseComplement(int N) {
         if (N == 0) { return 1; }
-        final List<Integer> ints = new ArrayList<>();
-        int res = 0;
-        while (N != 0) {
-            ints.add(N % 2 == 0 ? 1 : 0);
-            N >>= 1;
+        int mask = ~0;
+        while ((N & mask) != 0) {
+            mask <<= 1;
         }
-        for (int i = ints.size() - 1; i >= 0; i--) {
-            res = (res << 1) + ints.get(i);
-        }
-        return res;
+        return N ^ ~mask;
+    }
+
+    public int bitwiseComplementLib(int N) {
+        if (N == 0) { return 1; }
+        return (Integer.highestOneBit(N << 1) - 1) ^ N;
     }
 }
