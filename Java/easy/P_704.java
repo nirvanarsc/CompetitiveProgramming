06c3e9095ef03c11a5e23a5e6f83fc82fa57d44d
nirvanarsc <nirvana_rsc@yahoo.com>
2020-10-08 23:01:30 +0900

    Running on empty - food review! part 110

--- Java/easy/P_704.java
+++ Java/easy/P_704.java
@@ -5,7 +5,7 @@ public class P_704 {
     public int search(int[] nums, int target) {
         int lo = 0, hi = nums.length - 1;
         while (lo <= hi) {
-            final int mid = (lo + hi) >>> 1;
+            final int mid = lo + hi >>> 1;
             if (nums[mid] < target) {
                 lo = mid + 1;
             } else if (nums[mid] == target) {
@@ -15,5 +15,31 @@ public class P_704 {
             }
         }
         return -1;
+    }
+
+    public int searchUpperBound(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            final int mid = lo + hi + 1 >>> 1;
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
+            }
+        }
+        return nums[lo] == target ? lo : -1;
+    }
+
+    public int searchLowerBound(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            final int mid = lo + hi >>> 1;
+            if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return nums[lo] == target ? lo : -1;
     }
 }
