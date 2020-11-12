e90894d43468ad36c7f02d8b30d6ac32f3230057
nirvanarsc <nirvana_rsc@yahoo.com>
2020-11-12 23:27:31 +0900

    Running on empty - food review! part 142

--- Java/leetcode/hard/P_710.java
+++ Java/leetcode/hard/P_710.java
@@ -1,19 +1,21 @@
 package leetcode.hard;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
-@SuppressWarnings({ "MethodParameterNamingConvention", "unused" })
+@SuppressWarnings({ "MethodParameterNamingConvention", "InnerClassMayBeStatic", "unused" })
 public class P_710 {
 
-    static class Solution {
-
+    class SolutionMap {
         Map<Integer, Integer> map;
         Random r;
         int range;
 
-        Solution(int N, int[] blacklist) {
+        SolutionMap(int N, int[] blacklist) {
             map = new HashMap<>();
             for (int b : blacklist) {
                 map.put(b, -1);
@@ -34,6 +36,34 @@ public class P_710 {
         public int pick() {
             final int p = r.nextInt(range);
             return map.getOrDefault(p, p);
+        }
+    }
+
+    class Solution {
+        Random r;
+        Map<Integer, Integer> banned;
+        int limit;
+
+        Solution(int N, int[] blacklist) {
+            Arrays.sort(blacklist);
+            r = new Random();
+            banned = new HashMap<>();
+            limit = N - blacklist.length;
+            final Set<Integer> set = new HashSet<>();
+            for (int num : blacklist) {
+                set.add(num);
+            }
+            for (int i = limit, j = 0, k = 0; k < blacklist.length; k++, i++) {
+                if (set.contains(i)) {
+                    continue;
+                }
+                banned.put(blacklist[j++], i);
+            }
+        }
+
+        public int pick() {
+            final int num = r.nextInt(limit);
+            return banned.getOrDefault(num, num);
         }
     }
 }
