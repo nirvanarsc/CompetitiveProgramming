e018af62606729d80f0ddca88aad0c363739180c
nirvanarsc <nirvana_rsc@yahoo.com>
2020-11-16 23:02:38 +0900

    Running on empty - food review! part 146

---
+++ Java/atcoder/beginner_183/C.java
@@ -0,0 +1,121 @@
+package atcoder.beginner_183;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public final class C {
+
+    public static void main(String[] args) {
+        final FastScanner fs = new FastScanner();
+        final int n = fs.nextInt();
+        final int k = fs.nextInt();
+        final int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = fs.nextInt();
+            }
+        }
+        final int[] res = { 0 };
+        dfs(grid, 1, 0, (1 << n) - 1, k, 0, res);
+        System.out.println(res[0]);
+    }
+
+    private static void dfs(int[][] grid, int mask, int prev, int target, int k, int path, int[] res) {
+        if (mask == target) {
+            if ((grid[prev][0] + path) == k) {
+                res[0]++;
+            }
+            return;
+        }
+        for (int i = 1; i < grid.length; i++) {
+            if ((mask & (1 << i)) == 0) {
+                dfs(grid, mask | (1 << i), i, target, k, path + grid[prev][i], res);
+            }
+        }
+    }
+
+    static final class Utils {
+        public static void shuffleSort(int[] arr) {
+            shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffle(int[] arr) {
+            final Random r = new Random();
+
+            for (int i = 0; i <= arr.length - 2; i++) {
+                final int j = i + r.nextInt(arr.length - i);
+                swap(arr, i, j);
+            }
+        }
+
+        public static void shuffle(long[] arr) {
+            final Random r = new Random();
+
+            for (int i = 0; i <= arr.length - 2; i++) {
+                final int j = i + r.nextInt(arr.length - i);
+                swap(arr, i, j);
+            }
+        }
+
+        public static void swap(int[] arr, int i, int j) {
+            final int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+
+        public static void swap(long[] arr, int i, int j) {
+            final long t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+
+        private Utils() {}
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        private String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    //noinspection CallToPrintStackTrace
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] nextIntArray(int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) { a[i] = nextInt(); }
+            return a;
+        }
+
+        long[] nextLongArray(int n) {
+            final long[] a = new long[n];
+            for (int i = 0; i < n; i++) { a[i] = nextLong(); }
+            return a;
+        }
+    }
+}
