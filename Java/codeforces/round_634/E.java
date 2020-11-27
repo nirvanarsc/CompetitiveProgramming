9496bdf346bedf523f0193f8465189c4709cfa8b
nirvanarsc <nirvana_rsc@yahoo.com>
2020-11-27 20:35:54 +0900

    Running on empty - food review! part 157

---
+++ Java/codeforces/round_634/E.java
@@ -0,0 +1,138 @@
+package codeforces.round_634;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public final class E {
+
+    public static void main(String[] args) {
+        final FastScanner fs = new FastScanner();
+        final int t = fs.nextInt();
+        for (int test = 0; test < t; test++) {
+            final int n = fs.nextInt();
+            final int[] arr = fs.nextIntArray(n);
+            final Map<Integer, List<Integer>> indices = new HashMap<>();
+            final int[][] preSum = new int[200][n];
+            for (int i = 0; i < n; i++) {
+                final int curr = arr[i];
+                indices.computeIfAbsent(curr, val -> new ArrayList<>()).add(i);
+            }
+            for (int i = 1; i <= 200; i++) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    count += arr[j] == i ? 1 : 0;
+                    preSum[i - 1][j] = count;
+                }
+            }
+            int res = 0;
+            for (int i = 1; i <= 200; i++) {
+                final List<Integer> curr = indices.getOrDefault(i, Collections.emptyList());
+                res = Math.max(res, curr.size());
+                if (curr.size() <= 1) {
+                    continue;
+                }
+                for (int side = 1; side * 2 <= curr.size(); side++) {
+                    final int l = curr.get(side - 1);
+                    final int r = curr.get(curr.size() - side);
+                    for (int j = 0; j < 200; j++) {
+                        if (j + 1 != i) {
+                            final int mid = preSum[j][r] - preSum[j][l];
+                            res = Math.max(res, 2 * side + mid);
+                        }
+                    }
+                }
+            }
+            System.out.println(res);
+        }
+    }
+
+    static final class Utils {
+        public static void shuffleSort(int[] x) {
+            shuffle(x);
+            Arrays.sort(x);
+        }
+
+        public static void shuffleSort(long[] x) {
+            shuffle(x);
+            Arrays.sort(x);
+        }
+
+        public static void shuffle(int[] x) {
+            final Random r = new Random();
+
+            for (int i = 0; i <= x.length - 2; i++) {
+                final int j = i + r.nextInt(x.length - i);
+                swap(x, i, j);
+            }
+        }
+
+        public static void shuffle(long[] x) {
+            final Random r = new Random();
+
+            for (int i = 0; i <= x.length - 2; i++) {
+                final int j = i + r.nextInt(x.length - i);
+                swap(x, i, j);
+            }
+        }
+
+        public static void swap(int[] x, int i, int j) {
+            final int t = x[i];
+            x[i] = x[j];
+            x[j] = t;
+        }
+
+        public static void swap(long[] x, int i, int j) {
+            final long t = x[i];
+            x[i] = x[j];
+            x[j] = t;
+        }
+
+        private Utils() {}
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        private String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    //noinspection CallToPrintStackTrace
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] nextIntArray(int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) { a[i] = nextInt(); }
+            return a;
+        }
+
+        long[] nextLongArray(int n) {
+            final long[] a = new long[n];
+            for (int i = 0; i < n; i++) { a[i] = nextLong(); }
+            return a;
+        }
+    }
+}
--- Java/leetcode/medium/P_416.java
+++ Java/leetcode/medium/P_416.java
@@ -4,74 +4,47 @@ public class P_416 {
 
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int i : nums) {
-            sum += i;
+        for (int num : nums) {
+            sum += num;
         }
         if (sum % 2 != 0) {
             return false;
         }
-        final boolean[] dp = new boolean[sum / 2 + 1];
+        final int target = sum / 2;
+        final boolean[] dp = new boolean[target + 1];
         dp[0] = true;
-        for (int i : nums) {
-            for (int j = sum / 2; j >= i; j--) {
-                dp[j] |= dp[j - i];
+        for (int num : nums) {
+            for (int s = target; s >= 0; s--) {
+                if (num <= s) {
+                    dp[s] |= dp[s - num];
+                }
             }
         }
-
-        return dp[sum / 2];
+        return dp[target];
     }
 
     public boolean canPartitionTopDown(int[] nums) {
         int sum = 0;
-        for (int i : nums) { sum += i; }
-        if (sum % 2 != 0) { return false; }
-        return process(nums, sum / 2, 0, 0);
-    }
-
-    private static boolean process(int[] nums, int target, int index, int sum) {
-        if (sum > target) {
-            return false;
-        }
-        if (sum == target) {
-            return true;
-        }
-        for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) { continue; }
-            if (process(nums, target, i + 1, sum + nums[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean canPartition2(int[] nums) {
-        int sum = 0;
-        for (int i : nums) {
-            sum += i;
+        for (int num : nums) {
+            sum += num;
         }
         if (sum % 2 != 0) {
             return false;
         }
+        return dfs(nums, sum / 2, 0, 0, new Boolean[nums.length][sum / 2 + 1]);
+    }
 
-        final int k = sum / 2;
-        final int[][] dp = new int[nums.length][k + 1];
-
-        for (int i = 0; i <= k; i++) {
-            if (nums[0] <= i) {
-                dp[0][i] = nums[0];
-            }
+    private static boolean dfs(int[] nums, int target, int index, int sum, Boolean[][] dp) {
+        if (index == nums.length) {
+            return sum == target;
         }
-
-        for (int i = 1; i < nums.length; i++) {
-            for (int n = 0; n <= k; n++) {
-                if (n >= nums[i]) {
-                    dp[i][n] = Math.max(dp[i - 1][n], dp[i - 1][n - nums[i]] + nums[i]);
-                } else {
-                    dp[i][n] = dp[i - 1][n];
-                }
-            }
+        if (dp[index][sum] != null) {
+            return dp[index][sum];
         }
-
-        return dp[nums.length - 1][k] == k;
+        boolean res = dfs(nums, target, index + 1, sum, dp);
+        if (sum + nums[index] <= target) {
+            res |= dfs(nums, target, index + 1, sum + nums[index], dp);
+        }
+        return dp[index][sum] = res;
     }
 }
