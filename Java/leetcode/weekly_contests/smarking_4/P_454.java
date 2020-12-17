b7bd41ecc4909ced70ac86e7604809493dfa7c86
nirvanarsc <nirvana_rsc@yahoo.com>
2020-12-17 23:18:31 +0900

    Running on empty - food review! part 177

--- Java/leetcode/weekly_contests/smarking_4/P_454.java
+++ Java/leetcode/weekly_contests/smarking_4/P_454.java
@@ -3,20 +3,21 @@ package leetcode.weekly_contests.smarking_4;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("MethodParameterNamingConvention")
 public class P_454 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         int res = 0;
-        final Map<Integer, Integer> map = new HashMap<>();
-        for (int valueA : A) {
-            for (int valueB : B) {
-                map.merge(valueA + valueB, 1, Integer::sum);
+        final Map<Integer, Integer> countAB = new HashMap<>();
+        for (int ll : A) {
+            for (int rr : B) {
+                countAB.merge(ll + rr, 1, Integer::sum);
             }
         }
-        for (int valueC : C) {
-            for (int valueD : D) {
-                res += map.getOrDefault(-valueC - valueD, 0);
+        for (int ll : C) {
+            for (int rr : D) {
+                final int target = ll + rr;
+                res += countAB.getOrDefault(-target, 0);
             }
         }
         return res;
