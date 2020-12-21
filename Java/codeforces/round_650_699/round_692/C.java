cb14d12be1d971ef887e5e3ea9fec8cfafdbe23e
nirvanarsc <nirvana_rsc@yahoo.com>
2020-12-21 20:57:10 +0900

    Running on empty - food review! part 181

---
+++ Java/codeforces/round_650_699/round_692/C.java
@@ -0,0 +1,165 @@
+package codeforces.round_650_699.round_692;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public final class C {
+
+    private static final class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+        private int count;
+
+        private UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            final int rootP = find(p);
+            final int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+                size[rootQ] = 0;
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+                size[rootP] = 0;
+            }
+            count--;
+        }
+
+        public int count() { return count; }
+
+        public int[] size() { return size; }
+    }
+
+    public static void main(String[] args) {
+        final FastScanner fs = new FastScanner();
+        final int t = fs.nextInt();
+        for (int test = 0; test < t; test++) {
+            final int n = fs.nextInt();
+            final int m = fs.nextInt();
+            final UnionFind uf = new UnionFind(n);
+            int res = m;
+            for (int i = 0; i < m; i++) {
+                final int u = fs.nextInt() - 1;
+                final int v = fs.nextInt() - 1;
+                if (u == v) {
+                    res--;
+                } else {
+                    if (uf.find(u) == uf.find(v)) {
+                        res++;
+                    } else {
+                        uf.union(u, v);
+                    }
+                }
+            }
+            System.out.println(res);
+        }
+    }
+
+    static final class Utils {
+        public static void shuffleSort(int[] x) {
+            shuffle(x);
+            Arrays.sort(x);
+        }
+
+        public static void shuffleSort(long[] x) {
+            shuffle(x);
+            Arrays.sort(x);
+        }
+
+        public static void shuffle(int[] x) {
+            final Random r = new Random();
+
+            for (int i = 0; i <= x.length - 2; i++) {
+                final int j = i + r.nextInt(x.length - i);
+                swap(x, i, j);
+            }
+        }
+
+        public static void shuffle(long[] x) {
+            final Random r = new Random();
+
+            for (int i = 0; i <= x.length - 2; i++) {
+                final int j = i + r.nextInt(x.length - i);
+                swap(x, i, j);
+            }
+        }
+
+        public static void swap(int[] x, int i, int j) {
+            final int t = x[i];
+            x[i] = x[j];
+            x[j] = t;
+        }
+
+        public static void swap(long[] x, int i, int j) {
+            final long t = x[i];
+            x[i] = x[j];
+            x[j] = t;
+        }
+
+        private Utils() {}
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        private String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    //noinspection CallToPrintStackTrace
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] nextIntArray(int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) { a[i] = nextInt(); }
+            return a;
+        }
+
+        long[] nextLongArray(int n) {
+            final long[] a = new long[n];
+            for (int i = 0; i < n; i++) { a[i] = nextLong(); }
+            return a;
+        }
+    }
+}
