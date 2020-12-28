13d0d068d71ae7151b3b70128ffd2c191c43f022
nirvanarsc <nirvana_rsc@yahoo.com>
2020-12-28 22:40:30 +0900

    Running on empty - food review! part 188

--- Java/leetcode/weekly_contests/weekly_65/P_754.java
+++ Java/leetcode/weekly_contests/weekly_65/P_754.java
@@ -2,14 +2,29 @@ package leetcode.weekly_contests.weekly_65;
 
 public class P_754 {
 
+    private static long sum(int n) {
+        return ((long) n * (n + 1)) / 2;
+    }
+
     public int reachNumber(int target) {
-        final long lTarget = Math.abs(target);
-        int step = 0;
-        int sum = 0;
-        while (sum < lTarget || (sum - lTarget) % 2 != 0) {
-            step++;
-            sum += step;
+        int steps = 0;
+        target = Math.abs(target);
+        while (sum(steps) < target) {
+            steps++;
         }
-        return step;
+        if (steps % 4 == 1 || steps % 4 == 2) {
+            if (target % 2 == 0) {
+                while (steps % 4 != 3) {
+                    steps++;
+                }
+            }
+        } else {
+            if (target % 2 != 0) {
+                while (steps % 4 != 1) {
+                    steps++;
+                }
+            }
+        }
+        return steps;
     }
 }
