811abc1682eee71fb27c8ea8d1dc31adec30cdf2
nirvanarsc <nirvana_rsc@yahoo.com>
2021-01-21 23:01:01 +0900

    Running on empty - food review! part 212

--- Java/leetcode/weekly_contests/weekly_217/P_1673.java
+++ Java/leetcode/weekly_contests/weekly_217/P_1673.java
@@ -17,4 +17,64 @@ public class P_1673 {
         }
         return dq.stream().limit(k).mapToInt(Integer::intValue).toArray();
     }
+
+    private static class SegTree {
+        int leftMost, rightMost;
+        SegTree left, right;
+        int[] min = { Integer.MAX_VALUE, -1 };
+
+        SegTree(int leftMost, int rightMost, int[] arr) {
+            this.leftMost = leftMost;
+            this.rightMost = rightMost;
+            if (leftMost == rightMost) {
+                min[0] = arr[leftMost];
+                min[1] = leftMost;
+            } else {
+                final int mid = leftMost + rightMost >>> 1;
+                left = new SegTree(leftMost, mid, arr);
+                right = new SegTree(mid + 1, rightMost, arr);
+                recalc();
+            }
+        }
+
+        private void recalc() {
+            if (leftMost == rightMost) {
+                return;
+            }
+            if (left.min[0] <= right.min[0]) {
+                min = left.min;
+            } else {
+                min = right.min;
+            }
+        }
+
+        private int[] query(int l, int r) {
+            if (r < leftMost || l > rightMost) {
+                return new int[] { Integer.MAX_VALUE, -1 };
+            }
+            if (l <= leftMost && rightMost <= r) {
+                return min;
+            }
+            final int[] ll = left.query(l, r);
+            final int[] rr = right.query(l, r);
+            if (ll[0] <= rr[0]) {
+                return ll;
+            } else {
+                return rr;
+            }
+        }
+    }
+
+    public int[] mostCompetitiveST(int[] nums, int k) {
+        final int n = nums.length;
+        final SegTree st = new SegTree(0, n - 1, nums);
+        final int[] res = new int[k];
+        int l = 0;
+        for (int i = 0; i < k; i++) {
+            final int[] min = st.query(l, n - k + i);
+            res[i] = min[0];
+            l = min[1] + 1;
+        }
+        return res;
+    }
 }
