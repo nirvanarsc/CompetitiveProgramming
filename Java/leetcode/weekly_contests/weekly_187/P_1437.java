734532b78bf518985adc098add770512f73d08b7
nirvanarsc <nirvana_rsc@yahoo.com>
2021-01-25 22:00:00 +0900

    Running on empty - food review! part 216

--- Java/leetcode/weekly_contests/weekly_187/P_1437.java
+++ Java/leetcode/weekly_contests/weekly_187/P_1437.java
@@ -3,18 +3,16 @@ package leetcode.weekly_contests.weekly_187;
 public class P_1437 {
 
     public boolean kLengthApart(int[] nums, int k) {
-        int j = 0;
-        int res = Integer.MAX_VALUE;
-        int sum = 0;
+        int max = (int) 1e9;
+        int j = -1;
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (sum == 2) {
-                while (sum == 2) {
-                    sum -= nums[j++];
-                }
-                res = Math.min(res, i - j);
+            if (nums[i] == 1 && j == -1) {
+                j = i;
+            } else if (nums[i] == 1) {
+                max = Math.min(max, i - j - 1);
+                j = i;
             }
         }
-        return res >= k;
+        return max >= k;
     }
 }
