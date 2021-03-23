daa1a489b17c35ac3a71cabf34e25a792196e7c6
nirvanarsc <nirvana_rsc@yahoo.com>
2021-03-23 23:55:56 +0900

    Running on empty - food review! part 272

--- Java/leetcode/weekly_contests/weekly_106/P_923.java
+++ Java/leetcode/weekly_contests/weekly_106/P_923.java
@@ -2,87 +2,55 @@ package leetcode.weekly_contests.weekly_106;
 
 import java.util.Arrays;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_923 {
 
     private static final int MOD = (int) (1e9 + 7);
 
-    private static class Combinations {
-        long[] factorial;
-        long[] facInverse;
-        long[] inverse;
-
-        Combinations(int n) {
-            final int MAX = n + 2;
-            factorial = new long[MAX];
-            facInverse = new long[MAX];
-            inverse = new long[MAX];
-            factorial[0] = factorial[1] = 1;
-            facInverse[0] = facInverse[1] = 1;
-            inverse[1] = 1;
-            for (int i = 2; i < MAX; i++) {
-                factorial[i] = factorial[i - 1] * i % MOD;
-                final long inv = inverse[i] = MOD - inverse[MOD % i] * (MOD / i) % MOD;
-                facInverse[i] = facInverse[i - 1] * inv % MOD;
-            }
-        }
-
-        long ncr(int n, int r) {
-            if (n < r) { return 0; }
-            if (n < 0 || r < 0) { return 0; }
-            return factorial[n] * (facInverse[r] * facInverse[n - r] % MOD) % MOD;
-        }
-
-        long modpow(long a, long n) {
-            long res = 1;
-            while (n > 0) {
-                if (n % 2 == 1) {
-                    res = res * a % MOD;
-                }
-                a = a * a % MOD;
-                n /= 2;
-            }
-            return res;
-        }
-    }
-
-    public int threeSumMulti(int[] A, int target) {
-        Arrays.sort(A);
-        final Combinations comb = new Combinations((int) (3e3 + 5));
-        final int n = A.length;
-        final int[] count = new int[101];
-        for (int num : A) {
-            count[num]++;
+    public int threeSumMulti(int[] arr, int target) {
+        Arrays.sort(arr);
+        final int[] f = new int[105];
+        final int n = arr.length;
+        for (int num : arr) {
+            f[num]++;
         }
         long res = 0;
         for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && A[i] == A[i - 1]) { continue; }
+            if (i > 0 && arr[i - 1] == arr[i]) {
+                continue;
+            }
             int lo = i + 1;
             int hi = n - 1;
-            final int currT = target - A[i];
             while (lo < hi) {
-                if (A[lo] + A[hi] > currT) {
+                final int sum = arr[i] + arr[lo] + arr[hi];
+                if (sum == target) {
+                    if (arr[i] == arr[lo] && arr[i] == arr[hi]) {
+                        res = (res + nC3(f[arr[i]])) % MOD;
+                    } else if (arr[i] == arr[lo]) {
+                        res = (res + nC2(f[arr[lo]]) * f[arr[hi]]) % MOD;
+                    } else if (arr[lo] == arr[hi]) {
+                        res = (res + nC2(f[arr[lo]]) * f[arr[i]]) % MOD;
+                    } else {
+                        res = (res + (long) f[arr[i]] * f[arr[lo]] * f[arr[hi]]) % MOD;
+                    }
+                    while (lo < hi && arr[lo + 1] == arr[lo]) { lo++; }
+                    while (lo < hi && arr[hi - 1] == arr[hi]) { hi--; }
+                    lo++;
                     hi--;
-                } else if (A[lo] + A[hi] < currT) {
+                } else if (sum < target) {
                     lo++;
                 } else {
-                    if (A[i] == A[lo] && A[lo] == A[hi]) {
-                        res += comb.ncr(count[A[lo]], 3);
-                    } else if (A[lo] == A[hi]) {
-                        res += comb.ncr(count[A[lo]], 2) * count[A[i]];
-                    } else if (A[i] == A[lo]) {
-                        res += comb.ncr(count[A[lo]], 2) * count[A[hi]];
-                    } else {
-                        res += count[A[lo]] * count[A[hi]] * count[A[i]];
-                    }
-                    res %= MOD;
-                    while (lo < hi && A[lo + 1] == A[lo]) { lo++; }
-                    while (lo < hi && A[hi - 1] == A[hi]) { hi--; }
-                    lo++;
                     hi--;
                 }
             }
         }
         return (int) res;
+    }
+
+    private static long nC3(long n) {
+        return (n * (n - 1) * (n - 2)) / 6;
+    }
+
+    private static long nC2(long n) {
+        return (n * (n - 1)) / 2;
     }
 }
--- Java/leetcode/weekly_contests/weekly_233/P_1803.java
+++ Java/leetcode/weekly_contests/weekly_233/P_1803.java
@@ -3,94 +3,48 @@ package leetcode.weekly_contests.weekly_233;
 public class P_1803 {
 
     private static class Trie {
-        Trie left;
-        Trie right;
-        Trie up;
-        long count = 1;
+        Trie[] c = new Trie[2];
+        int count;
     }
 
-    private static void update(Trie root) {
-        long count = 0;
-        count += root.left == null ? 0 : root.left.count;
-        count += root.right == null ? 0 : root.right.count;
-        root.count += count;
-
-        if (root.up != null) {
-            update(root.up);
+    private static void add(int num, Trie root) {
+        Trie iter = root;
+        for (int i = 15; i >= 0; i--) {
+            final int bit = (num >> i) & 1;
+            if (iter.c[bit] == null) {
+                iter.c[bit] = new Trie();
+            }
+            iter = iter.c[bit];
+            iter.count++;
         }
     }
 
-    private static void add(int num, int level, Trie root) {
-        if (level == -1) {
-            update(root);
-            return;
-        }
-        if ((num & (1 << level)) != 0) {
-            if (root.right == null) {
-                final Trie c = new Trie();
-                root.right = c;
-                c.up = root;
+    private static int f(Trie root, int n, int k) {
+        int res = 0;
+        for (int i = 15; i >= 0 && root != null; i--) {
+            final int x = (n >> i) & 1;
+            final int y = (k >> i) & 1;
+            if (y == 1) {
+                if (root.c[x] != null) {
+                    res += root.c[x].count;
+                }
+                root = root.c[1 ^ x];
+            } else {
+                root = root.c[x];
             }
-            add(num, level - 1, root.right);
-        } else {
-            if (root.left == null) {
-                final Trie c = new Trie();
-                root.left = c;
-                c.up = root;
-            }
-            add(num, level - 1, root.left);
         }
+        return res;
     }
 
     public int countPairs(int[] nums, int low, int high) {
-        final int h = 16;
         final Trie root = new Trie();
-        add(0, 16, root);
-        final long[] hi = { 0 };
+        int lo = 0;
+        int hi = 0;
         for (int num : nums) {
-            dfs(num, high, h, root, hi);
-            add(num, h, root);
+            hi += f(root, num, high + 1);
+            lo += f(root, num, low);
+            add(num, root);
         }
-        final Trie root2 = new Trie();
-        add(0, 16, root2);
-        final long[] lo = { 0 };
-        for (int num : nums) {
-            dfs(num, low, h, root2, lo);
-            add(num, h, root2);
-        }
-        return (int) (hi[0] - lo[0]);
-    }
-
-    private static void dfs(int num, int k, int level, Trie root, long[] ans) {
-        if (level == -1) {
-            return;
-        }
-        if ((num & (1 << level)) != 0) {
-            if ((k & (1 << level)) != 0) {
-                if (root.right != null) {
-                    ans[0] += root.right.count;
-                }
-                if (root.left != null) {
-                    dfs(num, k, level - 1, root.left, ans);
-                }
-            } else {
-                if (root.right != null) {
-                    dfs(num, k, level - 1, root.right, ans);
-                }
-            }
-        } else {
-            if ((k & (1 << level)) != 0) {
-                if (root.left != null) {
-                    ans[0] += root.left.count;
-                }
-                if (root.right != null) {
-                    dfs(num, k, level - 1, root.right, ans);
-                }
-            } else {
-                if (root.left != null) {
-                    dfs(num, k, level - 1, root.left, ans);
-                }
-            }
-        }
+        return hi - lo;
     }
 }
