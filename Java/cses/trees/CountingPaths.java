6d79f99c655197ba179adeef33ae4e1600ad0e25
nirvanarsc <nirvana_rsc@yahoo.com>
2021-04-03 23:18:11 +0900

    Running on empty - food review! part 283

---
+++ Java/cses/trees/CountingPaths.java
@@ -0,0 +1,401 @@
+package cses.trees;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
+public final class CountingPaths {
+
+    static int n;
+    static int m;
+    static int h;
+    static int time;
+    static int[][] parents;
+    static int[][] edges;
+    static int[][] g;
+    static int[] parent;
+    static int[] depth;
+    static int[] in;
+    static int[] out;
+    static int[] add;
+    static int[] remove;
+    static int[] common;
+    static int[] res;
+
+    public static void main(String[] args) throws IOException {
+        final FastReader fs = new FastReader();
+        n = fs.nextInt();
+        m = fs.nextInt();
+        h = 18;
+        edges = new int[n - 1][2];
+        for (int i = 0; i < (n - 1); i++) {
+            edges[i] = new int[] { fs.nextInt() - 1, fs.nextInt() - 1 };
+        }
+        g = packG();
+        parent = new int[n];
+        depth = new int[n];
+        in = new int[n];
+        out = new int[n];
+        add = new int[n];
+        remove = new int[n];
+        common = new int[n];
+        res = new int[n];
+        dfs1();
+        dfs2();
+        initParents();
+        for (int i = 0; i < m; i++) {
+            final int l = fs.nextInt() - 1;
+            final int r = fs.nextInt() - 1;
+            final int lca = getLca(l, r);
+            add[l]++;
+            add[r]++;
+            remove[lca] += 2;
+            if (lca != l && lca != r) {
+                common[lca]++;
+            } else {
+                add[lca]--;
+                remove[lca]--;
+            }
+        }
+        dfs3();
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(res[i]);
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs1() {
+        final int[] stack = new int[n];
+        int stackIdx = 0;
+        stack[stackIdx++] = 0;
+        parent[0] = 0;
+        depth[0] = 0;
+        while (stackIdx > 0) {
+            final int u = stack[--stackIdx];
+            depth[u] = depth[parent[u]] + 1;
+            for (int v : g[u]) {
+                if (v != parent[u]) {
+                    parent[v] = u;
+                    stack[stackIdx++] = v;
+                }
+            }
+        }
+    }
+
+    private static void dfs2() {
+        final int[] stack = new int[n];
+        final int[] idx = new int[n];
+        int stackIdx = 0;
+        stack[stackIdx++] = 0;
+        while (stackIdx > 0) {
+            final int u = stack[stackIdx - 1];
+            if (idx[u] == 0) {
+                in[u] = time++;
+            }
+            if (idx[u] < g[u].length) {
+                final int v = g[u][idx[u]++];
+                if (parent[u] != v) {
+                    stack[stackIdx++] = v;
+                }
+            } else {
+                out[u] = time;
+                stackIdx--;
+            }
+        }
+    }
+
+    private static void dfs3() {
+        final int[] stack = new int[n];
+        final int[] idx = new int[n];
+        int stackIdx = 0;
+        stack[stackIdx++] = 0;
+        while (stackIdx > 0) {
+            final int u = stack[stackIdx - 1];
+            if (idx[u] < g[u].length) {
+                final int v = g[u][idx[u]++];
+                if (parent[u] != v) {
+                    stack[stackIdx++] = v;
+                }
+            } else {
+                res[u] += add[u] - common[u];
+                add[u] -= remove[u];
+                add[parent[u]] += add[u];
+                stackIdx--;
+            }
+        }
+    }
+
+//    private static void dfs12(int u, int v) {
+//        parent[u] = v;
+//        depth[u] = depth[v] + 1;
+//        in[u] = time++;
+//        for (int next : g[u]) {
+//            if (next != v) {
+//                dfs1(next, u);
+//            }
+//        }
+//        out[u] = time;
+//    }
+//
+//    private static void dfs3(int u, int v) {
+//        for (int next : g[u]) {
+//            if (next != v) {
+//                dfs2(next, u);
+//            }
+//        }
+//        res[u] += add[u] - common[u];
+//        add[u] -= remove[u];
+//        add[parent[u]] += add[u];
+//    }
+
+    private static int[][] packG() {
+        final int[][] g = new int[n][];
+        final int[] size = new int[n];
+        for (int[] edge : edges) {
+            ++size[edge[0]];
+            ++size[edge[1]];
+        }
+        for (int i = 0; i < n; i++) {
+            g[i] = new int[size[i]];
+        }
+        for (int[] edge : edges) {
+            g[edge[0]][--size[edge[0]]] = edge[1];
+            g[edge[1]][--size[edge[1]]] = edge[0];
+        }
+        return g;
+    }
+
+    private static void initParents() {
+        parents = new int[h + 1][n];
+        parents[0] = parent;
+        for (int i = 1; i < h; i++) {
+            for (int u = 0; u < n; u++) {
+                final int nodeParent = parents[i - 1][u];
+                parents[i][u] = parents[i - 1][nodeParent];
+            }
+        }
+    }
+
+    private static boolean isAncestor(int u, int v) {
+        return in[u] <= in[v] && out[v] <= out[u];
+    }
+
+    private static int getLca(int u, int v) {
+        if (isAncestor(u, v)) { return u; }
+        if (isAncestor(v, u)) { return v; }
+        for (int i = h; i >= 0; i--) {
+            if (!isAncestor(parents[i][u], v)) {
+                u = parents[i][u];
+            }
+        }
+        return parents[0][u];
+    }
+
+    static final class Utils {
+        private static class Shuffler {
+            private static void shuffle(int[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void shuffle(long[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void swap(int[] x, int i, int j) {
+                final int t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+
+            private static void swap(long[] x, int i, int j) {
+                final long t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+
+        public static void shuffleSort(int[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        private static int[][] packG(int[][] edges, int n) {
+            final int[][] g = new int[n][];
+            final int[] size = new int[n];
+            for (int[] edge : edges) {
+                ++size[edge[0]];
+            }
+            for (int i = 0; i < n; i++) {
+                g[i] = new int[size[i]];
+            }
+            for (int[] edge : edges) {
+                g[edge[0]][--size[edge[0]]] = edge[1];
+            }
+            return g;
+        }
+
+        private static int[][][] packGW(int[][] edges, int n) {
+            final int[][][] g = new int[n][][];
+            final int[] size = new int[n];
+            for (int[] edge : edges) {
+                ++size[edge[0]];
+            }
+            for (int i = 0; i < n; i++) {
+                g[i] = new int[size[i]][2];
+            }
+            for (int[] edge : edges) {
+                g[edge[0]][--size[edge[0]]] = new int[] { edge[1], edge[2] };
+            }
+            return g;
+        }
+
+        private Utils() {}
+    }
+
+    static class FastReader {
+        private static final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            final byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextSign() throws IOException {
+            byte c = read();
+            while ('+' != c && '-' != c) {
+                c = read();
+            }
+            return '+' == c ? 0 : 1;
+        }
+
+        private static boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+
+        private int skip() throws IOException {
+            int b;
+            //noinspection StatementWithEmptyBody
+            while ((b = read()) != -1 && isSpaceChar(b)) {}
+            return b;
+        }
+
+        public char nc() throws IOException {
+            return (char) skip();
+        }
+
+        public String next() throws IOException {
+            int b = skip();
+            final StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b)) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = read();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) { buffer[0] = -1; }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) { fillBuffer(); }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
+        }
+    }
+}
---
+++ Java/cses/trees/SubtreeQueries.java
@@ -0,0 +1,363 @@
+package cses.trees;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
+public final class SubtreeQueries {
+
+    static int n;
+    static int q;
+    static int h;
+    static int time;
+    static int[][] edges;
+    static int[][] g;
+    static int[] parent;
+    static int[] depth;
+    static int[] in;
+    static int[] out;
+    static int[] add;
+    static int[] remove;
+    static int[] common;
+    static int[] res;
+
+    private static final class BIT {
+        private final int n;
+        private final long[] data;
+
+        private BIT(int n) {
+            this.n = n;
+            data = new long[n + 1];
+        }
+
+        public void add(int idx, long val) {
+            for (int i = idx + 1; i <= n; i += lsb(i)) {
+                data[i] += val;
+            }
+        }
+
+        public long sum(int l, int r) {
+            return sum(r - 1) - sum(l - 1);
+        }
+
+        private long sum(int idx) {
+            long res = 0;
+            for (int i = idx + 1; i > 0; i -= lsb(i)) {
+                res += data[i];
+            }
+            return res;
+        }
+
+        private static int lsb(int i) {
+            return i & -i;  // zeroes all the bits except the least significant one
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        final FastReader fs = new FastReader();
+        n = fs.nextInt();
+        q = fs.nextInt();
+        final int[] arr = new int[n];
+        final BIT bit = new BIT(n);
+        for (int i = 0; i < n; i++) {
+            arr[i] = fs.nextInt();
+        }
+        h = 18;
+        edges = new int[n - 1][2];
+        for (int i = 0; i < (n - 1); i++) {
+            edges[i] = new int[] { fs.nextInt() - 1, fs.nextInt() - 1 };
+        }
+        g = packG();
+        parent = new int[n];
+        depth = new int[n];
+        in = new int[n];
+        out = new int[n];
+        add = new int[n];
+        remove = new int[n];
+        common = new int[n];
+        res = new int[n];
+        dfs1();
+        dfs2();
+        for (int i = 0; i < n; i++) {
+            bit.add(in[i], arr[i]);
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < q; i++) {
+            final int type = fs.nextInt();
+            final int u = fs.nextInt() - 1;
+            if (type == 1) {
+                final int v = fs.nextInt();
+                final int diff = v - arr[u];
+                arr[u] = v;
+                bit.add(in[u], diff);
+            } else {
+                sb.append(bit.sum(in[u], out[u]));
+                sb.append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs1() {
+        final int[] stack = new int[n];
+        int stackIdx = 0;
+        stack[stackIdx++] = 0;
+        parent[0] = 0;
+        depth[0] = 0;
+        while (stackIdx > 0) {
+            final int u = stack[--stackIdx];
+            depth[u] = depth[parent[u]] + 1;
+            for (int v : g[u]) {
+                if (v != parent[u]) {
+                    parent[v] = u;
+                    stack[stackIdx++] = v;
+                }
+            }
+        }
+    }
+
+    private static void dfs2() {
+        final int[] stack = new int[n];
+        final int[] idx = new int[n];
+        int stackIdx = 0;
+        stack[stackIdx++] = 0;
+        while (stackIdx > 0) {
+            final int u = stack[stackIdx - 1];
+            if (idx[u] == 0) {
+                in[u] = time++;
+            }
+            if (idx[u] < g[u].length) {
+                final int v = g[u][idx[u]++];
+                if (parent[u] != v) {
+                    stack[stackIdx++] = v;
+                }
+            } else {
+                out[u] = time;
+                stackIdx--;
+            }
+        }
+    }
+
+    private static int[][] packG() {
+        final int[][] g = new int[n][];
+        final int[] size = new int[n];
+        for (int[] edge : edges) {
+            ++size[edge[0]];
+            ++size[edge[1]];
+        }
+        for (int i = 0; i < n; i++) {
+            g[i] = new int[size[i]];
+        }
+        for (int[] edge : edges) {
+            g[edge[0]][--size[edge[0]]] = edge[1];
+            g[edge[1]][--size[edge[1]]] = edge[0];
+        }
+        return g;
+    }
+
+    static final class Utils {
+        private static class Shuffler {
+            private static void shuffle(int[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void shuffle(long[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void swap(int[] x, int i, int j) {
+                final int t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+
+            private static void swap(long[] x, int i, int j) {
+                final long t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+
+        public static void shuffleSort(int[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        private static int[][] packG(int[][] edges, int n) {
+            final int[][] g = new int[n][];
+            final int[] size = new int[n];
+            for (int[] edge : edges) {
+                ++size[edge[0]];
+            }
+            for (int i = 0; i < n; i++) {
+                g[i] = new int[size[i]];
+            }
+            for (int[] edge : edges) {
+                g[edge[0]][--size[edge[0]]] = edge[1];
+            }
+            return g;
+        }
+
+        private static int[][][] packGW(int[][] edges, int n) {
+            final int[][][] g = new int[n][][];
+            final int[] size = new int[n];
+            for (int[] edge : edges) {
+                ++size[edge[0]];
+            }
+            for (int i = 0; i < n; i++) {
+                g[i] = new int[size[i]][2];
+            }
+            for (int[] edge : edges) {
+                g[edge[0]][--size[edge[0]]] = new int[] { edge[1], edge[2] };
+            }
+            return g;
+        }
+
+        private Utils() {}
+    }
+
+    static class FastReader {
+        private static final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            final byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextSign() throws IOException {
+            byte c = read();
+            while ('+' != c && '-' != c) {
+                c = read();
+            }
+            return '+' == c ? 0 : 1;
+        }
+
+        private static boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+
+        private int skip() throws IOException {
+            int b;
+            //noinspection StatementWithEmptyBody
+            while ((b = read()) != -1 && isSpaceChar(b)) {}
+            return b;
+        }
+
+        public char nc() throws IOException {
+            return (char) skip();
+        }
+
+        public String next() throws IOException {
+            int b = skip();
+            final StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b)) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = read();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) { buffer[0] = -1; }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) { fillBuffer(); }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
+        }
+    }
+}
