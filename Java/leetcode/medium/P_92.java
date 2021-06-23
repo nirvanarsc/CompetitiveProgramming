e4822d55abbbe152bfeb8166867bcc8be19e8d93
nirvanarsc <nirvana_rsc@yahoo.com>
2021-06-23 23:59:17 +0900

    Running on empty - food review! part 364

--- Java/leetcode/medium/P_92.java
+++ Java/leetcode/medium/P_92.java
@@ -4,6 +4,36 @@ import utils.DataStructures.ListNode;
 
 public class P_92 {
 
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode iter1 = new ListNode(-1);
+        final ListNode res = iter1;
+        iter1.next = head;
+        for (int i = 0; i < left; i++) {
+            iter1 = iter1.next;
+        }
+        ListNode iter2 = iter1;
+        for (int i = 0; i < right - left + 1; i++) {
+            iter2 = iter2.next;
+        }
+        final ListNode after = iter2.next;
+        // cut
+        iter2.next = null;
+        final ListNode revStart = iter1.next;
+        iter1.next = reverse(revStart, null);
+        revStart.next = after;
+        return res.next;
+    }
+
+    private static ListNode reverse(ListNode curr, ListNode tail) {
+        if (curr == null) {
+            return tail;
+        }
+        final ListNode next = curr.next;
+        curr.next = tail;
+        return reverse(next, curr);
+    }
+
+
     public ListNode reverseBetween(ListNode head, int m, int n) {
         final ListNode dummy = new ListNode(-1);
         dummy.next = head;
