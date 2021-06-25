ee753c59195b388f8043faefdf3d759671e8b96f
nirvanarsc <nirvana_rsc@yahoo.com>
2021-06-25 18:19:02 +0900

    Running on empty - food review! part 366

--- Java/leetcode/weekly_contests/weekly_51/P_684.java
+++ Java/leetcode/weekly_contests/weekly_51/P_684.java
@@ -1,19 +1,67 @@
 package leetcode.weekly_contests.weekly_51;
 
-import utils.DataStructures.UnionFind;
-
+@SuppressWarnings({ "NullThrown", "ConstantConditions" })
 public class P_684 {
 
-    @SuppressWarnings({ "ConstantConditions", "ReturnOfNull" })
-    public int[] findRedundantConnection(int[][] edges) {
-        final UnionFind uf = new UnionFind(edges.length + 1);
-        for (int[] e : edges) {
-            if (uf.find(e[0]) != uf.find(e[1])) {
-                uf.union(e[0], e[1]);
-            } else {
-                return e;
+    private static final class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+        private int count;
+
+        private UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
-        return null;
+
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            final int rootP = find(p);
+            final int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+                size[rootQ] = 0;
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+                size[rootP] = 0;
+            }
+            count--;
+        }
+
+        public int count() { return count; }
+
+        public int[] size() { return size; }
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        final int n = edges.length;
+        final UnionFind uf = new UnionFind(n);
+        for (int[] e : edges) {
+            final int u = e[0] - 1;
+            final int v = e[1] - 1;
+            if (uf.find(u) == uf.find(v)) {
+                return e;
+            }
+            uf.union(u, v);
+        }
+        throw null;
     }
 }
