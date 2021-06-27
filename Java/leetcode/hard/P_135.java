b95cc50575d941b487dd9d13eccbc3a4092a383e
nirvanarsc <nirvana_rsc@yahoo.com>
2021-06-27 18:12:08 +0900

    Running on empty - food review! part 368

--- Java/leetcode/hard/P_135.java
+++ Java/leetcode/hard/P_135.java
@@ -1,24 +1,21 @@
 package leetcode.hard;
 
-import java.util.Arrays;
-
 public class P_135 {
 
     public int candy(int[] ratings) {
         final int n = ratings.length;
         final int[] res = new int[n];
-        Arrays.fill(res, 1);
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                res[i] = res[i - 1] + 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (ratings[i] < ratings[i + 1]) {
+                res[i + 1] = res[i] + 1;
             }
         }
         for (int i = n - 1; i > 0; i--) {
             if (ratings[i - 1] > ratings[i]) {
-                res[i - 1] = Math.max(res[i] + 1, res[i - 1]);
+                res[i - 1] = Math.max(res[i - 1], res[i] + 1);
             }
         }
-        int sum = 0;
+        int sum = n;
         for (int candy : res) {
             sum += candy;
         }
