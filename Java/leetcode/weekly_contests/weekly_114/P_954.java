2bf90b1a21199e12134865b55827bddad834c093
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-11 21:06:39 +0900

    Running on empty - food review! part 413

--- Java/leetcode/weekly_contests/weekly_114/P_954.java
+++ Java/leetcode/weekly_contests/weekly_114/P_954.java
@@ -1,44 +1,36 @@
 package leetcode.weekly_contests.weekly_114;
 
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_954 {
 
-    public boolean canReorderDoubled(int[] A) {
-        for (int i = 0; i < A.length; i++) {
-            A[i] = Math.abs(A[i]);
+    public boolean canReorderDoubled(int[] arr) {
+        final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(Math::abs));
+        final Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : arr) {
+            freq.merge(num, 1, Integer::sum);
+            pq.offer(num);
         }
-        Arrays.sort(A);
-        final int n = A.length;
-        final boolean[] paired = new boolean[n];
-        for (int i = 0, j = 1; i < n; i++) {
-            if (!paired[i]) {
-                while (j < n && A[j] != 2 * A[i]) { j++; }
-                if (j == n) { return false; }
-                paired[j++] = true;
+        while (!pq.isEmpty()) {
+            final int num = pq.remove();
+            if (!freq.containsKey(num)) {
+                continue;
+            }
+            final int l = freq.merge(num, -1, Integer::sum);
+            final int f = freq.merge(num * 2, -1, Integer::sum);
+            if (f < 0) {
+                return false;
+            }
+            if (f == 0) {
+                freq.remove(num * 2);
+            }
+            if (l == 0) {
+                freq.remove(num);
             }
         }
-        return true;
-    }
-
-    public boolean canReorderDoubledTreeMap(int[] A) {
-        final Map<Integer, Integer> count = new TreeMap<>();
-        for (int num : A) {
-            count.merge(num, 1, Integer::sum);
-        }
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            final int curr = entry.getKey();
-            if (entry.getValue() != 0) {
-                final int target = curr < 0 ? curr / 2 : curr * 2;
-                if (curr < 0 && curr % 2 != 0 || entry.getValue() > count.getOrDefault(target, 0)) {
-                    return false;
-                }
-                count.put(target, count.get(target) - entry.getValue());
-            }
-        }
-        return true;
+        return freq.isEmpty();
     }
 }
