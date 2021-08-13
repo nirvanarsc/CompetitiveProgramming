c298b0164dddcc9f8ec1d7256dd4784c0ce68e88
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-13 23:29:22 +0900

    Running on empty - food review! part 415

--- Java/leetcode/medium/P_73.java
+++ Java/leetcode/medium/P_73.java
@@ -5,22 +5,20 @@ public class P_73 {
     public void setZeroes(int[][] matrix) {
         final int n = matrix.length;
         final int m = matrix[0].length;
-        boolean firstRow = false, firstCol = false;
-
+        boolean firstRow = false;
+        boolean firstCol = false;
         for (int[] ints : matrix) {
             if (ints[0] == 0) {
                 firstCol = true;
                 break;
             }
         }
-
         for (int i = 0; i < m; i++) {
             if (matrix[0][i] == 0) {
                 firstRow = true;
                 break;
             }
         }
-
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (matrix[i][j] == 0) {
@@ -29,31 +27,28 @@ public class P_73 {
                 }
             }
         }
-
         for (int i = 1; i < n; i++) {
             if (matrix[i][0] == 0) {
-                nullifyRow(matrix, i);
+                nullifyRow(matrix, i, m);
             }
         }
-
         for (int j = 1; j < m; j++) {
             if (matrix[0][j] == 0) {
-                nullifyCol(matrix, j);
+                nullifyCol(matrix, j, n);
             }
         }
-
-        if (firstRow) { nullifyRow(matrix, 0); }
-        if (firstCol) { nullifyCol(matrix, 0); }
+        if (firstRow) { nullifyRow(matrix, 0, m); }
+        if (firstCol) { nullifyCol(matrix, 0, n); }
     }
 
-    private static void nullifyRow(int[][] matrix, int row) {
-        for (int i = 0; i < matrix[0].length; i++) {
+    private static void nullifyRow(int[][] matrix, int row, int m) {
+        for (int i = 0; i < m; i++) {
             matrix[row][i] = 0;
         }
     }
 
-    private static void nullifyCol(int[][] matrix, int col) {
-        for (int i = 0; i < matrix.length; i++) {
+    private static void nullifyCol(int[][] matrix, int col, int n) {
+        for (int i = 0; i < n; i++) {
             matrix[i][col] = 0;
         }
     }
