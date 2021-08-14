5d4201c0dbc10b4f6d9a322748e2cb3e18225993
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-14 23:51:10 +0900

    Running on empty - food review! part 416

--- Java/leetcode/weekly_contests/weekly_25/P_546.java
+++ Java/leetcode/weekly_contests/weekly_25/P_546.java
@@ -2,53 +2,35 @@ package leetcode.weekly_contests.weekly_25;
 
 public class P_546 {
 
+    static int[][][] dp;
+    static boolean[][][] seen;
+
     public int removeBoxes(int[] boxes) {
         final int n = boxes.length;
-        return left(boxes, 0, n - 1, 1, new Integer[n][n][n + 1]);
+        dp = new int[n][n][n + 1];
+        seen = new boolean[n][n][n + 1];
+        return dfs(boxes, 0, n - 1, 0);
     }
 
-    private static int left(int[] s, int i, int j, int k, Integer[][][] dp) {
-        if (i > j) {
+    private static int dfs(int[] boxes, int l, int r, int k) {
+        if (l > r) {
             return 0;
         }
-        if (dp[i][j][k] != null) {
-            return dp[i][j][k];
-        }
-        while (i < j && s[i + 1] == s[i]) {
-            i++;
+        // important to do before checking cache
+        while (r > l && boxes[r] == boxes[r - 1]) {
+            r--;
             k++;
         }
-        int res = k * k + left(s, i + 1, j, 1, dp);
-        for (int m = i + 1; m <= j; m++) {
-            if (s[m] == s[i]) {
-                res = Math.max(res, left(s, i + 1, m - 1, 1, dp) + left(s, m, j, k + 1, dp));
+        if (seen[l][r][k]) {
+            return dp[l][r][k];
+        }
+        int res = (k + 1) * (k + 1) + dfs(boxes, l, r - 1, 0);
+        for (int mid = l; mid < r; mid++) {
+            if (boxes[mid] == boxes[r]) {
+                res = Math.max(res, dfs(boxes, l, mid, k + 1) + dfs(boxes, mid + 1, r - 1, 0));
             }
         }
-        return dp[i][j][k] = res;
-    }
-
-    public int removeBoxesRight(int[] boxes) {
-        final int n = boxes.length;
-        return right(boxes, 0, n - 1, 1, new Integer[n][n][n + 1]);
-    }
-
-    private static int right(int[] s, int i, int j, int k, Integer[][][] dp) {
-        if (i > j) {
-            return 0;
-        }
-        if (dp[i][j][k] != null) {
-            return dp[i][j][k];
-        }
-        while (i < j && s[j - 1] == s[j]) {
-            j--;
-            k++;
-        }
-        int res = k * k + right(s, i, j - 1, 1, dp);
-        for (int m = i; m < j; m++) {
-            if (s[m] == s[j]) {
-                res = Math.max(res, right(s, i, m, k + 1, dp) + right(s, m + 1, j - 1, 1, dp));
-            }
-        }
-        return dp[i][j][k] = res;
+        seen[l][r][k] = true;
+        return dp[l][r][k] = res;
     }
 }
