360bb10e42ae26b86f123730e200d9744ec366b4
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-16 23:15:04 +0900

    Running on empty - food review! part 418

---
+++ Java/codeforces/round_700_749/round_738/D.java
@@ -0,0 +1,175 @@
+package codeforces.round_700_749.round_738;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public final class D {
+
+    private static final class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+        private int count;
+
+        private UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            final int rootP = find(p);
+            final int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+                size[rootQ] = 0;
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+                size[rootP] = 0;
+            }
+            count--;
+        }
+
+        public int count() { return count; }
+
+        public int[] size() { return size; }
+    }
+
+    public static void main(String[] args) {
+        final FastScanner fs = new FastScanner();
+        final int n = fs.nextInt();
+        final UnionFind uf1 = new UnionFind(n);
+        final UnionFind uf2 = new UnionFind(n);
+        final int m1 = fs.nextInt();
+        final int m2 = fs.nextInt();
+        for (int i = 0; i < m1; i++) {
+            final int u = fs.nextInt() - 1;
+            final int v = fs.nextInt() - 1;
+            uf1.union(u, v);
+        }
+        for (int i = 0; i < m2; i++) {
+            final int u = fs.nextInt() - 1;
+            final int v = fs.nextInt() - 1;
+            uf2.union(u, v);
+        }
+        int count = 0;
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (uf1.find(i) != uf1.find(j) && uf2.find(i) != uf2.find(j)) {
+                    count++;
+                    sb.append(i + 1).append(' ').append(j + 1).append('\n');
+                    uf1.union(i, j);
+                    uf2.union(i, j);
+                }
+            }
+        }
+        System.out.println(count);
+        System.out.println(sb);
+    }
+
+    static final class Utils {
+        private static class Shuffler {
+            private static void shuffle(int[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void shuffle(long[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void swap(int[] x, int i, int j) {
+                final int t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+
+            private static void swap(long[] x, int i, int j) {
+                final long t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+
+        public static void shuffleSort(int[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        private Utils() {}
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        private String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    //noinspection CallToPrintStackTrace
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] nextIntArray(int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) { a[i] = nextInt(); }
+            return a;
+        }
+
+        long[] nextLongArray(int n) {
+            final long[] a = new long[n];
+            for (int i = 0; i < n; i++) { a[i] = nextLong(); }
+            return a;
+        }
+    }
+}
--- Java/leetcode/easy/P_303.java
+++ Java/leetcode/easy/P_303.java
@@ -1,26 +1,22 @@
 package leetcode.easy;
 
+@SuppressWarnings({ "InnerClassMayBeStatic", "PublicConstructorInNonPublicClass", "unused" })
 public class P_303 {
 
     class NumArray {
-        int[] dp;
 
-        NumArray(int[] nums) {
-            if (nums.length > 0) {
-                dp = new int[nums.length];
-                dp[0] = nums[0];
-                for (int i = 1; i < nums.length; i++) {
-                    dp[i] = nums[i] + dp[i - 1];
-                }
+        int[] pre;
+
+        public NumArray(int[] nums) {
+            final int n = nums.length;
+            pre = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                pre[i] = pre[i - 1] + nums[i - 1];
             }
         }
 
-        public int sumRange(int i, int j) {
-            if (i == 0) {
-                return dp[j];
-            } else {
-                return dp[j] - dp[i - 1];
-            }
+        public int sumRange(int left, int right) {
+            return pre[right + 1] - pre[left];
         }
     }
 }
