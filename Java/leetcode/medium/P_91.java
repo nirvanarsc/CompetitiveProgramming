5aa9d0d337ff83ba98c9e12b8885e202c108687b
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-18 23:29:44 +0900

    Running on empty - food review! part 420

--- Java/leetcode/medium/P_91.java
+++ Java/leetcode/medium/P_91.java
@@ -2,29 +2,37 @@ package leetcode.medium;
 
 public class P_91 {
 
+    static int[] dp;
+    static boolean[] seen;
+    static int n;
+
     public int numDecodings(String s) {
-        return dfs(s.toCharArray(), 0, new Integer[s.length()]);
+        n = s.length();
+        dp = new int[n];
+        seen = new boolean[n];
+        return dfs(s.toCharArray(), 0);
     }
 
-    private static int dfs(char[] s, int idx, Integer[] dp) {
-        if (idx == s.length) {
+    private static int dfs(char[] s, int idx) {
+        if (idx == n) {
             return 1;
         }
-        if (dp[idx] != null) {
+        if (seen[idx]) {
             return dp[idx];
         }
         int res = 0;
         if (s[idx] > '0') {
-            res += dfs(s, idx + 1, dp);
+            res += dfs(s, idx + 1);
         }
-        if (idx < s.length - 1) {
+        if (idx < n - 1) {
             if (s[idx] == '2' && s[idx + 1] <= '6') {
-                res += dfs(s, idx + 2, dp);
+                res += dfs(s, idx + 2);
             }
             if (s[idx] == '1') {
-                res += dfs(s, idx + 2, dp);
+                res += dfs(s, idx + 2);
             }
         }
+        seen[idx] = true;
         return dp[idx] = res;
     }
 }
