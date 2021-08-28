20085c44039ea776ba26a915e5c4e3ce8f8dd8ed
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-28 20:16:45 +0900

    Running on empty - food review! part 430

--- Java/leetcode/weekly_contests/weekly_159/P_1235.java
+++ Java/leetcode/weekly_contests/weekly_159/P_1235.java
@@ -9,6 +9,50 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class P_1235 {
 
+    static boolean[] seen;
+    static int[] dp;
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        final int n = startTime.length;
+        seen = new boolean[n];
+        dp = new int[n];
+        final int[][] indexed = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            indexed[i] = new int[] { startTime[i], endTime[i], profit[i] };
+        }
+        Arrays.sort(indexed, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1])
+                                                    : Integer.compare(a[0], b[0]));
+        return dfs(indexed, 0);
+    }
+
+    private static int dfs(int[][] arr, int idx) {
+        if (idx == arr.length) {
+            return 0;
+        }
+        if (seen[idx]) {
+            return dp[idx];
+        }
+        int res = 0;
+        res = Math.max(res, arr[idx][2] + dfs(arr, lowerBound(arr, arr[idx][1])));
+        res = Math.max(res, dfs(arr, idx + 1));
+        seen[idx] = true;
+        return dp[idx] = res;
+    }
+
+    private static int lowerBound(int[][] arr, int target) {
+        int lo = 0;
+        int hi = arr.length;
+        while (lo < hi) {
+            final int mid = lo + hi >>> 1;
+            if (arr[mid][0] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+
     public int jobSchedulingBottomUp(int[] startTime, int[] endTime, int[] profit) {
         final int[][] items = new int[startTime.length][3];
         for (int i = 0; i < startTime.length; i++) {
