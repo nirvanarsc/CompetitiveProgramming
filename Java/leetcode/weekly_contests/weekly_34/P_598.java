a028434c78dd5a5c6b97bfe91e2e72e5d2067599
nirvanarsc <nirvana_rsc@yahoo.com>
2021-08-30 22:40:56 +0200

    Running on empty - food review! part 432

--- Java/leetcode/weekly_contests/weekly_34/P_598.java
+++ Java/leetcode/weekly_contests/weekly_34/P_598.java
@@ -3,10 +3,12 @@ package leetcode.weekly_contests.weekly_34;
 public class P_598 {
 
     public int maxCount(int m, int n, int[][] ops) {
+        int minX = m;
+        int minY = n;
         for (int[] op : ops) {
-            m = Math.min(m, op[0]);
-            n = Math.min(n, op[1]);
+            minX = Math.min(minX, op[0]);
+            minY = Math.min(minY, op[1]);
         }
-        return m * n;
+        return minX * minY;
     }
 }
