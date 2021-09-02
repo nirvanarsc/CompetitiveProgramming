06e7a3f1727dc2270fc7f2b90470e1d50d1c90e5
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-02 20:30:09 +0200

    Running on empty - food review! part 435

--- Java/leetcode/medium/P_95.java
+++ Java/leetcode/medium/P_95.java
@@ -9,18 +9,17 @@ import utils.DataStructures.TreeNode;
 public class P_95 {
 
     public List<TreeNode> generateTrees(int n) {
-        return n > 0 ? recurse(1, n) : Collections.emptyList();
+        return dfs(1, n);
     }
 
-    public List<TreeNode> recurse(int start, int n) {
-        final List<TreeNode> res = new ArrayList<>();
+    public List<TreeNode> dfs(int start, int n) {
         if (n == 0) {
-            res.add(null);
+            return Collections.singletonList(null);
         }
-
+        final List<TreeNode> res = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            final List<TreeNode> leftNodes = recurse(start, i);
-            final List<TreeNode> rightNodes = recurse(start + i + 1, n - 1 - i);
+            final List<TreeNode> leftNodes = dfs(start, i);
+            final List<TreeNode> rightNodes = dfs(start + i + 1, n - 1 - i);
             for (TreeNode left : leftNodes) {
                 for (TreeNode right : rightNodes) {
                     final TreeNode treeNode = new TreeNode(start + i);
@@ -30,7 +29,6 @@ public class P_95 {
                 }
             }
         }
-
         return res;
     }
 }
