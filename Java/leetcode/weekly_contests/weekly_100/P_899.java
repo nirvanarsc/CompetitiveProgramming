0c3932f240f827c05aa904ee8de0cf58b5086422
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-05 22:39:59 +0200

    Running on empty - food review! part 438

--- Java/leetcode/weekly_contests/weekly_100/P_899.java
+++ Java/leetcode/weekly_contests/weekly_100/P_899.java
@@ -4,16 +4,15 @@ import java.util.Arrays;
 
 public class P_899 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public String orderlyQueue(String S, int K) {
-        if (K > 1) {
-            final char[] chars = S.toCharArray();
+    public String orderlyQueue(String s, int k) {
+        if (k > 1) {
+            final char[] chars = s.toCharArray();
             Arrays.sort(chars);
             return new String(chars);
         }
-        String res = S;
-        for (int i = 1; i < S.length(); i++) {
-            final String tmp = S.substring(i) + S.substring(0, i);
+        String res = s;
+        for (int i = 1; i < s.length(); i++) {
+            final String tmp = s.substring(i) + s.substring(0, i);
             if (res.compareTo(tmp) > 0) {
                 res = tmp;
             }
