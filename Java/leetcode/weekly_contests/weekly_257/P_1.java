9883b736d874f168ac39a6f74cecc5aa646bc47e
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-06 23:40:32 +0200

    Running on empty - food review! part 439

---
+++ Java/leetcode/weekly_contests/weekly_257/P_1.java
@@ -0,0 +1,21 @@
+package leetcode.weekly_contests.weekly_257;
+
+public class P_1 {
+
+    public int countQuadruplets(int[] nums) {
+        int res = 0;
+        final int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        if (nums[i] + nums[j] + nums[k] == nums[l]) {
+                            res++;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
