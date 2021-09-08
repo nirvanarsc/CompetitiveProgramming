9417a61d4e9ef07f19a0b747006cd7791f3dac18
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-08 22:31:17 +0200

    Running on empty - food review! part 441

--- Java/leetcode/weekly_contests/weekly_88/P_848.java
+++ Java/leetcode/weekly_contests/weekly_88/P_848.java
@@ -2,12 +2,12 @@ package leetcode.weekly_contests.weekly_88;
 
 public class P_848 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public String shiftingLetters(String S, int[] shifts) {
-        final char[] chars = S.toCharArray();
+    public String shiftingLetters(String s, int[] shifts) {
+        final char[] chars = s.toCharArray();
+        final int n = s.length();
         int sum = 0;
-        for (int i = shifts.length - 1; i >= 0; i--) {
-            sum += shifts[i] % 26;
+        for (int i = n - 1; i >= 0; i--) {
+            sum = (sum + shifts[i]) % 26;
             chars[i] = (char) ((chars[i] - 'a' + sum) % 26 + 'a');
         }
         return new String(chars);
