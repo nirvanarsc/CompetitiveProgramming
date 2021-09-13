555662d3f4ae44389985389d650efbb49773286b
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-13 22:39:59 +0200

    Running on empty - food review! part 446

--- Java/leetcode/weekly_contests/weekly_154/P_1189.java
+++ Java/leetcode/weekly_contests/weekly_154/P_1189.java
@@ -3,20 +3,16 @@ package leetcode.weekly_contests.weekly_154;
 public class P_1189 {
 
     public int maxNumberOfBalloons(String text) {
-        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        final int[] f = new int[26];
         for (char c : text.toCharArray()) {
-            if (c == 'b') { b++; }
-            if (c == 'a') { a++; }
-            if (c == 'l') { l++; }
-            if (c == 'o') { o++; }
-            if (c == 'n') { n++; }
+            f[c - 'a']++;
         }
-        int res = Integer.MAX_VALUE;
-        res = Math.min(res, b);
-        res = Math.min(res, a);
-        res = Math.min(res, l / 2);
-        res = Math.min(res, o / 2);
-        res = Math.min(res, n);
+        f['l' - 'a'] /= 2;
+        f['o' - 'a'] /= 2;
+        int res = (int) 1e9;
+        for (char c : "balloon".toCharArray()) {
+            res = Math.min(res, f[c - 'a']);
+        }
         return res;
     }
 }
