4fbee72041d9718c8629433567de24c18bb34b14
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-15 22:39:59 +0200

    Running on empty - food review! part 448

--- Java/leetcode/weekly_contests/weekly_120/P_978.java
+++ Java/leetcode/weekly_contests/weekly_120/P_978.java
@@ -2,18 +2,35 @@ package leetcode.weekly_contests.weekly_120;
 
 public class P_978 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public int maxTurbulenceSize(int[] A) {
-        final int[][] dp = new int[A.length][2];
-        int maxLen = 0;
-        for (int i = 1; i < A.length; i++) {
-            if (A[i - 1] < A[i]) {
+    public int maxTurbulenceSizeBottomUp(int[] arr) {
+        final int n = arr.length;
+        final int[][] dp = new int[n][2];
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] < arr[i]) {
                 dp[i][0] = dp[i - 1][1] + 1;
-            } else if (A[i - 1] > A[i]) {
+            } else if (arr[i - 1] > arr[i]) {
                 dp[i][1] = dp[i - 1][0] + 1;
             }
-            maxLen = Math.max(maxLen, Math.max(dp[i][0], dp[i][1]));
+            res = Math.max(res, dp[i][0]);
+            res = Math.max(res, dp[i][1]);
         }
-        return maxLen + 1;
+        return res + 1;
+    }
+
+    public int maxTurbulenceSize(int[] arr) {
+        final int n = arr.length;
+        int res = 1;
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+            final int sign = Integer.compare(arr[i - 1], arr[i]);
+            if (sign == 0) {
+                j = i;
+            } else if (i == n - 1 || sign * Integer.compare(arr[i], arr[i + 1]) != -1) {
+                res = Math.max(res, i - j + 1);
+                j = i;
+            }
+        }
+        return res;
     }
 }
