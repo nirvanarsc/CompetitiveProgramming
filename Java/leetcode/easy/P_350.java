b6343e8725644d346b7237818e2b637f02d83234
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-17 21:32:37 +0300

    Running on empty - food review! part 450

--- Java/leetcode/easy/P_350.java
+++ Java/leetcode/easy/P_350.java
@@ -1,30 +1,25 @@
 package leetcode.easy;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class P_350 {
 
-    @SuppressWarnings("TailRecursion")
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return intersect(nums2, nums1);
-        }
-        final Map<Integer, Integer> freq = new HashMap<>();
+        final int[] f1 = new int[1005];
+        final int[] f2 = new int[1005];
         for (int num : nums1) {
-            freq.merge(num, 1, Integer::sum);
+            f1[num]++;
         }
-        int idx = 0;
-        for (int i = 0; i < nums2.length; i++) {
-            if (freq.containsKey(nums2[i])) {
-                if (freq.merge(nums2[i], -1, Integer::sum) == 0) {
-                    freq.remove(nums2[i]);
-                }
-                nums2[idx] = nums2[i];
-                idx++;
+        for (int num : nums2) {
+            f2[num]++;
+        }
+        final List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < 1005; i++) {
+            for (int count = 0; count < Math.min(f1[i], f2[i]); count++) {
+                res.add(i);
             }
         }
-        return Arrays.copyOfRange(nums2, 0, idx);
+        return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
