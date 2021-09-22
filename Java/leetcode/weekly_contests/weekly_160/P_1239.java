0cb1e76f2136351c30f82dd424ce78478b7ed6fb
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-22 18:49:51 +0300

    Running on empty - food review! part 455

--- Java/leetcode/weekly_contests/weekly_160/P_1239.java
+++ Java/leetcode/weekly_contests/weekly_160/P_1239.java
@@ -4,33 +4,43 @@ import java.util.List;
 
 public class P_1239 {
 
+    static int[] masks;
+    static int n;
+
     public int maxLength(List<String> arr) {
-        final int[] res = { 0 };
-        recurse(arr, 0, "", res);
-        return res[0];
-    }
-
-    private static void recurse(List<String> arr, int i, String s, int[] res) {
-        if (i == arr.size()) {
-            return;
-        }
-
-        for (int k = i; k < arr.size(); k++) {
-            if (isUnique(s + arr.get(k))) {
-                res[0] = Math.max(res[0], (s + arr.get(k)).length());
-                recurse(arr, k + 1, s + arr.get(k), res);
+        n = arr.size();
+        masks = new int[n];
+        for (int i = 0; i < n; i++) {
+            boolean ok = true;
+            int mask = 0;
+            for (char c : arr.get(i).toCharArray()) {
+                if ((mask & (1 << (c - 'a'))) != 0) {
+                    ok = false;
+                    break;
+                }
+                mask |= 1 << (c - 'a');
+            }
+            if (ok) {
+                masks[i] = mask;
             }
         }
-    }
-
-    private static boolean isUnique(String s) {
-        int map = 0;
-        for (char c : s.toCharArray()) {
-            if ((map & (1 << (c - 'a'))) != 0) {
-                return false;
+        int res = 0;
+        for (int mask = 0; mask < (1 << n); mask++) {
+            boolean ok = true;
+            int curr = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    if ((curr & masks[i]) != 0) {
+                        ok = false;
+                        break;
+                    }
+                    curr |= masks[i];
+                }
             }
-            map |= 1 << (c - 'a');
+            if (ok) {
+                res = Math.max(res, Integer.bitCount(curr));
+            }
         }
-        return true;
+        return res;
     }
 }
