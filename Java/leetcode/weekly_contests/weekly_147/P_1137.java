e8907041318a426a921a9296b4b166ad52bae8ce
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-24 19:48:28 +0300

    Running on empty - food review! part 457

--- Java/leetcode/weekly_contests/weekly_147/P_1137.java
+++ Java/leetcode/weekly_contests/weekly_147/P_1137.java
@@ -3,16 +3,11 @@ package leetcode.weekly_contests.weekly_147;
 public class P_1137 {
 
     public int tribonacci(int n) {
-        if (n < 2) { return n; }
-        int a = 0;
-        int b = 1;
-        int c = 1;
-        for (int i = 3; i <= n; i++) {
-            final int d = a + b + c;
-            a = b;
-            b = c;
-            c = d;
+        final int[] dp = new int[100];
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
         }
-        return c;
+        return dp[n];
     }
 }
