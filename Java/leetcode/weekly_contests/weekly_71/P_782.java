732915e13f4e08db22bdb244b6a1e0d953194091
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-26 22:39:59 +0200

    Running on empty - food review! part 459

--- Java/leetcode/weekly_contests/weekly_71/P_782.java
+++ Java/leetcode/weekly_contests/weekly_71/P_782.java
@@ -2,42 +2,45 @@ package leetcode.weekly_contests.weekly_71;
 
 public class P_782 {
 
-    public int movesToChessboard(int[][] b) {
-        final int N = b.length;
-        int rowSum = 0, colSum = 0, rowSwap = 0, colSwap = 0;
-        for (int[] row : b) {
-            for (int j = 0; j < N; ++j) {
-                if ((b[0][0] ^ row[0] ^ b[0][j] ^ row[j]) == 1) {
+    public int movesToChessboard(int[][] board) {
+        final int n = board.length;
+        int rowSum = 0;
+        int colSum = 0;
+        int rowSwap = 0;
+        int colSwap = 0;
+        for (int[] row : board) {
+            for (int j = 0; j < n; j++) {
+                if ((board[0][0] ^ row[0] ^ board[0][j] ^ row[j]) == 1) {
                     return -1;
                 }
             }
         }
-        for (int i = 0; i < N; ++i) {
-            rowSum += b[0][i];
-            colSum += b[i][0];
-            if (b[i][0] == i % 2) {
+        for (int i = 0; i < n; i++) {
+            rowSum += board[0][i];
+            colSum += board[i][0];
+            if (board[i][0] == i % 2) {
                 rowSwap++;
             }
-            if (b[0][i] == i % 2) {
+            if (board[0][i] == i % 2) {
                 colSwap++;
             }
         }
-        if (rowSum != N / 2 && rowSum != (N + 1) / 2) {
+        if (rowSum != n / 2 && rowSum != (n + 1) / 2) {
             return -1;
         }
-        if (colSum != N / 2 && colSum != (N + 1) / 2) {
+        if (colSum != n / 2 && colSum != (n + 1) / 2) {
             return -1;
         }
-        if (N % 2 == 1) {
+        if (n % 2 == 1) {
             if (colSwap % 2 != 0) {
-                colSwap = N - colSwap;
+                colSwap = n - colSwap;
             }
             if (rowSwap % 2 != 0) {
-                rowSwap = N - rowSwap;
+                rowSwap = n - rowSwap;
             }
         } else {
-            colSwap = Math.min(N - colSwap, colSwap);
-            rowSwap = Math.min(N - rowSwap, rowSwap);
+            colSwap = Math.min(n - colSwap, colSwap);
+            rowSwap = Math.min(n - rowSwap, rowSwap);
         }
         return (colSwap + rowSwap) / 2;
     }
