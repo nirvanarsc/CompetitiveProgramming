878a5a4c6e67da5287430e0e9bec24caea3ef683
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-27 22:59:24 +0200

    Running on empty - food review! part 460

--- Java/leetcode/weekly_contests/weekly_108/P_929.java
+++ Java/leetcode/weekly_contests/weekly_108/P_929.java
@@ -3,19 +3,19 @@ package leetcode.weekly_contests.weekly_108;
 import java.util.HashSet;
 import java.util.Set;
 
+@SuppressWarnings("DynamicRegexReplaceableByCompiledPattern")
 public class P_929 {
 
-    @SuppressWarnings("DynamicRegexReplaceableByCompiledPattern")
     public int numUniqueEmails(String[] emails) {
         final Set<String> unique = new HashSet<>();
         for (String e : emails) {
             final String[] split = e.split("@");
             String localName = split[0].replaceAll("\\.", "");
-            final String domain = split[1];
-            if (localName.indexOf('+') > 0) {
-                localName = localName.substring(0, localName.indexOf('+'));
+            final int idx = localName.indexOf('+');
+            if (idx > 0) {
+                localName = localName.substring(0, idx);
             }
-            unique.add(localName + '@' + domain);
+            unique.add(localName + '@' + split[1]);
         }
         return unique.size();
     }
