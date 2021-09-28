30d0b59646b9a3b4eb24167a5d46d1a3031703a0
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-28 22:39:59 +0900

    Running on empty - food review! part 461

--- Java/leetcode/weekly_contests/weekly_106/P_922.java
+++ Java/leetcode/weekly_contests/weekly_106/P_922.java
@@ -1,39 +1,25 @@
 package leetcode.weekly_contests.weekly_106;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_922 {
 
-    public int[] sortArrayByParityII(int[] A) {
+    public int[] sortArrayByParityII(int[] nums) {
+        final int n = nums.length;
         final int[] map = new int[1001];
-        for (int num : A) { map[num]++; }
+        for (int num : nums) {
+            map[num]++;
+        }
         int even = 0;
         int odd = 1;
-        for (int i = 0; i < A.length; i++) {
-            if (i % 2 == 0) {
-                while (even % 2 != 0 || map[even] == 0) { even++; }
-                A[i] = even;
-                map[even]--;
-            } else {
-                while (odd % 2 == 0 || map[odd] == 0) { odd++; }
-                A[i] = odd;
-                map[odd]--;
-            }
+        for (int i = 0; i < n; i += 2) {
+            while (map[even] == 0) { even += 2; }
+            nums[i] = even;
+            map[even]--;
         }
-        return A;
-    }
-
-    public int[] sortArrayByParity(int[] A) {
-        int odd = 1;
-        for (int i = 0; i < A.length; i += 2) {
-            if (A[i] % 2 != 0) {
-                while (A[odd] % 2 != 0) {
-                    odd += 2;
-                }
-                final int tmp = A[i];
-                A[i] = A[odd];
-                A[odd] = tmp;
-            }
+        for (int i = 1; i < n; i += 2) {
+            while (map[odd] == 0) { odd += 2; }
+            nums[i] = odd;
+            map[odd]--;
         }
-        return A;
+        return nums;
     }
 }
