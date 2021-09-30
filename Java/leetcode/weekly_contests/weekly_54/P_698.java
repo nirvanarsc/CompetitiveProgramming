6e0eff784d3507e68f4c87dfacf96f04abb89f47
nirvanarsc <nirvana_rsc@yahoo.com>
2021-09-30 22:39:59 +0900

    Running on empty - food review! part 463

--- Java/leetcode/weekly_contests/weekly_54/P_698.java
+++ Java/leetcode/weekly_contests/weekly_54/P_698.java
@@ -4,31 +4,49 @@ import java.util.Arrays;
 
 public class P_698 {
 
+    static boolean[] good;
+    static boolean[][] seen;
+    static int[][] dp;
+    static int n;
+
     public boolean canPartitionKSubsets(int[] nums, int k) {
         final int sum = Arrays.stream(nums).sum();
         final int target = sum / k;
         if (target * k != sum) {
             return false;
         }
-        return recurse(nums, new boolean[nums.length], 0, target, k, nums.length - 1);
-    }
-
-    private static boolean recurse(int[] nums, boolean[] taken, int currSum, int target, int k, int idx) {
-        if (k == 0 && currSum == 0) {
-            return true;
-        }
-        if (currSum == target && recurse(nums, taken, 0, target, k - 1, nums.length - 1)) {
-            return true;
-        }
-        for (int i = idx; i >= 0; i--) {
-            if (!taken[i] && currSum + nums[i] <= target) {
-                taken[i] = true;
-                if (recurse(nums, taken, currSum + nums[i], target, k, i - 1)) {
-                    return true;
+        n = nums.length;
+        good = new boolean[1 << n];
+        seen = new boolean[1 << n][k + 1];
+        dp = new int[1 << n][k + 1];
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int curr = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    curr += nums[i];
                 }
-                taken[i] = false;
+            }
+            if (curr == target) {
+                good[mask] = true;
             }
         }
-        return false;
+        return dfs((1 << n) - 1, k) == 1;
+    }
+
+    private static int dfs(int mask, int k) {
+        if (k == 0) {
+            return mask == 0 ? 1 : 0;
+        }
+        if (seen[mask][k]) {
+            return dp[mask][k];
+        }
+        int res = 0;
+        for (int subMask = mask; subMask > 0; subMask = (subMask - 1) & mask) {
+            if (good[subMask]) {
+                res = Math.max(res, dfs(mask ^ subMask, k - 1));
+            }
+        }
+        seen[mask][k] = true;
+        return dp[mask][k] = res;
     }
 }
