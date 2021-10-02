a36b8983eb113d9ea00e588efeabc1f2a505c0c7
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-02 23:25:46 +0900

    Running on empty - food review! part 465

--- Java/leetcode/hard/P_174.java
+++ Java/leetcode/hard/P_174.java
@@ -2,32 +2,34 @@ package leetcode.hard;
 
 public class P_174 {
 
+    static int n;
+    static int m;
+    static int[][] dp;
+    static boolean[][] seen;
+
     public int calculateMinimumHP(int[][] dungeon) {
-        final int n = dungeon.length;
-        final int m = dungeon[0].length;
-        final int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return 1 + dfs(dungeon, 0, 0, n, m, dp);
+        n = dungeon.length;
+        m = dungeon[0].length;
+        dp = new int[n][m];
+        seen = new boolean[n][m];
+        return 1 + dfs(dungeon, 0, 0);
     }
 
-    private static int dfs(int[][] d, int r, int c, int n, int m, int[][] dp) {
+    private static int dfs(int[][] d, int r, int c) {
         if (r == n - 1 && c == m - 1) {
             return Math.max(0, -d[r][c]);
         }
-        if (dp[r][c] != -1) {
+        if (seen[r][c]) {
             return dp[r][c];
         }
         int res = (int) 1e9;
         if (r < n - 1) {
-            res = Math.min(res, Math.max(0, -d[r][c] + dfs(d, r + 1, c, n, m, dp)));
+            res = Math.min(res, Math.max(0, -d[r][c] + dfs(d, r + 1, c)));
         }
         if (c < m - 1) {
-            res = Math.min(res, Math.max(0, -d[r][c] + dfs(d, r, c + 1, n, m, dp)));
+            res = Math.min(res, Math.max(0, -d[r][c] + dfs(d, r, c + 1)));
         }
+        seen[r][c] = true;
         return dp[r][c] = res;
     }
 }
