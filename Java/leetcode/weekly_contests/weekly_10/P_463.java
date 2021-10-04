6bf7c83bf327666225d806ccfda6c1b42bb101a0
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-04 22:57:04 +0900

    Running on empty - food review! part 467

--- Java/leetcode/weekly_contests/weekly_10/P_463.java
+++ Java/leetcode/weekly_contests/weekly_10/P_463.java
@@ -2,45 +2,24 @@ package leetcode.weekly_contests.weekly_10;
 
 public class P_463 {
 
+    private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
     public int islandPerimeter(int[][] grid) {
-        int perimeter = 0;
+        int res = 0;
         final int n = grid.length;
         final int m = grid[0].length;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 0) { continue; }
-                if (i == 0 || grid[i - 1][j] == 0) { perimeter++; }
-                if (i == n - 1 || grid[i + 1][j] == 0) { perimeter++; }
-                if (j == 0 || grid[i][j - 1] == 0) { perimeter++; }
-                if (j == m - 1 || grid[i][j + 1] == 0) { perimeter++; }
-            }
-        }
-        return perimeter;
-    }
-
-    private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-
-    public int islandPerimeterOld(int[][] grid) {
-        int res = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    res += getP(grid, i, j);
+                    for (int[] dir : DIRS) {
+                        final int nx = i + dir[0];
+                        final int ny = j + dir[1];
+                        final boolean inBounds = 0 <= nx && nx < n && 0 <= ny && ny < m;
+                        if (!inBounds || grid[nx][ny] == 0) {
+                            res++;
+                        }
+                    }
                 }
-            }
-        }
-        return res;
-    }
-
-    private static int getP(int[][] grid, int r, int c) {
-        int res = 0;
-        for (int[] dir : DIRS) {
-            final int nx = r + dir[0];
-            final int ny = c + dir[1];
-            if (nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length) {
-                res += grid[nx][ny] == 0 ? 1 : 0;
-            } else {
-                res++;
             }
         }
         return res;
