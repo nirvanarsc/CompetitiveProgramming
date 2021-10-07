54f71bac536989b67c5c331599b1eaa0d850b96a
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-07 22:39:59 +0900

    Running on empty - food review! part 470

--- Java/leetcode/medium/P_79.java
+++ Java/leetcode/medium/P_79.java
@@ -4,9 +4,14 @@ public class P_79 {
 
     private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
+    static int n;
+    static int m;
+
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        n = board.length;
+        m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (dfs(board, i, j, word, 0)) {
                     return true;
                 }
@@ -19,7 +24,7 @@ public class P_79 {
         if (index == word.length()) {
             return true;
         }
-        if (i < 0 || i == board.length || j < 0 || j == board[0].length || board[i][j] != word.charAt(index)) {
+        if (i < 0 || i == n || j < 0 || j == m || board[i][j] != word.charAt(index)) {
             return false;
         }
         final char t = board[i][j];
