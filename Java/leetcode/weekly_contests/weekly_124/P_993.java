009950134036829dadd22c39e5a34d585114043c
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-18 23:35:44 +0900

    Running on empty - food review! part 481

--- Java/leetcode/weekly_contests/weekly_124/P_993.java
+++ Java/leetcode/weekly_contests/weekly_124/P_993.java
@@ -2,36 +2,28 @@ package leetcode.weekly_contests.weekly_124;
 
 import utils.DataStructures.TreeNode;
 
-@SuppressWarnings({ "ReturnOfNull", "ConstantConditions" })
 public class P_993 {
 
-    static class Pair {
-        int depth;
-        TreeNode parent;
-
-        Pair(int depth, TreeNode parent) {
-            this.depth = depth;
-            this.parent = parent;
-        }
-    }
+    static int[] d;
+    static int[] p;
+    static int l, r;
 
     public boolean isCousins(TreeNode root, int x, int y) {
-        final Pair p1 = findParent(root, null, x, 0);
-        final Pair p2 = findParent(root, null, y, 0);
-        return p1.depth == p2.depth && p1.parent != p2.parent;
+        l = x;
+        r = y;
+        d = new int[101];
+        p = new int[101];
+        dfs(root, new TreeNode(0));
+        return d[x] == d[y] && p[x] != p[y];
     }
 
-    private static Pair findParent(TreeNode node, TreeNode parent, int t, int d) {
-        if (node == null) {
-            return null;
+    private static void dfs(TreeNode curr, TreeNode par) {
+        if (curr == null) {
+            return;
         }
-
-        if (node.val == t) {
-            return new Pair(d, parent);
-        }
-
-        final Pair left = findParent(node.left, node, t, d + 1);
-        final Pair right = findParent(node.right, node, t, d + 1);
-        return left != null ? left : right;
+        d[curr.val] = d[par.val] + 1;
+        p[curr.val] = par.val;
+        dfs(curr.left, curr);
+        dfs(curr.right, curr);
     }
 }
