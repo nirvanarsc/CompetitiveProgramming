4e7a5972a4b6c15e72849aeb04ecf2c095486ce2
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-21 23:59:16 +0900

    Running on empty - food review! part 484

--- Java/leetcode/medium/P_380.java
+++ Java/leetcode/medium/P_380.java
@@ -7,43 +7,47 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+@SuppressWarnings({ "InnerClassMayBeStatic", "unused", "PublicConstructorInNonPublicClass" })
 public class P_380 {
 
-    public static class RandomizedSet {
+    class RandomizedSet {
         List<Integer> list;
         Map<Integer, Integer> map;
         Random r;
+        int idx;
 
         public RandomizedSet() {
             list = new ArrayList<>();
             map = new HashMap<>();
             r = new Random();
+            idx = 0;
         }
 
         public boolean insert(int val) {
             if (map.containsKey(val)) {
                 return false;
             }
-            final int idx = list.size();
             list.add(val);
-            map.put(val, idx);
+            map.put(val, idx++);
             return true;
         }
 
         public boolean remove(int val) {
-            if (!map.containsKey(val)) {
+            final Integer prev = map.get(val);
+            if (prev == null) {
                 return false;
             }
-            final int idx = map.get(val);
-            Collections.swap(list, idx, list.size() - 1);
-            map.put(list.get(idx), idx);
-            list.remove(list.size() - 1);
+            idx--;
+            final int last = list.get(idx);
+            Collections.swap(list, prev, idx);
+            list.remove(idx);
+            map.put(last, prev);
             map.remove(val);
             return true;
         }
 
         public int getRandom() {
-            return list.get(r.nextInt(list.size()));
+            return list.get(r.nextInt(idx));
         }
     }
 }
