64b04b37bbe07bc5b59c96880f060363472cb1cf
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-22 22:39:59 +0900

    Running on empty - food review! part 485

--- Java/leetcode/medium/P_451.java
+++ Java/leetcode/medium/P_451.java
@@ -1,53 +1,29 @@
 package leetcode.medium;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class P_451 {
 
-    public String frequencyBucketSort(String s) {
-        final int[] freq = new int[128];
-        final List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i)]++;
-        }
-        for (int i = 0; i < 128; i++) {
-            if (freq[i] != 0) {
-                list.add(i);
-            }
-        }
-        list.sort((a, b) -> Integer.compare(freq[b], freq[a]));
-        final StringBuilder sb = new StringBuilder();
-        for (int i : list) {
-            while (freq[i]-- > 0) {
-                sb.append((char) i);
-            }
-        }
-        return sb.toString();
-    }
-
     public String frequencySort(String s) {
-        final Character[] arr = new Character[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            arr[i] = s.charAt(i);
+        final int n = s.length();
+        final int alphabetSize = 128;
+        final int[] f = new int[alphabetSize];
+        final int[][] sorted = new int[alphabetSize][2];
+        final char[] res = new char[n];
+        for (char c : s.toCharArray()) {
+            f[c]++;
         }
-        final Map<Character, Integer> freq = new HashMap<>();
-        for (Character c : arr) {
-            freq.merge(c, 1, Integer::sum);
+        for (int i = 0; i < alphabetSize; i++) {
+            sorted[i] = new int[] { f[i], i };
         }
-        Arrays.sort(arr, (a, b) -> {
-            if (freq.get(a).equals(freq.get(b))) {
-                return Character.compare(a, b);
+        Arrays.sort(sorted, (a, b) -> Integer.compare(b[0], a[0]));
+        int idx = 0;
+        for (int i = 0; i < alphabetSize; i++) {
+            final char c = (char) sorted[i][1];
+            for (int j = 0; j < sorted[i][0]; j++) {
+                res[idx++] = c;
             }
-            return Integer.compare(freq.get(b), freq.get(a));
-        });
-        final StringBuilder sb = new StringBuilder();
-        for (Character c : arr) {
-            sb.append(c);
         }
-        return sb.toString();
+        return new String(res);
     }
 }
