beb945cc8488fcf2fabb9cdc8418eed246449e27
nirvanarsc <nirvana_rsc@yahoo.com>
2021-10-24 21:30:04 +0900

    Running on empty - food review! part 487

--- Java/leetcode/medium/P_222.java
+++ Java/leetcode/medium/P_222.java
@@ -8,29 +8,20 @@ public class P_222 {
         if (root == null) {
             return 0;
         }
-        final int left = getHeight(root.left);
-        final int right = getHeight(root.right);
+        final int left = f(root.left);
+        final int right = f(root.right);
         if (left == right) {
             return (1 << left) + countNodes(root.right);
         }
         return (1 << right) + countNodes(root.left);
     }
 
-    private static int getHeight(TreeNode node) {
-        if (node == null) {
-            return 0;
+    private static int f(TreeNode node) {
+        int res = 0;
+        while (node != null) {
+            node = node.left;
+            res++;
         }
-        return 1 + getHeight(node.left);
-    }
-
-    public int countNodesOld(TreeNode root) {
-        final int h = height(root);
-        return h < 0 ? 0 :
-               height(root.right) == h - 1 ? (1 << h) + countNodesOld(root.right)
-                                           : (1 << h - 1) + countNodesOld(root.left);
-    }
-
-    private static int height(TreeNode root) {
-        return root == null ? -1 : 1 + height(root.left);
+        return res;
     }
 }
