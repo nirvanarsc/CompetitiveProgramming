0789442a27a173f3972e15bafea41a0a671e2b2c
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-03 21:44:47 +0900

    Running on empty - food review! part 496

--- Java/leetcode/medium/P_129.java
+++ Java/leetcode/medium/P_129.java
@@ -4,23 +4,28 @@ import utils.DataStructures.TreeNode;
 
 public class P_129 {
 
+    static int total;
+    static int curr;
+
     public int sumNumbers(TreeNode root) {
-        final int[] res = { 0, 0 };
-        dfs(root, res);
-        return res[1];
+        total = 0;
+        curr = 0;
+        dfs(root);
+        return total;
     }
 
-    @SuppressWarnings("ConstantConditions")
-    private static void dfs(TreeNode root, int[] num) {
-        if (root == null) {
+    private static void dfs(TreeNode node) {
+        if (node == null) {
             return;
         }
-        num[0] = (num[0] * 10) + root.val;
-        dfs(root.left, num);
-        if (root.left == null && root.right == null) {
-            num[1] += num[0];
+        curr *= 10;
+        curr += node.val;
+        if (node.left == null && node.right == null) {
+            total += curr;
         }
-        dfs(root.right, num);
-        num[0] /= 10;
+        //noinspection ConstantConditions
+        dfs(node.left);
+        dfs(node.right);
+        curr /= 10;
     }
 }
