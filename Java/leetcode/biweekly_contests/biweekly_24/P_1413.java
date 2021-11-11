f87cae2a4b884cdba73833fa15db3b0cf7827132
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-11 23:59:21 +0900

    Running on empty - food review! part 504

--- Java/leetcode/biweekly_contests/biweekly_24/P_1413.java
+++ Java/leetcode/biweekly_contests/biweekly_24/P_1413.java
@@ -3,12 +3,12 @@ package leetcode.biweekly_contests.biweekly_24;
 public class P_1413 {
 
     public int minStartValue(int[] nums) {
-        int min = 0;
+        int res = 0;
         int sum = 0;
         for (int num : nums) {
             sum += num;
-            min = Math.min(min, sum);
+            res = Math.min(res, sum);
         }
-        return -min + 1;
+        return -res + 1;
     }
 }
