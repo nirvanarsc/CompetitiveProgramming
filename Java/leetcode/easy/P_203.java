7a0c817bd42caefa2ae6690aa68826797945518f
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-12 23:46:15 +0900

    Running on empty - food review! part 505

--- Java/leetcode/easy/P_203.java
+++ Java/leetcode/easy/P_203.java
@@ -2,19 +2,20 @@ package leetcode.easy;
 
 import utils.DataStructures.ListNode;
 
+@SuppressWarnings("ConstantConditions")
 public class P_203 {
 
-    @SuppressWarnings("ConstantConditions")
     public ListNode removeElements(ListNode head, int val) {
         final ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        for (ListNode iter = dummy; iter != null && iter.next != null; iter = iter.next) {
-            ListNode next = iter;
-            while (next.next != null && next.next.val == val) {
-                next = next.next;
+        ListNode iter = dummy;
+        while (head != null) {
+            if (head.val != val) {
+                iter.next = head;
+                iter = iter.next;
             }
-            iter.next = next.next;
+            head = head.next;
         }
+        iter.next = null;
         return dummy.next;
     }
 }
