5ad882eff4177f2d96be80666387433e0c83f1e8
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-15 23:52:50 +0900

    Running on empty - food review! part 508

--- Java/leetcode/medium/P_368.java
+++ Java/leetcode/medium/P_368.java
@@ -8,12 +8,15 @@ import java.util.Map;
 
 public class P_368 {
 
+    static Map<Integer, List<Integer>> dp;
+
     public List<Integer> largestDivisibleSubset(int[] nums) {
+        dp = new HashMap<>();
         Arrays.sort(nums);
-        return dfs(nums, 0, new HashMap<>());
+        return dfs(nums, 0);
     }
 
-    private static List<Integer> dfs(int[] nums, int idx, Map<Integer, List<Integer>> dp) {
+    private static List<Integer> dfs(int[] nums, int idx) {
         if (dp.containsKey(idx)) {
             return dp.get(idx);
         }
@@ -21,7 +24,7 @@ public class P_368 {
         final int div = idx == 0 ? 1 : nums[idx - 1];
         for (int k = idx; k < nums.length; k++) {
             if (nums[k] % div == 0) {
-                final List<Integer> next = new ArrayList<>(dfs(nums, k + 1, dp));
+                final List<Integer> next = new ArrayList<>(dfs(nums, k + 1));
                 next.add(nums[k]);
                 if (next.size() > res.size()) {
                     res = next;
