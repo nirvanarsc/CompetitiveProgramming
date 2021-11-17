44b5d2f762b706112d13c72fda9c90ae0f9ccab8
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-17 23:47:54 +0900

    Running on empty - food review! part 510

--- Java/leetcode/medium/P_62.java
+++ Java/leetcode/medium/P_62.java
@@ -4,22 +4,34 @@ import java.util.Arrays;
 
 public class P_62 {
 
-    public int uniquePaths2(int m, int n) {
-        final int[] dp = new int[m];
-        dp[0] = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (j > 0) {
-                    dp[j] += dp[j - 1];
-                }
-            }
-        }
-
-        return dp[m - 1];
-    }
+    static int[][] dp;
+    static boolean[][] seen;
+    static int lr;
+    static int lc;
 
     public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        seen = new boolean[m][n];
+        lr = m - 1;
+        lc = n - 1;
+        return dfs(0, 0);
+    }
+
+    private static int dfs(int r, int c) {
+        if (r > lr || c > lc) {
+            return 0;
+        }
+        if (r == lr && c == lc) {
+            return 1;
+        }
+        if (seen[r][c]) {
+            return dp[r][c];
+        }
+        seen[r][c] = true;
+        return dp[r][c] = dfs(r + 1, c) + dfs(r, c + 1);
+    }
+
+    public int uniquePathsBottomUp(int m, int n) {
         final int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
