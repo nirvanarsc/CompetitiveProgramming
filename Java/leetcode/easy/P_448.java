ac81f6b128fa2e181b922ba1ce2f36388cdac94d
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-18 22:39:59 +0900

    Running on empty - food review! part 511

--- Java/leetcode/easy/P_448.java
+++ Java/leetcode/easy/P_448.java
@@ -6,14 +6,16 @@ import java.util.List;
 public class P_448 {
 
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        final int n = nums.length;
-        final boolean[] seen = new boolean[n];
-        for (int num : nums) {
-            seen[num - 1] = true;
-        }
         final List<Integer> res = new ArrayList<>();
+        final int n = nums.length;
         for (int i = 0; i < n; i++) {
-            if (!seen[i]) {
+            final int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
                 res.add(i + 1);
             }
         }
