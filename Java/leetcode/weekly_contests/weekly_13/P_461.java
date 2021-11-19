03f63440d9f6ae2e917c5bcdb8f9d77fcd96f92b
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-19 22:39:59 +0900

    Running on empty - food review! part 512

--- Java/leetcode/weekly_contests/weekly_13/P_461.java
+++ Java/leetcode/weekly_contests/weekly_13/P_461.java
@@ -3,10 +3,10 @@ package leetcode.weekly_contests.weekly_13;
 public class P_461 {
 
     public int hammingDistance(int x, int y) {
-        int n = x ^ y;
+        int xor = x ^ y;
         int res = 0;
-        while (n > 0) {
-            n &= n - 1;
+        while (xor > 0) {
+            xor &= xor - 1;
             res++;
         }
         return res;
