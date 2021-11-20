240ca4db5efa9755e2816160e70e0815271b7480
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-20 22:39:59 +0100

    Running on empty - food review! part 513

--- Java/leetcode/medium/P_540.java
+++ Java/leetcode/medium/P_540.java
@@ -3,18 +3,16 @@ package leetcode.medium;
 public class P_540 {
 
     public int singleNonDuplicate(int[] nums) {
-        int start = 0, end = nums.length - 1;
-        while (start < end) {
-            int mid = start + end >>> 1;
-            if (mid % 2 == 1) {
-                mid--;
-            }
-            if (nums[mid] != nums[mid + 1]) {
-                end = mid;
+        int lo = 0;
+        int hi = nums.length / 2;
+        while (lo < hi) {
+            final int mid = lo + hi >>> 1;
+            if (nums[2 * mid] == nums[2 * mid + 1]) {
+                lo = mid + 1;
             } else {
-                start = mid + 2;
+                hi = mid;
             }
         }
-        return nums[start];
+        return nums[2 * lo];
     }
 }
