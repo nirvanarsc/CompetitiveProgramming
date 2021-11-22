c28d21595e801c799d3777c6b5e4974dc8221526
nirvanarsc <nirvana_rsc@yahoo.com>
2021-11-22 23:08:36 +0100

    Running on empty - food review! part 515

--- Java/leetcode/medium/P_450.java
+++ Java/leetcode/medium/P_450.java
@@ -6,44 +6,31 @@ import utils.DataStructures.TreeNode;
 public class P_450 {
 
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null || (root.left == null && root.right == null && root.val == key)) {
+        if (root == null) {
             return null;
         }
         if (root.val < key) {
             root.right = deleteNode(root.right, key);
-            return root;
-        }
-        if (root.val > key) {
+        } else if (root.val > key) {
             root.left = deleteNode(root.left, key);
-            return root;
-        }
-        if (root.left == null) {
-            return root.right;
-        }
-        if (root.right == null) {
-            return root.left;
-        }
-
-        //Two children
-        if (root.right.left == null) {
-            root.right.left = root.left;
-            return root.right;
         } else {
-            final TreeNode smallest = deleteSmallest(root.right);
-            smallest.left = root.left;
-            smallest.right = root.right;
-            return smallest;
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            if (root.left == null) {
+                return root.right;
+            } else {
+                TreeNode iter = root.right;
+                while (iter.left != null) {
+                    iter = iter.left;
+                }
+                root.val = iter.val;
+                root.right = deleteNode(root.right, root.val);
+            }
         }
-    }
-
-    private static TreeNode deleteSmallest(TreeNode root) {
-        TreeNode curr = root.left;
-        TreeNode pre = root;
-        while (curr.left != null) {
-            pre = curr;
-            curr = curr.left;
-        }
-        pre.left = curr.right;
-        return curr;
+        return root;
     }
 }
