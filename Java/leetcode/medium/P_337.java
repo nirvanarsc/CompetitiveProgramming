1a8c39a1e56ac60a49e2d5ec80a01207c89446a1
nirvanarsc <nirvana_rsc@yahoo.com>
2021-12-05 22:39:59 +0100

    Running on empty - food review! part 528

--- Java/leetcode/medium/P_337.java
+++ Java/leetcode/medium/P_337.java
@@ -1,55 +1,23 @@
 package leetcode.medium;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import utils.DataStructures.TreeNode;
 
 public class P_337 {
 
     public int rob(TreeNode root) {
-        return rob(root, new HashMap<>());
-    }
-
-    public int rob(TreeNode root, Map<TreeNode, Integer> dp) {
-        if (root == null) {
-            return 0;
-        }
-
-        if (dp.containsKey(root)) {
-            return dp.get(root);
-        }
-
-        int left = 0;
-        if (root.left != null) {
-            left = rob(root.left.left, dp) + rob(root.left.right, dp);
-        }
-        int right = 0;
-        if (root.right != null) {
-            right = rob(root.right.left, dp) + rob(root.right.right, dp);
-        }
-
-        dp.put(root, Math.max(root.val + left + right, rob(root.left, dp) + rob(root.right, dp)));
-        return dp.get(root);
-    }
-
-    public int robOptimized(TreeNode root) {
         final int[] res = dfs(root);
         return Math.max(res[0], res[1]);
     }
 
-    private static int[] dfs(TreeNode root) {
-        if (root == null) {
+    private static int[] dfs(TreeNode curr) {
+        if (curr == null) {
             return new int[2];
         }
-
-        final int[] left = dfs(root.left);
-        final int[] right = dfs(root.right);
+        final int[] left = dfs(curr.left);
+        final int[] right = dfs(curr.right);
         final int[] res = new int[2];
-
         res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        res[1] = root.val + left[0] + right[0];
-
+        res[1] = curr.val + left[0] + right[0];
         return res;
     }
 }
--- Java/leetcode/weekly_contests/weekly_133/P_1030.java
+++ Java/leetcode/weekly_contests/weekly_133/P_1030.java
@@ -7,12 +7,12 @@ public class P_1030 {
 
     private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
-    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
-        final int[][] res = new int[R * C][2];
-        final boolean[][] visited = new boolean[R][C];
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        final int[][] res = new int[rows * cols][2];
+        final boolean[][] visited = new boolean[rows][cols];
         final Deque<int[]> queue = new ArrayDeque<>();
-        queue.offerLast(new int[] { r0, c0 });
-        visited[r0][c0] = true;
+        queue.offerLast(new int[] { rCenter, cCenter });
+        visited[rCenter][cCenter] = true;
         int i = 0;
         while (!queue.isEmpty()) {
             final int[] ints = queue.removeFirst();
@@ -20,7 +20,7 @@ public class P_1030 {
             for (int[] dir : DIRS) {
                 final int newX = ints[0] + dir[0];
                 final int newY = ints[1] + dir[1];
-                if (newX >= 0 && newX < R && newY >= 0 && newY < C && !visited[newX][newY]) {
+                if (newX >= 0 && newX < rows && newY >= 0 && newY < cols && !visited[newX][newY]) {
                     visited[newX][newY] = true;
                     queue.offerLast(new int[] { newX, newY });
                 }
