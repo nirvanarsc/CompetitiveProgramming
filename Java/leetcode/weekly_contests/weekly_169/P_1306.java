5a26a1e21e839983b6c14862a688decab4a1f21f
nirvanarsc <nirvana_rsc@yahoo.com>
2021-12-09 22:14:04 +0100

    Running on empty - food review! part 532

--- Java/leetcode/weekly_contests/weekly_169/P_1306.java
+++ Java/leetcode/weekly_contests/weekly_169/P_1306.java
@@ -1,30 +1,29 @@
 package leetcode.weekly_contests.weekly_169;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public final class P_1306 {
 
+    static int n;
+    static int[] dp;
+    static boolean[] seen;
+
     public boolean canReach(int[] arr, int start) {
-        final Deque<Integer> dq = new ArrayDeque<>();
-        dq.offerLast(start);
-        final int n = arr.length;
-        final boolean[] seen = new boolean[n];
-        while (!dq.isEmpty()) {
-            final int curr = dq.removeFirst();
-            if (arr[curr] == 0) {
-                return true;
-            }
-            final int move = arr[curr];
-            if (curr - move >= 0 && !seen[curr - move]) {
-                seen[curr - move] = true;
-                dq.offerLast(curr - move);
-            }
-            if (curr + move < n && !seen[curr + move]) {
-                seen[curr + move] = true;
-                dq.offerLast(curr + move);
-            }
+        n = arr.length;
+        dp = new int[n];
+        seen = new boolean[n];
+        return dfs(arr, start) == 1;
+    }
+
+    private static int dfs(int[] arr, int idx) {
+        if (idx < 0 || idx >= n) {
+            return 0;
         }
-        return false;
+        if (arr[idx] == 0) {
+            return 1;
+        }
+        if (seen[idx]) {
+            return dp[idx];
+        }
+        seen[idx] = true;
+        return dp[idx] = Math.max(dfs(arr, idx - arr[idx]), dfs(arr, idx + arr[idx]));
     }
 }
