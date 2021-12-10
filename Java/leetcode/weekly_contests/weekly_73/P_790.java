89e5b583717ebf6487e50f5a91ddafaa06f39560
nirvanarsc <nirvana_rsc@yahoo.com>
2021-12-10 23:35:11 +0100

    Running on empty - food review! part 533

--- Java/leetcode/weekly_contests/weekly_73/P_790.java
+++ Java/leetcode/weekly_contests/weekly_73/P_790.java
@@ -4,18 +4,19 @@ public class P_790 {
 
     private static final int MOD = (int) (1e9 + 7);
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public int numTilings(int N) {
-        if (N < 3) {
-            return N;
+    static int[] dp;
+    static boolean init;
+
+    public int numTilings(int n) {
+        if (!init) {
+            dp = new int[(int) (1e3 + 5)];
+            dp[0] = dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i < dp.length; i++) {
+                dp[i] = ((2 * dp[i - 1]) % MOD + dp[i - 3]) % MOD;
+            }
+            init = true;
         }
-        final int[] dp = new int[N + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= N; i++) {
-            dp[i] = ((2 * dp[i - 1]) % MOD + dp[i - 3]) % MOD;
-        }
-        return dp[N];
+        return dp[n];
     }
 }
