c42881813ad436e97ea27beb111dcf4dcb521ca5
nirvanarsc <nirvana_rsc@yahoo.com>
2021-12-21 22:56:16 +0100

    Running on empty - food review! part 544

---
+++ Java/leetcode/weekly_contests/weekly_272/P_1.java
@@ -0,0 +1,24 @@
+package leetcode.weekly_contests.weekly_272;
+
+public class P_1 {
+
+    public String firstPalindrome(String[] words) {
+        for (String s : words) {
+            if (isPalindrome(s.toCharArray())) {
+                return s;
+            }
+        }
+        return "";
+    }
+
+    private static boolean isPalindrome(char[] w) {
+        int l = 0;
+        int r = w.length - 1;
+        while (l < r) {
+            if (w[l++] != w[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Java/leetcode/weekly_contests/weekly_272/P_2.java
@@ -0,0 +1,20 @@
+package leetcode.weekly_contests.weekly_272;
+
+public class P_2 {
+
+    public String addSpaces(String s, int[] spaces) {
+        final StringBuilder sb = new StringBuilder();
+        final char[] w = s.toCharArray();
+        final int n = s.length();
+        final int m = spaces.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (j < m && spaces[j] == i) {
+                j++;
+                sb.append(' ');
+            }
+            sb.append(w[i]);
+        }
+        return sb.toString();
+    }
+}
