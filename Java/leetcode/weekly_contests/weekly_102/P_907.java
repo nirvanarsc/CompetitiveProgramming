03a90cfca0a4cc645f92b5e9d609fc94a0600454
nirvanarsc <nirvana_rsc@yahoo.com>
2021-12-23 21:35:30 +0100

    Running on empty - food review! part 546

--- Java/leetcode/weekly_contests/weekly_102/P_907.java
+++ Java/leetcode/weekly_contests/weekly_102/P_907.java
@@ -3,37 +3,38 @@ package leetcode.weekly_contests.weekly_102;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_907 {
 
     private static final int MOD = (int) (1e9 + 7);
 
-    public int sumSubarrayMinsStack(int[] A) {
-        final Deque<int[]> stack1 = new ArrayDeque<>();
-        final Deque<int[]> stack2 = new ArrayDeque<>();
-        final int[] left = new int[A.length];
-        final int[] right = new int[A.length];
-        for (int i = 0; i < A.length; i++) {
+    public int sumSubarrayMins(int[] arr) {
+        final Deque<int[]> dq = new ArrayDeque<>();
+        final int n = arr.length;
+        final int[] left = new int[n];
+        final int[] right = new int[n];
+        for (int i = 0; i < n; i++) {
             int curr = 1;
-            while (!stack1.isEmpty() && stack1.peekFirst()[0] > A[i]) {
-                curr += stack1.removeFirst()[1];
+            while (!dq.isEmpty() && dq.peekFirst()[0] > arr[i]) {
+                curr += dq.removeFirst()[1];
             }
-            stack1.addFirst(new int[] { A[i], curr });
+            dq.addFirst(new int[] { arr[i], curr });
             left[i] = curr;
         }
-        for (int i = A.length - 1; i >= 0; i--) {
+        dq.clear();
+        for (int i = n - 1; i >= 0; i--) {
             int curr = 1;
-            while (!stack2.isEmpty() && stack2.peekFirst()[0] >= A[i]) {
-                curr += stack2.removeFirst()[1];
+            while (!dq.isEmpty() && dq.peekFirst()[0] >= arr[i]) {
+                curr += dq.removeFirst()[1];
             }
-            stack2.addFirst(new int[] { A[i], curr });
+            dq.addFirst(new int[] { arr[i], curr });
             right[i] = curr;
         }
-        int res = 0;
-        for (int i = 0; i < A.length; i++) {
-            res = (res + left[i] * right[i] * A[i]) % MOD;
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            final long add = ((long) left[i] * right[i] * arr[i]) % MOD;
+            res = (res + add) % MOD;
         }
-        return res;
+        return (int) res;
     }
 
     private static class SegTree {
@@ -73,21 +74,21 @@ public class P_907 {
         }
     }
 
-    public int sumSubarrayMins(int[] A) {
-        return dfs(A, 0, A.length - 1, new SegTree(0, A.length - 1, A));
+    public int sumSubarrayMinsST(int[] arr) {
+        return dfs(arr, 0, arr.length - 1, new SegTree(0, arr.length - 1, arr));
     }
 
-    private static int dfs(int[] A, int start, int end, SegTree st) {
+    private static int dfs(int[] arr, int start, int end, SegTree st) {
         if (start > end) {
             return 0;
         }
         if (start == end) {
-            return A[start];
+            return arr[start];
         }
-        final int min = st.query(start, end, A);
-        final int left = dfs(A, start, min - 1, st);
-        final int right = dfs(A, min + 1, end, st);
-        int res = (min - start + 1) * A[min] * (end - min + 1);
+        final int min = st.query(start, end, arr);
+        final int left = dfs(arr, start, min - 1, st);
+        final int right = dfs(arr, min + 1, end, st);
+        int res = (min - start + 1) * arr[min] * (end - min + 1);
         res = (res + left) % MOD;
         res = (res + right) % MOD;
         return res;
---
+++ Java/leetcode/weekly_contests/weekly_271/P_4.java
@@ -0,0 +1,69 @@
+package leetcode.weekly_contests.weekly_271;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class P_4 {
+
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        final List<int[]> ll = new ArrayList<>();
+        final List<int[]> rr = new ArrayList<>();
+        for (int[] fruit : fruits) {
+            if (fruit[0] <= startPos) {
+                ll.add(new int[] { startPos - fruit[0], fruit[1] });
+            } else {
+                rr.add(new int[] { fruit[0] - startPos, fruit[1] });
+            }
+        }
+        Collections.reverse(ll);
+        for (int i = 1; i < ll.size(); i++) {
+            final int[] prev = ll.get(i - 1);
+            final int[] curr = ll.get(i);
+            ll.set(i, new int[] { curr[0], curr[1] + prev[1] });
+        }
+        for (int i = 1; i < rr.size(); i++) {
+            final int[] prev = rr.get(i - 1);
+            final int[] curr = rr.get(i);
+            rr.set(i, new int[] { curr[0], curr[1] + prev[1] });
+        }
+        int res = 0;
+        for (int i = 0; i <= k; i++) {
+            int l = upperBound(ll, i);
+            int r = upperBound(rr, k - 2 * i);
+            int curr = 0;
+            if (l < ll.size() && ll.get(l)[0] <= i) {
+                curr += ll.get(l)[1];
+            }
+            if (r < rr.size() && rr.get(r)[0] <= (k - 2 * i)) {
+                curr += rr.get(r)[1];
+            }
+            res = Math.max(res, curr);
+            r = upperBound(rr, i);
+            l = upperBound(ll, k - 2 * i);
+            curr = 0;
+            if (r < rr.size() && rr.get(r)[0] <= i) {
+                curr += rr.get(r)[1];
+            }
+            if (l < ll.size() && ll.get(l)[0] <= (k - 2 * i)) {
+                curr += ll.get(l)[1];
+            }
+            res = Math.max(res, curr);
+        }
+        return res;
+    }
+
+    private static int upperBound(List<int[]> list, int target) {
+        int lo = 0;
+        int hi = list.size() - 1;
+        while (lo < hi) {
+            final int mid = 1 + lo + hi >>> 1;
+            if (list.get(mid)[0] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
+            }
+        }
+        return lo;
+    }
+}
