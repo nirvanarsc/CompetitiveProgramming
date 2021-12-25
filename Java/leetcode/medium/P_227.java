95856378b498f903b07ebb9617a9bf87328884bf
nirvanarsc <nirvana_rsc@yahoo.com>
2021-12-25 14:42:17 +0100

    Running on empty - food review! part 548

--- Java/leetcode/medium/P_227.java
+++ Java/leetcode/medium/P_227.java
@@ -1,50 +1,31 @@
 package leetcode.medium;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class P_227 {
 
     public int calculate(String s) {
-        final Deque<Integer> operands = new ArrayDeque<>();
-        final Deque<Character> op1 = new ArrayDeque<>();
-        final Deque<Character> op2 = new ArrayDeque<>();
-        final char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            final char c = chars[i];
-            if (c == ' ') {
-                continue;
-            } else if (Character.isDigit(c)) {
-                int j = i;
-                int num = 0;
-                while (j < chars.length && Character.isDigit(chars[j])) {
-                    num = num * 10 + chars[j++] - '0';
+        final int n = s.length();
+        int res = 0;
+        int curr = 0;
+        int prev = 0;
+        char operation = '+';
+        for (int i = 0; i < n; i++) {
+            final char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                curr = (curr * 10) + (c - '0');
+            }
+            if (!Character.isDigit(c) && !Character.isWhitespace(c) || i == n - 1) {
+                if (operation == '+' || operation == '-') {
+                    res += prev;
+                    prev = (operation == '+') ? curr : -curr;
+                } else if (operation == '*') {
+                    prev *= curr;
+                } else if (operation == '/') {
+                    prev /= curr;
                 }
-                while (!op1.isEmpty()) {
-                    final char op = op1.removeFirst();
-                    if (op == '*') {
-                        num *= operands.removeFirst();
-                    } else {
-                        num = operands.removeFirst() / num;
-                    }
-                }
-                operands.addFirst(num);
-                i = j - 1;
-            } else if (c == '*' || c == '/') {
-                op1.addFirst(c);
-            } else {
-                op2.addFirst(c);
+                operation = c;
+                curr = 0;
             }
         }
-        int res = operands.removeLast();
-        while (!operands.isEmpty()) {
-            final char op = op2.removeLast();
-            if (op == '+') {
-                res += operands.removeLast();
-            } else {
-                res -= operands.removeLast();
-            }
-        }
-        return res;
+        return res + prev;
     }
 }
