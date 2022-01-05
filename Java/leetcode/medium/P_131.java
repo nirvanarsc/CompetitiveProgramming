12ecafbff68ea31e37eb97e9ad68749434e2ce65
nirvanarsc <nirvana_rsc@yahoo.com>
2022-01-05 23:33:45 +0100

    Running on empty - food review! part 559

--- Java/leetcode/medium/P_131.java
+++ Java/leetcode/medium/P_131.java
@@ -34,4 +34,47 @@ public class P_131 {
         }
         return true;
     }
+
+    public List<List<String>> partitionMasks(String s) {
+        final char[] w = s.toCharArray();
+        final int n = w.length - 1;
+        final StringBuilder sb = new StringBuilder();
+        final List<String> curr = new ArrayList<>();
+        final List<List<String>> res = new ArrayList<>();
+        for (int mask = 0; mask < (1 << n); mask++) {
+            sb.append(w[0]);
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    curr.add(sb.toString());
+                    sb.setLength(0);
+                }
+                sb.append(w[i + 1]);
+            }
+            curr.add(sb.toString());
+            sb.setLength(0);
+            boolean ok = true;
+            for (String c : curr) {
+                if (!isPalindrome(c.toCharArray())) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                res.add(new ArrayList<>(curr));
+            }
+            curr.clear();
+        }
+        return res;
+    }
+
+    private static boolean isPalindrome(char[] w) {
+        int l = 0;
+        int r = w.length - 1;
+        while (l < r) {
+            if (w[l++] != w[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
