218ae447bbf7455e4953e05220418b7c32ee6b0e
nirvanarsc <nirvana_rsc@yahoo.com>
2022-01-11 21:47:31 +0100

    Running on empty - food review! part 565

--- Java/leetcode/weekly_contests/weekly_131/P_1022.java
+++ Java/leetcode/weekly_contests/weekly_131/P_1022.java
@@ -5,23 +5,24 @@ import utils.DataStructures.TreeNode;
 @SuppressWarnings("ConstantConditions")
 public class P_1022 {
 
+    static int res;
+
     public int sumRootToLeaf(TreeNode root) {
-        final int[] path = { 0 };
-        final int[] res = { 0 };
-        dfs(root, path, res);
-        return res[0];
+        res = 0;
+        dfs(root, new int[1]);
+        return res;
     }
 
-    private static void dfs(TreeNode node, int[] path, int[] res) {
+    private static void dfs(TreeNode node, int[] path) {
         if (node == null) {
             return;
         }
         path[0] = path[0] << 1 | node.val;
-        dfs(node.left, path, res);
         if (node.left == null && node.right == null) {
-            res[0] += path[0];
+            res += path[0];
         }
-        dfs(node.right, path, res);
+        dfs(node.left, path);
+        dfs(node.right, path);
         path[0] >>= 1;
     }
 
