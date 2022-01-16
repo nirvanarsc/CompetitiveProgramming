aab62b6c4d2aec753c22f8334744b2120f5c13bd
nirvanarsc <nirvana_rsc@yahoo.com>
2022-01-16 22:22:19 +0100

    Running on empty - food review! part 570

--- Java/leetcode/weekly_contests/weekly_88/P_849.java
+++ Java/leetcode/weekly_contests/weekly_88/P_849.java
@@ -4,27 +4,25 @@ public class P_849 {
 
     public int maxDistToClosest(int[] seats) {
         final int n = seats.length;
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            if (seats[i] == 1) {
-                int j = i + 1;
-                while (j < n && seats[j] == 0) {
-                    j++;
+        int res;
+        int i = 0;
+        while (i < n && seats[i] == 0) {
+            i++;
+        }
+        res = i;
+        i = n - 1;
+        while (i >= 0 && seats[i] == 0) {
+            i--;
+        }
+        res = Math.max(res, n - 1 - i);
+        for (int j = 0; j < n; j++) {
+            if (seats[j] == 0) {
+                i = j;
+                while (i < n && seats[i] == 0) {
+                    i++;
                 }
-                res = Math.max(res, (j - i) / 2);
-                i = j - 1;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (seats[i] == 1) {
-                res = Math.max(res, i);
-                break;
-            }
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            if (seats[i] == 1) {
-                res = Math.max(res, n - i - 1);
-                break;
+                res = Math.max(res, (i - j + 1) / 2);
+                j = i - 1;
             }
         }
         return res;
