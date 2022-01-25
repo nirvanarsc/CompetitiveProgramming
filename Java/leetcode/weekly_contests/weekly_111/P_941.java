f9a439566b2d85e47190697ac2a6b3cfae7b967d
nirvanarsc <nirvana_rsc@yahoo.com>
2022-01-25 23:34:08 +0100

    Running on empty - food review! part 579

--- Java/leetcode/weekly_contests/weekly_111/P_941.java
+++ Java/leetcode/weekly_contests/weekly_111/P_941.java
@@ -3,27 +3,19 @@ package leetcode.weekly_contests.weekly_111;
 public class P_941 {
 
     public boolean validMountainArray(int[] arr) {
-        if (arr.length < 3) {
-            return false;
-        }
-        int max = -1;
-        int maxIdx = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-                maxIdx = i;
+        final int n = arr.length;
+        int peak = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i - 1] == arr[i] || arr[i] == arr[i + 1]) {
+                return false;
+            }
+            if (arr[i - 1] > arr[i] && arr[i] < arr[i + 1]) {
+                return false;
+            }
+            if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
+                peak++;
             }
         }
-        if (maxIdx == 0 || maxIdx == arr.length - 1) {
-            return false;
-        }
-        boolean ok = true;
-        for (int i = 0; i < maxIdx; i++) {
-            ok &= arr[i] < arr[i + 1];
-        }
-        for (int i = arr.length - 1; i > maxIdx; i--) {
-            ok &= arr[i] < arr[i - 1];
-        }
-        return ok;
+        return peak == 1;
     }
 }
