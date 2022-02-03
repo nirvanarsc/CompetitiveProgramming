3b85ba8bfa74dd17a29cb0ca44034acd38fd80c3
nirvanarsc <nirvana_rsc@yahoo.com>
2022-02-03 23:51:47 +0100

    Running on empty - food review! part 588

--- Java/atcoder/beginner_200_299/abc_224/E.java
+++ Java/atcoder/beginner_200_299/abc_224/E.java
@@ -3,18 +3,104 @@ package atcoder.beginner_200_299.abc_224;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public final class E {
 
     public static void main(String[] args) throws IOException {
         final FastReader fs = new FastReader();
-        final int t = fs.nextInt();
-        for (int test = 0; test < t; test++) {
-            final int n = fs.nextInt();
-            System.out.println(n);
+        final int h = fs.nextInt();
+        final int w = fs.nextInt();
+        final int n = fs.nextInt();
+        final Map<Integer, List<int[]>> row = new HashMap<>();
+        final Map<Integer, List<int[]>> col = new HashMap<>();
+        final Map<Integer, List<Integer>> g = new HashMap<>();
+        final int[][] arr = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new int[] { fs.nextInt(), fs.nextInt(), fs.nextInt(), i };
+            row.computeIfAbsent(arr[i][0], val -> new ArrayList<>()).add(arr[i]);
+            col.computeIfAbsent(arr[i][1], val -> new ArrayList<>()).add(arr[i]);
         }
+        for (List<int[]> value : row.values()) {
+            value.sort((a, b) -> a[2] == b[2] ? Integer.compare(b[0], a[0]) : Integer.compare(a[2], b[2]));
+        }
+        for (List<int[]> value : col.values()) {
+            value.sort((a, b) -> a[2] == b[2] ? Integer.compare(b[1], a[1]) : Integer.compare(a[2], b[2]));
+        }
+        final int[] inDeg = new int[n];
+        for (int[] p : arr) {
+            final int l = lowerBound(p[2] + 1, row.get(p[0]));
+            final int r = lowerBound(p[2] + 1, col.get(p[1]));
+            final int u = p[3];
+            int v;
+            if (l != -1) {
+                v = row.get(p[0]).get(l)[3];
+                g.computeIfAbsent(u, val -> new ArrayList<>()).add(v);
+                inDeg[v]++;
+            }
+            if (r != -1) {
+                v = col.get(p[1]).get(r)[3];
+                g.computeIfAbsent(u, val -> new ArrayList<>()).add(v);
+                inDeg[v]++;
+            }
+        }
+        final int[] topSort = topSort(g, inDeg, n);
+        final int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            final int u = topSort[i];
+            for (int v : g.getOrDefault(u, Collections.emptyList())) {
+                if (dp[v] + 1 > dp[u]) {
+                    dp[u] = dp[v] + 1;
+                }
+            }
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(dp[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static int lowerBound(int target, List<int[]> list) {
+        int lo = 0;
+        int hi = list.size();
+        while (lo < hi) {
+            final int mid = lo + hi >>> 1;
+            if (list.get(mid)[2] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo == list.size() ? -1 : lo;
+    }
+
+    private static int[] topSort(Map<Integer, List<Integer>> g, int[] inDeg, int n) {
+        final Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (inDeg[i] == 0) {
+                dq.offerLast(i);
+            }
+        }
+        final int[] res = new int[n];
+        for (int i = 0; !dq.isEmpty(); i++) {
+            final int u = dq.removeFirst();
+            res[i] = u;
+            for (int v : g.getOrDefault(u, Collections.emptyList())) {
+                if (--inDeg[v] == 0) {
+                    dq.offerLast(v);
+                }
+            }
+        }
+        return res;
     }
 
     static final class Utils {
--- Java/leetcode/weekly_contests/smarking_4/P_454.java
+++ Java/leetcode/weekly_contests/smarking_4/P_454.java
@@ -3,21 +3,19 @@ package leetcode.weekly_contests.smarking_4;
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_454 {
 
-    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         int res = 0;
-        final Map<Integer, Integer> countAB = new HashMap<>();
-        for (int ll : A) {
-            for (int rr : B) {
-                countAB.merge(ll + rr, 1, Integer::sum);
+        final Map<Integer, Integer> map = new HashMap<>();
+        for (int ll : nums1) {
+            for (int rr : nums2) {
+                map.merge(ll + rr, 1, Integer::sum);
             }
         }
-        for (int ll : C) {
-            for (int rr : D) {
-                final int target = ll + rr;
-                res += countAB.getOrDefault(-target, 0);
+        for (int ll : nums3) {
+            for (int rr : nums4) {
+                res += map.getOrDefault(-(ll + rr), 0);
             }
         }
         return res;
