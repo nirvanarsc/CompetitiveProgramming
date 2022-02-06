1bafcb1735bb7721140174bc5e58813ba7749fe9
nirvanarsc <nirvana_rsc@yahoo.com>
2022-02-06 18:16:29 +0100

    Running on empty - food review! part 591

--- Java/leetcode/medium/P_80.java
+++ Java/leetcode/medium/P_80.java
@@ -3,19 +3,19 @@ package leetcode.medium;
 public final class P_80 {
 
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        int j = 0;
-        while (i < nums.length) {
+        final int n = nums.length;
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
             int k = i;
-            while (k < nums.length && nums[k] == nums[i]) {
+            while (k < n && nums[k] == nums[i]) {
                 k++;
             }
             final int count = k - i;
             for (int l = 0; l < Math.min(2, count); l++) {
-                nums[j++] = nums[i];
+                nums[idx++] = nums[i];
             }
-            i = k;
+            i = k - 1;
         }
-        return j;
+        return idx;
     }
 }
---
+++ Java/leetcode/weekly_contests/weekly_279/P_1.java
@@ -0,0 +1,28 @@
+package leetcode.weekly_contests.weekly_279;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class P_1 {
+
+    public int[] sortEvenOdd(int[] nums) {
+        final int n = nums.length;
+        final PriorityQueue<Integer> odd = new PriorityQueue<>(Comparator.reverseOrder());
+        final PriorityQueue<Integer> even = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                even.add(nums[i]);
+            } else {
+                odd.add(nums[i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                nums[i] = even.remove();
+            } else {
+                nums[i] = odd.remove();
+            }
+        }
+        return nums;
+    }
+}
