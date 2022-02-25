3bb5979c2a9a4dc2f19fb7f638010e59d117434f
nirvanarsc <nirvana_rsc@yahoo.com>
2022-02-25 22:55:27 +0100

    Running on empty - food review! part 610

--- Java/leetcode/medium/P_165.java
+++ Java/leetcode/medium/P_165.java
@@ -7,13 +7,14 @@ public class P_165 {
     public int compareVersion(String version1, String version2) {
         final int[] left = Arrays.stream(version1.split("\\.")).mapToInt(Integer::valueOf).toArray();
         final int[] right = Arrays.stream(version2.split("\\.")).mapToInt(Integer::valueOf).toArray();
-        for (int i = 0; i < Math.max(left.length, right.length); i++) {
-            final int l = i >= left.length ? 0 : left[i];
-            final int r = i >= right.length ? 0 : right[i];
+        final int n = left.length;
+        final int m = right.length;
+        for (int i = 0; i < Math.max(n, m); i++) {
+            final int l = i >= n ? 0 : left[i];
+            final int r = i >= m ? 0 : right[i];
             if (l > r) {
                 return 1;
-            }
-            if (l < r) {
+            } else if (l < r) {
                 return -1;
             }
         }
