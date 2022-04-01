5096c34f0e55ba03bf2e71400d8be5dc9c54bd91
nirvanarsc <nirvana_rsc@yahoo.com>
2022-04-01 23:51:28 +0200

    Running on empty - food review! part 645

--- Java/leetcode/easy/P_344.java
+++ Java/leetcode/easy/P_344.java
@@ -3,28 +3,14 @@ package leetcode.easy;
 public class P_344 {
 
     public void reverseString(char[] s) {
-        for (int start = 0, end = s.length - 1; start < end; start++, end--) {
-            if (s[start] != s[end]) {
-                s[start] ^= s[end];
-                s[end] ^= s[start];
-                s[start] ^= s[end];
-            }
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            final char t = s[l];
+            s[l] = s[r];
+            s[r] = t;
+            l++;
+            r--;
         }
-    }
-
-    public void reverseStringR(char[] s) {
-        helper(s, 0, s.length - 1);
-    }
-
-    private static void helper(char[] s, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-        if (s[start] != s[end]) {
-            s[start] ^= s[end];
-            s[end] ^= s[start];
-            s[start] ^= s[end];
-        }
-        helper(s, start + 1, end - 1);
     }
 }
