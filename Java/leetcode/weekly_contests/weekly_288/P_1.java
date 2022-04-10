eb1de6bce68bfcdc83132a4fefe192ce6fed0ecd
nirvanarsc <nirvana_rsc@yahoo.com>
2022-04-10 21:21:34 +0200

    Running on empty - food review! part 654

---
+++ Java/leetcode/weekly_contests/weekly_288/P_1.java
@@ -0,0 +1,31 @@
+package leetcode.weekly_contests.weekly_288;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class P_1 {
+
+    public int largestInteger(int num) {
+        final char[] w = Integer.toString(num).toCharArray();
+        final List<Integer> l = new ArrayList<>();
+        final List<Integer> r = new ArrayList<>();
+        for (char c : w) {
+            if (c % 2 == 0) {
+                l.add(c - '0');
+            } else {
+                r.add(c - '0');
+            }
+        }
+        l.sort(Comparator.reverseOrder());
+        r.sort(Comparator.reverseOrder());
+        for (int i = 0; i < w.length; i++) {
+            if (w[i] % 2 == 0) {
+                w[i] = (char) (l.remove(0) + '0');
+            } else {
+                w[i] = (char) (r.remove(0) + '0');
+            }
+        }
+        return Integer.parseInt(new String(w));
+    }
+}
