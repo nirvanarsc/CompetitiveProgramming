e4224329f2f9cf205a6699b28bd63bc362ad063b
nirvanarsc <nirvana_rsc@yahoo.com>
2022-04-19 23:20:28 +0200

    Running on empty - food review! part 663

--- Java/leetcode/weekly_contests/weekly_289/P_3.java
+++ Java/leetcode/weekly_contests/weekly_289/P_3.java
@@ -3,10 +3,68 @@ package leetcode.weekly_contests.weekly_289;
 public class P_3 {
 
     public int maxTrailingZeros(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        final int n = grid.length;
+        final int m = grid[0].length;
+        final int[][] r2 = fRow(n, m, grid, 2);
+        final int[][] r5 = fRow(n, m, grid, 5);
+        final int[][] c2 = fCol(n, m, grid, 2);
+        final int[][] c5 = fCol(n, m, grid, 5);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int copy = grid[i][j];
+                int f2 = 0;
+                int f5 = 0;
+                while (copy % 2 == 0) {
+                    f2++;
+                    copy /= 2;
+                }
+                while (copy % 5 == 0) {
+                    f5++;
+                    copy /= 5;
+                }
+                res = Math.max(res, Math.min(r2[i][j + 1] + c2[i + 1][j] - f2,
+                                             r5[i][j + 1] + c5[i + 1][j] - f5));
+                res = Math.max(res, Math.min(r2[i][m] - r2[i][j] + c2[i + 1][j] - f2,
+                                             r5[i][m] - r5[i][j] + c5[i + 1][j] - f5));
+                res = Math.max(res, Math.min(r2[i][j + 1] + c2[n][j] - c2[i][j] - f2,
+                                             r5[i][j + 1] + c5[n][j] - c5[i][j] - f5));
+                res = Math.max(res, Math.min(r2[i][m] - r2[i][j] + c2[n][j] - c2[i][j] - f2,
+                                             r5[i][m] - r5[i][j] + c5[n][j] - c5[i][j] - f5));
+            }
+        }
+        return res;
+    }
 
-      //  int[][] pre2R
-        return 5;
+    private static int[][] fRow(int n, int m, int[][] g, int f) {
+        final int[][] res = new int[n][m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int c = 0;
+                int copy = g[i][j - 1];
+                while (copy % f == 0) {
+                    c++;
+                    copy /= f;
+                }
+                res[i][j] = res[i][j - 1] + c;
+            }
+        }
+        return res;
+    }
+
+    private static int[][] fCol(int n, int m, int[][] g, int f) {
+        final int[][] res = new int[n + 1][m];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < m; j++) {
+                int c = 0;
+                int copy = g[i - 1][j];
+                while (copy % f == 0) {
+                    c++;
+                    copy /= f;
+                }
+                res[i][j] = res[i - 1][j] + c;
+            }
+        }
+        return res;
     }
 }
--- Java/leetcode/weekly_contests/weekly_289/P_4.java
+++ Java/leetcode/weekly_contests/weekly_289/P_4.java
@@ -1,42 +1,41 @@
 package leetcode.weekly_contests.weekly_289;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class P_4 {
 
     static int n;
     static int[][] edges;
     static int[][] g;
-    static char[] l;
-    static int curr;
-    static boolean[] seen;
+    static char[] w;
+    static int res;
 
     public int longestPath(int[] parent, String s) {
         n = parent.length;
+        w = s.toCharArray();
         edges = new int[n - 1][2];
         for (int i = 1; i < n; i++) {
-            edges[i - 1] = new int[] { i, parent[i] };
+            edges[i - 1] = new int[] { parent[i], i };
         }
         g = packG();
-        l = s.toCharArray();
-        int res = 0;
-        seen = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (!seen[i]) {
-                curr = 0;
-                dfs(i, -1);
-                res = Math.max(res, curr);
-            }
-        }
+        res = 0;
+        dfs(0);
         return res;
     }
 
-    private static void dfs(int u, int p) {
-        seen[u] = true;
-        curr++;
+    private static int dfs(int u) {
+        final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         for (int v : g[u]) {
-            if (l[u] != l[v] && v != p) {
-                dfs(v, u);
+            final int curr = dfs(v);
+            if (w[u] != w[v]) {
+                pq.offer(curr);
             }
         }
+        final int big1 = pq.isEmpty() ? 0 : pq.remove();
+        final int big2 = pq.isEmpty() ? 0 : pq.remove();
+        res = Math.max(res, big1 + big2 + 1);
+        return big1 + 1;
     }
 
     private static int[][] packG() {
@@ -44,14 +43,12 @@ public class P_4 {
         final int[] size = new int[n];
         for (int[] edge : edges) {
             ++size[edge[0]];
-            ++size[edge[1]];
         }
         for (int i = 0; i < n; i++) {
             g[i] = new int[size[i]];
         }
         for (int[] edge : edges) {
             g[edge[0]][--size[edge[0]]] = edge[1];
-            g[edge[1]][--size[edge[1]]] = edge[0];
         }
         return g;
     }
