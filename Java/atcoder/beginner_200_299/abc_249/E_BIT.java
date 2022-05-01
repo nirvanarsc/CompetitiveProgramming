fe78b7612ceb040efe3415d952d45a1d0e4c5ed3
nirvanarsc <nirvana_rsc@yahoo.com>
2022-05-01 18:24:15 +0200

    Running on empty - food review! part 675

--- Java/atcoder/beginner_200_299/abc_249/E_BIT.java
+++ Java/atcoder/beginner_200_299/abc_249/E_BIT.java
@@ -8,88 +8,56 @@ import java.util.Random;
 
 public final class E_BIT {
 
-    private static final class BIT {
-        private final int n;
-        private final long[] data;
-        private final int mod;
-
-        private BIT(int n, int mod) {
-            this.n = n;
-            this.mod = mod;
-            data = new long[n + 1];
-        }
-
-        public void add(int idx, long val) {
-            for (int i = idx + 1; i <= n; i += lsb(i)) {
-                data[i] = (data[i] + val) % mod;
-            }
-        }
-
-        public long sum(int l, int r) {
-            return sum(r) - sum(l - 1);
-        }
-
-        private long sum(int idx) {
-            long res = 0;
-            for (int i = idx + 1; i > 0; i -= lsb(i)) {
-                res = (res + data[i]) % mod;
-            }
-            return res;
-        }
-
-        private static int lsb(int i) {
-            return i & -i;  // zeroes all the bits except the least significant one
-        }
-
-        // get k-th element
-        public int getKth(int k) {
-            int lo = 0;
-            int hi = n;
-            while (lo < hi) {
-                final int mid = lo + hi >>> 1;
-                if (k > sum(mid)) {
-                    lo = mid + 1;
-                } else {
-                    hi = mid;
-                }
-            }
-            return lo;
-        }
-    }
-
     static int n;
+    static int bitSize;
     static int p;
-    static BIT[] dp;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         final FastReader fs = new FastReader();
         n = fs.nextInt();
+        bitSize = n + 3;
         p = fs.nextInt();
-        dp = new BIT[n + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = new BIT(n + 1, p);
-        }
-        dp[0].add(0, 1);
-        dp[0].add(1, p - 1);
+        dp = new int[n + 1][bitSize];
+        bitAdd(0, 1, dp[0]);
+        bitAdd(1, p - 1, dp[0]);
         for (int idx = 0; idx < n; idx++) {
             for (int len = 0; len < n; len++) {
                 final long mul = idx == 0 ? 26L : 25L;
-                final long add = (mul * dp[len].sum(idx)) % p;
+                final int add = (int) ((mul * bitSum(idx, dp[len])) % p);
                 if (add == 0) {
                     continue;
                 }
                 for (int l = 1, r = 10; len + f(l) < n && idx + l <= n; l *= 10, r *= 10) {
                     r = Math.min(r, n - idx + 2);
-                    dp[len + f(l)].add(idx + l, add);
-                    dp[len + f(l)].add(idx + r, p - add);
+                    bitAdd(idx + l, add, dp[len + f(l)]);
+                    bitAdd(idx + r, p - add, dp[len + f(l)]);
                 }
             }
         }
         long res = 0;
         for (int i = 1; i <= n; i++) {
-            res = (res + dp[i].sum(n)) % p;
+            res = (res + bitSum(n, dp[i])) % p;
         }
         System.out.println(res);
+    }
+
+    private static void bitAdd(int idx, int val, int[] data) {
+        for (int i = idx + 1; i < bitSize; i += lsb(i)) {
+            data[i] = (data[i] + val) % p;
+        }
+    }
+
+    private static long bitSum(int idx, int[] data) {
+        long res = 0;
+        for (int i = idx + 1; i > 0; i -= lsb(i)) {
+            res = (res + data[i]) % p;
+        }
+        return res;
+    }
+
+    private static int lsb(int i) {
+        return i & -i;  // zeroes all the bits except the least significant one
     }
 
     private static int f(int n) {
---
+++ Java/leetcode/weekly_contests/weekly_291/P_2.java
@@ -0,0 +1,22 @@
+package leetcode.weekly_contests.weekly_291;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P_2 {
+
+    public int minimumCardPickup(int[] cards) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        final int n = cards.length;
+        int res = (int) 1e9;
+        for (int i = 0; i < n; i++) {
+            final int u = cards[i];
+            final Integer prev = map.get(u);
+            if (prev != null) {
+                res = Math.min(res, i - prev + 1);
+            }
+            map.put(u, i);
+        }
+        return res == (int) 1e9 ? -1 : res;
+    }
+}
