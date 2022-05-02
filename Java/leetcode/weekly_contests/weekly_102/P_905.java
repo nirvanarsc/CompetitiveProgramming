0debf5ca4347da1bcdbdbfcbaeaa2b10a008d471
nirvanarsc <nirvana_rsc@yahoo.com>
2022-05-02 23:30:07 +0200

    Running on empty - food review! part 676

--- Java/leetcode/weekly_contests/weekly_102/P_905.java
+++ Java/leetcode/weekly_contests/weekly_102/P_905.java
@@ -3,25 +3,13 @@ package leetcode.weekly_contests.weekly_102;
 import java.util.Arrays;
 import java.util.Comparator;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_905 {
 
-    public int[] sortArrayByParitySort(int[] A) {
-        return Arrays.stream(A)
+    public int[] sortArrayByParity(int[] nums) {
+        return Arrays.stream(nums)
                      .boxed()
                      .sorted(Comparator.comparingInt(a -> a % 2))
                      .mapToInt(Integer::intValue)
                      .toArray();
-    }
-
-    public int[] sortArrayByParity(int[] A) {
-        for (int i = 0, j = 0; j < A.length; j++) {
-            if (A[j] % 2 == 0) {
-                final int tmp = A[i];
-                A[i++] = A[j];
-                A[j] = tmp;
-            }
-        }
-        return A;
     }
 }
