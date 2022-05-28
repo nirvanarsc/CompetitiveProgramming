18c73fca6d5d82d55367a9193532c638b1bcb6d7
nirvanarsc <nirvana_rsc@yahoo.com>
2022-05-28 22:16:36 +0200

    Running on empty - food review! part 702

---
+++ Java/leetcode/biweekly_contests/biweekly_79/P_1.java
@@ -0,0 +1,17 @@
+package leetcode.biweekly_contests.biweekly_79;
+
+public class P_1 {
+
+    public boolean digitCount(String num) {
+        final int[] f = new int[10];
+        for (char c : num.toCharArray()) {
+            f[c - '0']++;
+        }
+        for (int i = 0; i < num.length(); i++) {
+            if (f[i] != num.charAt(i) - '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Java/leetcode/biweekly_contests/biweekly_79/P_2.java
@@ -0,0 +1,28 @@
+package leetcode.biweekly_contests.biweekly_79;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P_2 {
+
+    public String largestWordCount(String[] messages, String[] senders) {
+        final Map<String, Integer> map = new HashMap<>();
+        final int n = messages.length;
+        for (int i = 0; i < n; i++) {
+            map.merge(senders[i], messages[i].split(" ").length, Integer::sum);
+        }
+        String best = "";
+        int max = -1;
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            if (max < e.getValue()) {
+                best = e.getKey();
+                max = e.getValue();
+            } else if (max == e.getValue()) {
+                if (best.compareTo(e.getKey()) < 0) {
+                    best = e.getKey();
+                }
+            }
+        }
+        return best;
+    }
+}
