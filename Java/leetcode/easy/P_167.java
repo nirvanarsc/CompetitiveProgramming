74172e06b4363ecd34301995946e9380411ea1b3
nirvanarsc <nirvana_rsc@yahoo.com>
2022-06-09 23:31:51 +0200

    Running on empty - food review! part 714

--- Java/leetcode/easy/P_167.java
+++ Java/leetcode/easy/P_167.java
@@ -3,20 +3,20 @@ package leetcode.easy;
 public class P_167 {
 
     public int[] twoSum(int[] numbers, int target) {
-        int start = 0, end = numbers.length - 1;
         final int[] res = new int[2];
-        while (start < end) {
-            if (numbers[start] + numbers[end] == target) {
-                res[0] = start + 1;
-                res[1] = end + 1;
-                return res;
-            } else if (numbers[start] + numbers[end] < target) {
-                start++;
+        int l = 0;
+        int r = numbers.length - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] == target) {
+                res[0] = l + 1;
+                res[1] = r + 1;
+                break;
+            } else if (numbers[l] + numbers[r] < target) {
+                l++;
             } else {
-                end--;
+                r--;
             }
         }
-
         return res;
     }
 }
