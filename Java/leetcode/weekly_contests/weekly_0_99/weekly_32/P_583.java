10a9bf4ee78b4f21bca65bedcc53d3a5dc02d839
nirvanarsc <nirvana_rsc@yahoo.com>
2022-06-14 19:25:17 +0200

    Running on empty - food review! part 719

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_32/P_583.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_32/P_583.java
@@ -3,17 +3,24 @@ package leetcode.weekly_contests.weekly_0_99.weekly_32;
 public class P_583 {
 
     public int minDistance(String word1, String word2) {
-        final int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-        for (int i = 0; i < word1.length(); i++) {
-            for (int j = 0; j < word2.length(); j++) {
-                if (word1.charAt(i) == word2.charAt(j)) {
+        final int n = word1.length();
+        final int m = word2.length();
+        return n + m - 2 * lcs(word1, word2, n, m);
+    }
+
+    private static int lcs(String word1, String word2, int n, int m) {
+        final char[] l = word1.toCharArray();
+        final char[] r = word2.toCharArray();
+        final int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (l[i] == r[j]) {
                     dp[i + 1][j + 1] = dp[i][j] + 1;
                 } else {
                     dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
                 }
             }
         }
-        final int lcs = dp[word1.length()][word2.length()];
-        return word1.length() - lcs + word2.length() - lcs;
+        return dp[n][m];
     }
 }
