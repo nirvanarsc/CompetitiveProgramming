28e9c821d0da7fd609c8a03e5c7055b12291a7e5
nirvanarsc <nirvana_rsc@yahoo.com>
2022-06-21 23:05:06 +0200

    Running on empty - food review! part 726

--- Java/leetcode/weekly_contests/weekly_200_299/weekly_213/P_1642.java
+++ Java/leetcode/weekly_contests/weekly_200_299/weekly_213/P_1642.java
@@ -1,8 +1,6 @@
 package leetcode.weekly_contests.weekly_200_299.weekly_213;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class P_1642 {
@@ -41,19 +39,23 @@ public class P_1642 {
     }
 
     private static boolean f(int[] heights, int bricks, int ladders, int mid) {
-        if (mid == 0) {
-            return true;
-        }
-        final List<Integer> diff = new ArrayList<>();
+        int n = 0;
         for (int i = 0; i < mid; i++) {
             if (heights[i + 1] > heights[i]) {
-                diff.add(heights[i + 1] - heights[i]);
+                n++;
             }
         }
-        diff.sort(Comparator.reverseOrder());
+        final int[] diff = new int[n];
+        int idx = 0;
+        for (int i = 0; i < mid; i++) {
+            if (heights[i + 1] > heights[i]) {
+                diff[idx++] = heights[i + 1] - heights[i];
+            }
+        }
+        Arrays.sort(diff);
         long need = 0;
-        for (int i = ladders; i < diff.size(); i++) {
-            need += diff.get(i);
+        for (int i = 0; i < n - ladders; i++) {
+            need += diff[i];
         }
         return need <= bricks;
     }
