63431925249275731843de4dcee72d8b3caad7e6
nirvanarsc <nirvana_rsc@yahoo.com>
2022-06-23 23:19:30 +0200

    Running on empty - food review! part 728

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_38/P_630.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_38/P_630.java
@@ -9,14 +9,14 @@ public class P_630 {
     public int scheduleCourse(int[][] courses) {
         Arrays.sort(courses, Comparator.comparingInt(val -> val[1]));
         final PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
-        int t = 0;
+        int time = 0;
         for (int[] course : courses) {
-            if (t + course[0] <= course[1]) {
-                t += course[0];
+            if (time + course[0] <= course[1]) {
+                time += course[0];
                 pq.offer(course);
             } else if (!pq.isEmpty() && pq.element()[0] > course[0]) {
                 final int[] curr = pq.remove();
-                t += course[0] - curr[0];
+                time += course[0] - curr[0];
                 pq.offer(course);
             }
         }
