dd580b472b08a1fc35322a6e1c5056a7dc41885b
nirvanarsc <nirvana_rsc@yahoo.com>
2022-06-30 22:22:52 +0200

    Running on empty - food review! part 735

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_10/P_462.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_10/P_462.java
@@ -6,8 +6,9 @@ public class P_462 {
 
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
+        final int n = nums.length;
+        final int median = nums[n / 2];
         int res = 0;
-        final int median = nums[nums.length / 2];
         for (int num : nums) {
             res += Math.abs(num - median);
         }
