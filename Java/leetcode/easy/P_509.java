d54167925bf9b035878f87d108e4afa622cc2eaa
nirvanarsc <nirvana_rsc@yahoo.com>
2022-07-06 23:20:31 +0200

    Running on empty - food review! part 741

--- Java/leetcode/easy/P_509.java
+++ Java/leetcode/easy/P_509.java
@@ -1,23 +1,21 @@
 package leetcode.easy;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public final class P_509 {
 
-    public int fib(int N) {
-        int a = 0;
-        int b = 1;
-        for (int i = 0; i < N; i++) {
-            final int next = a + b;
-            a = b;
-            b = next;
+    public int fib(int n) {
+        final int[] dp = { 0, 1 };
+        for (int i = 0; i < n; i++) {
+            final int next = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = next;
         }
-        return a;
+        return dp[0];
     }
 
     // https://en.wikipedia.org/wiki/Fibonacci_number#Closed-form_expression
-    public int fibClosedForm(int N) {
+    public int fibClosedForm(int n) {
         final double sqrt5 = 2.23606797749979;
         final double goldenRatio = (1 + sqrt5) / 2;
-        return (int) Math.round(Math.pow(goldenRatio, N) / sqrt5);
+        return (int) Math.round(Math.pow(goldenRatio, n) / sqrt5);
     }
 }
