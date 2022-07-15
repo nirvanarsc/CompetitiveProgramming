e6c79a4f59e6b06a1720b03a94bc9ef6eaa3f24b
nirvanarsc <nirvana_rsc@yahoo.com>
2022-07-15 19:49:24 +0300

    Running on empty - food review! part 749

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_53/P_695.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_53/P_695.java
@@ -2,33 +2,81 @@ package leetcode.weekly_contests.weekly_0_99.weekly_53;
 
 public class P_695 {
 
+    private static final class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+        private int count;
+
+        private UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            final int rootP = find(p);
+            final int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+                size[rootQ] = 0;
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+                size[rootP] = 0;
+            }
+            count--;
+        }
+
+        public int count() { return count; }
+
+        public int[] size() { return size; }
+    }
+
     private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
     public int maxAreaOfIsland(int[][] grid) {
         int res = 0;
         final int n = grid.length;
         final int m = grid[0].length;
+        final UnionFind uf = new UnionFind(n * m);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    final int[] temp = { 0 };
-                    dfs(grid, i, j, n, m, temp);
-                    res = Math.max(res, temp[0]);
+                    final int u = i * m + j;
+                    for (int[] dir : DIRS) {
+                        final int nx = i + dir[0];
+                        final int ny = j + dir[1];
+                        if (0 <= nx && nx < n && 0 <= ny && ny < m && grid[nx][ny] == 1) {
+                            final int v = nx * m + ny;
+                            uf.union(u, v);
+                        }
+                    }
                 }
             }
         }
-        return res;
-    }
-
-    private static void dfs(int[][] g, int x, int y, int n, int m, int[] res) {
-        res[0]++;
-        g[x][y] = 0;
-        for (int[] dir : DIRS) {
-            final int nx = x + dir[0];
-            final int ny = y + dir[1];
-            if (0 <= nx && nx < n && 0 <= ny && ny < m && g[nx][ny] == 1) {
-                dfs(g, nx, ny, n, m, res);
+        for (int i = 0; i < n * m; i++) {
+            if (grid[i / m][i % m] == 1) {
+                res = Math.max(res, uf.size[i]);
             }
         }
+        return res;
     }
 }
