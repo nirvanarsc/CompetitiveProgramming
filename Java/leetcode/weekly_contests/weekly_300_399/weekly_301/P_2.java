28a627862cb0790c0b65df1f5062f113a7611c66
nirvanarsc <nirvana_rsc@yahoo.com>
2022-07-20 22:39:59 +0200

    Running on empty - food review! part 754

--- Java/leetcode/weekly_contests/weekly_300_399/weekly_301/P_2.java
+++ Java/leetcode/weekly_contests/weekly_300_399/weekly_301/P_2.java
@@ -1,4 +1,35 @@
 package leetcode.weekly_contests.weekly_300_399.weekly_301;
 
+import java.util.Arrays;
+
+@SuppressWarnings({ "InnerClassMayBeStatic", "unused", "PublicConstructorInNonPublicClass" })
 public class P_2 {
+
+    class SmallestInfiniteSet {
+
+        boolean[] set;
+        int smallest;
+
+        public SmallestInfiniteSet() {
+            set = new boolean[1005];
+            Arrays.fill(set, true);
+            smallest = 1;
+        }
+
+        public int popSmallest() {
+            final int res = smallest;
+            set[smallest++] = false;
+            while (!set[smallest]) {
+                smallest++;
+            }
+            return res;
+        }
+
+        public void addBack(int num) {
+            set[num] = true;
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+    }
 }
--- Java/leetcode/weekly_contests/weekly_300_399/weekly_301/P_4.java
+++ Java/leetcode/weekly_contests/weekly_300_399/weekly_301/P_4.java
@@ -1,4 +1,35 @@
 package leetcode.weekly_contests.weekly_300_399.weekly_301;
 
+import java.util.Arrays;
+
 public class P_4 {
+
+    private static final int MOD = (int) (1e9 + 7);
+
+    public int idealArrays(int n, int maxValue) {
+        final int[][] dp = new int[n + 1][maxValue + 1];
+        dp[0][1] = 1;
+        final int[] coeff = new int[maxValue + 1];
+        for (int prev = 1; prev <= maxValue; prev++) {
+            for (int j = prev; j <= maxValue; j += prev) {
+                coeff[j]++;
+            }
+        }
+        System.out.println(Arrays.toString(coeff));
+        for (int prev = 1; prev <= maxValue; prev++) {
+            for (int i = 0; i < n; i++) {
+                if (dp[i][prev] == 0) {
+                    continue;
+                }
+                for (int j = prev; j <= maxValue; j += prev) {
+                    dp[i + 1][j] = (dp[i + 1][j] + dp[i][prev]) % MOD;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i <= maxValue; i++) {
+            res = (res + dp[n][i]) % MOD;
+        }
+        return res;
+    }
 }
