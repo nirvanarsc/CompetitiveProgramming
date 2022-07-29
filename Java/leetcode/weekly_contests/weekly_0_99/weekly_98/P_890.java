47d13dccda0fbff2097d535450e0836c0f6011d7
nirvanarsc <nirvana_rsc@yahoo.com>
2022-07-29 23:26:39 +0200

    Running on empty - food review! part 763

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_98/P_890.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_98/P_890.java
@@ -9,35 +9,36 @@ public class P_890 {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         final List<String> res = new ArrayList<>();
         final int n = pattern.length();
+        char[] p = pattern.toCharArray();
         for (String w : words) {
-            if (match(pattern, n, w)) {
+            if (match(p, n, w.toCharArray())) {
                 res.add(w);
             }
         }
         return res;
     }
 
-    private static boolean match(String pattern, int n, String w) {
+    private static boolean match(char[] pattern, int n, char[] w) {
         final char[] mapL = new char[26];
         final char[] mapR = new char[26];
         Arrays.fill(mapL, '*');
         Arrays.fill(mapR, '*');
         for (int i = 0; i < n; i++) {
-            final int l = w.charAt(i) - 'a';
-            final int r = pattern.charAt(i) - 'a';
+            final int l = w[i] - 'a';
+            final int r = pattern[i] - 'a';
             if (mapL[l] != '*') {
-                if (mapL[l] != pattern.charAt(i)) {
+                if (mapL[l] != pattern[i]) {
                     return false;
                 }
             } else {
-                mapL[l] = pattern.charAt(i);
+                mapL[l] = pattern[i];
             }
             if (mapR[r] != '*') {
-                if (mapR[r] != w.charAt(i)) {
+                if (mapR[r] != w[i]) {
                     return false;
                 }
             } else {
-                mapR[r] = w.charAt(i);
+                mapR[r] = w[i];
             }
         }
         return true;
