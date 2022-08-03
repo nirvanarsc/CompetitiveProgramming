dd58de77d8e5febc88877d31f62e82750a33d65f
nirvanarsc <nirvana_rsc@yahoo.com>
2022-08-03 22:39:59 +0200

    Running on empty - food review! part 770

---
+++ Java/leetcode/biweekly_contests/biweekly_84/P_2.java
@@ -0,0 +1,18 @@
+package leetcode.biweekly_contests.biweekly_84;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P_2 {
+
+    public long countBadPairs(int[] nums) {
+        long good = 0;
+        final Map<Integer, Integer> f = new HashMap<>();
+        final int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            good += f.getOrDefault(nums[i] - i, 0);
+            f.merge(nums[i] - i, 1, Integer::sum);
+        }
+        return ((long) n * (n - 1) / 2) - good;
+    }
+}
