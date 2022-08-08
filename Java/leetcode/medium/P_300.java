c082e444043b5f5a07a950cf01f486d3aa5ad15e
nirvanarsc <nirvana_rsc@yahoo.com>
2022-08-08 22:22:55 +0200

    Running on empty - food review! part 773

--- Java/leetcode/medium/P_300.java
+++ Java/leetcode/medium/P_300.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class P_300 {
 
     public int lengthOfLIS(int[] nums) {
-        final int[] dp = new int[nums.length];
+        final int n = nums.length;
+        final int[] dp = new int[n];
         int len = 0;
         for (int num : nums) {
             final int idx = lowerBound(dp, num, len);
@@ -60,27 +61,5 @@ public class P_300 {
             res = Math.max(res, dp[i]);
         }
         return res;
-    }
-
-    public static int lengthOfLISTopDown(int[] nums) {
-        return recurse(nums, -1, 0, new Integer[nums.length]);
-    }
-
-    public static int recurse(int[] nums, int prev, int start, Integer[] dp) {
-        if (start == nums.length) {
-            return 0;
-        }
-        if (prev != -1 && dp[prev] != null) {
-            return dp[prev];
-        }
-        int take = 0;
-        if (prev < 0 || nums[start] > nums[prev]) {
-            take = 1 + recurse(nums, start, start + 1, dp);
-        }
-        final int skip = recurse(nums, prev, start + 1, dp);
-        if (prev != -1) {
-            dp[prev] = Math.max(take, skip);
-        }
-        return Math.max(take, skip);
     }
 }
