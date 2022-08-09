9f7dfce42f5c4ecc420bbf8dfe48dd201e54a49b
nirvanarsc <nirvana_rsc@yahoo.com>
2022-08-09 23:40:12 +0200

    Running on empty - food review! part 774

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_81/P_823.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_81/P_823.java
@@ -8,20 +8,22 @@ public class P_823 {
 
     private static final int MOD = (int) (1e9 + 7);
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public int numFactoredBinaryTrees(int[] A) {
-        long res = 0L;
-        Arrays.sort(A);
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
         final Map<Integer, Long> dp = new HashMap<>();
-        for (int i = 0; i < A.length; ++i) {
-            long count = 1;
-            for (int j = 0; j < i; ++j) {
-                if (A[i] % A[j] == 0 && dp.containsKey(A[i] / A[j])) {
-                    count += dp.get(A[j]) * dp.get(A[i] / A[j]);
+        final int n = arr.length;
+        long res = 0L;
+        for (int i = 0; i < n; i++) {
+            long curr = 1;
+            for (int j = 0; j < i; j++) {
+                final Long u = dp.get(arr[j]);
+                final Long v = dp.get(arr[i] / arr[j]);
+                if (arr[i] % arr[j] == 0 && v != null) {
+                    curr = (curr + u * v) % MOD;
                 }
             }
-            dp.put(A[i], count);
-            res = (res + dp.get(A[i])) % MOD;
+            dp.put(arr[i], curr);
+            res = (res + curr) % MOD;
         }
         return (int) res;
     }
