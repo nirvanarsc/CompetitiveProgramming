e9eab667fcd93919bd3b13deba1109ddf221e094
nirvanarsc <nirvana_rsc@yahoo.com>
2022-08-13 22:17:48 +0200

    Running on empty - food review! part 778

--- Java/leetcode/hard/P_30.java
+++ Java/leetcode/hard/P_30.java
@@ -1,7 +1,6 @@
 package leetcode.hard;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,31 +8,30 @@ import java.util.Map;
 public class P_30 {
 
     public List<Integer> findSubstring(String s, String[] words) {
-        if (words.length == 0) {
-            return Collections.emptyList();
-        }
         final List<Integer> res = new ArrayList<>();
-        final Map<String, Integer> wordsFreq = new HashMap<>();
+        final Map<String, Integer> f = new HashMap<>();
         for (String str : words) {
-            wordsFreq.merge(str, 1, Integer::sum);
+            f.merge(str, 1, Integer::sum);
         }
-        final int wordLength = words[0].length();
-        for (int start = 0; start < wordLength; start++) {
-            final Map<String, Integer> currOffset = new HashMap<>(wordsFreq);
-            int matched = words.length;
+        final int n = s.length();
+        final int m = words.length;
+        final int l = words[0].length();
+        for (int start = 0; start < l; start++) {
+            final Map<String, Integer> curr = new HashMap<>(f);
+            int matched = m;
             int j = start;
-            for (int i = start; i <= s.length() - wordLength; i += wordLength) {
-                if (currOffset.merge(s.substring(i, i + wordLength), -1, Integer::sum) >= 0) {
+            for (int i = start; i <= n - l; i += l) {
+                if (curr.merge(s.substring(i, i + l), -1, Integer::sum) >= 0) {
                     matched--;
                 }
                 while (matched == 0) {
-                    if (i - j + wordLength == words.length * wordLength) {
+                    if (i - j + l == m * l) {
                         res.add(j);
                     }
-                    if (currOffset.merge(s.substring(j, j + wordLength), 1, Integer::sum) == 1) {
+                    if (curr.merge(s.substring(j, j + l), 1, Integer::sum) == 1) {
                         matched++;
                     }
-                    j += wordLength;
+                    j += l;
                 }
             }
         }
