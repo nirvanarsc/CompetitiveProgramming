036db29479bfb39574facb8da2c20965bbfc7140
nirvanarsc <nirvana_rsc@yahoo.com>
2022-08-24 23:47:32 +0200

    Running on empty - food review! part 789

--- Java/leetcode/easy/P_326.java
+++ Java/leetcode/easy/P_326.java
@@ -3,14 +3,7 @@ package leetcode.easy;
 public class P_326 {
 
     public boolean isPowerOfThree(int n) {
-        return n > 0 && getMaxPowerOfThree() % n == 0;
-    }
-
-    private static int getMaxPowerOfThree() {
-        int p = 3;
-        while (p * 3 > p) {
-            p *= 3;
-        }
-        return p;
+        // 3^18 = 1162261467
+        return n > 0 && 1162261467 % n == 0;
     }
 }
