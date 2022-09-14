b462d451502d0cae3de0aca09c5c17978657675a
nirvanarsc <nirvana_rsc@yahoo.com>
2022-09-14 23:46:03 +0200

    Running on empty - food review! part 809

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_190/P_1457.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_190/P_1457.java
@@ -1,52 +1,32 @@
 package leetcode.weekly_contests.weekly_100_199.weekly_190;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import utils.DataStructures.TreeNode;
 
+@SuppressWarnings("ConstantConditions")
 public class P_1457 {
 
+    static int res;
+    static int mask;
+
     public int pseudoPalindromicPaths(TreeNode root) {
-        final int[] res = { 0 };
-        final int[] mask = { 0 };
-        dfs(root, res, mask);
-        return res[0];
+        res = 0;
+        mask = 0;
+        dfs(root);
+        return res;
     }
 
-    private static void dfs(TreeNode node, int[] res, int[] mask) {
+    private static void dfs(TreeNode node) {
         if (node == null) {
             return;
         }
-        mask[0] ^= 1 << node.val;
-        dfs(node.left, res, mask);
+        mask ^= 1 << node.val;
+        dfs(node.left);
         if (node.left == null && node.right == null) {
-            if (Integer.bitCount(mask[0]) < 2) {
-                res[0]++;
+            if ((mask & (mask - 1)) == 0) {
+                res++;
             }
         }
-        dfs(node.right, res, mask);
-        mask[0] ^= 1 << node.val;
-    }
-
-    public int pseudoPalindromicPathsMap(TreeNode root) {
-        final int[] res = { 0 };
-        dfs(root, new HashMap<>(), res);
-        return res[0];
-    }
-
-    private static void dfs(TreeNode node, Map<Integer, Integer> freq, int[] res) {
-        if (node == null) {
-            return;
-        }
-        freq.merge(node.val, 1, Integer::sum);
-        dfs(node.left, freq, res);
-        if (node.left == null && node.right == null) {
-            if (freq.values().stream().filter(x -> x % 2 != 0).count() < 2) {
-                res[0]++;
-            }
-        }
-        dfs(node.right, freq, res);
-        freq.merge(node.val, -1, Integer::sum);
+        dfs(node.right);
+        mask ^= 1 << node.val;
     }
 }
