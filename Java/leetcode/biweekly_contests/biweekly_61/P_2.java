c67ea68d35196d66591b5d8f44341eb8e2ce6e88
nirvanarsc <nirvana_rsc@yahoo.com>
2022-09-15 23:02:57 +0200

    Running on empty - food review! part 810

--- Java/leetcode/biweekly_contests/biweekly_61/P_2.java
+++ Java/leetcode/biweekly_contests/biweekly_61/P_2.java
@@ -6,28 +6,29 @@ public class P_2 {
 
     public int[] findOriginalArray(int[] changed) {
         final int n = changed.length;
-        @SuppressWarnings("ZeroLengthArrayAllocation")
-        final int[] empty = new int[0];
+        final int m = (int) (1e5 + 5);
         if (n % 2 != 0) {
-            return empty;
+            //noinspection ZeroLengthArrayAllocation
+            return new int[0];
         }
-        final int[] f = new int[(int) (1e5 + 5)];
+        final int[] f = new int[m];
+        final int[] res = new int[n / 2];
         for (int num : changed) {
             f[num]++;
         }
-        final int[] res = new int[n / 2];
         Arrays.sort(changed);
         int idx = 0;
         for (int num : changed) {
-            if (idx == res.length) {
+            if (idx == n / 2) {
                 break;
             }
             if (f[num] == 0) {
                 continue;
             }
             final int other = 2 * num;
-            if (other >= f.length || f[other] == 0 || (other == 0 && f[0] < 2)) {
-                return empty;
+            if (other >= m || f[other] == 0 || (other == 0 && f[0] < 2)) {
+                //noinspection ZeroLengthArrayAllocation
+                return new int[0];
             }
             res[idx++] = num;
             f[other]--;
