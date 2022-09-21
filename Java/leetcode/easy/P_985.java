3eca781b1dc0ff79f7a2f13e2c7734c9e9892bba
nirvanarsc <nirvana_rsc@yahoo.com>
2022-09-21 23:52:40 +0200

    Running on empty - food review! part 816

--- Java/leetcode/easy/P_985.java
+++ Java/leetcode/easy/P_985.java
@@ -1,26 +1,26 @@
 package leetcode.easy;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_985 {
 
-    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
-        final int[] res = new int[queries.length];
-        int idx = 0;
-        int evenSum = 0;
-        for (int i : A) {
-            if ((i & 1) == 0) {
-                evenSum += i;
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        final int n = queries.length;
+        final int[] res = new int[n];
+        int sum = 0;
+        for (int num : nums) {
+            if ((num & 1) == 0) {
+                sum += num;
             }
         }
-        for (int[] query : queries) {
-            if ((A[query[1]] & 1) == 0) {
-                evenSum -= A[query[1]];
+        for (int i = 0; i < n; i++) {
+            final int[] q = queries[i];
+            if ((nums[q[1]] & 1) == 0) {
+                sum -= nums[q[1]];
             }
-            A[query[1]] += query[0];
-            if ((A[query[1]] & 1) == 0) {
-                evenSum += A[query[1]];
+            nums[q[1]] += q[0];
+            if ((nums[q[1]] & 1) == 0) {
+                sum += nums[q[1]];
             }
-            res[idx++] = evenSum;
+            res[i] = sum;
         }
         return res;
     }
