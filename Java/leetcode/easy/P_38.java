8af641a17154a9c3805637e1186f09b72369d75e
nirvanarsc <nirvana_rsc@yahoo.com>
2022-10-18 23:20:26 +0200

    Running on empty - food review! part 843

--- Java/leetcode/easy/P_38.java
+++ Java/leetcode/easy/P_38.java
@@ -2,28 +2,6 @@ package leetcode.easy;
 
 public class P_38 {
 
-    public String countAndSayOld(int n) {
-        String s = "1";
-        for (int i = 1; i < n; i++) {
-            s = lookAndSay(s);
-        }
-        return s;
-    }
-
-    public String lookAndSay(String str) {
-        final StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            int count = 1;
-            while (i + 1 < str.length() && str.charAt(i) == str.charAt(i + 1)) {
-                i++;
-                count++;
-            }
-            sb.append(count);
-            sb.append(str.charAt(i));
-        }
-        return sb.toString();
-    }
-
     static final String[] dp = new String[30];
 
     public String countAndSay(int n) {
