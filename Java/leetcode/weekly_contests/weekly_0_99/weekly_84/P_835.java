dd92b999b683a64ef15c429ae01418c2542da77b
nirvanarsc <nirvana_rsc@yahoo.com>
2022-10-27 23:32:04 +0200

    Running on empty - food review! part 852

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_84/P_835.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_84/P_835.java
@@ -5,52 +5,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_835 {
 
-    public int largestOverlapMap(int[][] A, int[][] B) {
+    public int largestOverlap(int[][] img1, int[][] img2) {
         int res = 0;
-        final int N = A.length;
-        final List<int[]> listA = new ArrayList<>();
-        final List<int[]> listB = new ArrayList<>();
-        final Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (A[i][j] == 1) {
-                    listA.add(new int[] { i, j });
+        final int n = img1.length;
+        final int m = (int) 1e2;
+        final List<Integer> l = new ArrayList<>();
+        final List<Integer> r = new ArrayList<>();
+        final Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (img1[i][j] == 1) {
+                    l.add(i * m + j);
                 }
-                if (B[i][j] == 1) {
-                    listB.add(new int[] { i, j });
+                if (img2[i][j] == 1) {
+                    r.add(i * m + j);
                 }
             }
         }
-        for (int[] a : listA) {
-            for (int[] b : listB) {
-                final String key = (a[0] - b[0]) + "," + (a[1] - b[1]);
-                res = Math.max(res, map.merge(key, 1, Integer::sum));
-            }
-        }
-        return res;
-    }
-
-    public static int largestOverlap(int[][] A, int[][] B) {
-        int res = 0;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A[i].length; j++) {
-                res = Math.max(res, f(A, B, i, j));
-                res = Math.max(res, f(B, A, i, j));
-            }
-        }
-        return res;
-    }
-
-    private static int f(int[][] A, int[][] B, int i, int j) {
-        int res = 0;
-        for (int k = i; k < A.length; k++) {
-            for (int l = j; l < A[k].length; l++) {
-                if (A[k][l] == 1 && B[k - i][l - j] == 1) {
-                    res++;
-                }
+        for (int a : l) {
+            for (int b : r) {
+                res = Math.max(res, map.merge(a - b, 1, Integer::sum));
             }
         }
         return res;
