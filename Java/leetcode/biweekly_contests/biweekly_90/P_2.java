1d0953b8fc4fdc44b7ac38ad4c87670ff3a82fe8
nirvanarsc <nirvana_rsc@yahoo.com>
2022-10-29 21:34:16 +0200

    Running on empty - food review! part 854

---
+++ Java/leetcode/biweekly_contests/biweekly_90/P_2.java
@@ -0,0 +1,28 @@
+package leetcode.biweekly_contests.biweekly_90;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_2 {
+
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        final List<String> res = new ArrayList<>();
+        final int n = queries[0].length();
+        outer:
+        for (String q : queries) {
+            for (String d : dictionary) {
+                int diff = 0;
+                for (int i = 0; i < n; i++) {
+                    if (q.charAt(i) != d.charAt(i)) {
+                        diff++;
+                    }
+                }
+                if (diff < 3) {
+                    res.add(q);
+                    continue outer;
+                }
+            }
+        }
+        return res;
+    }
+}
