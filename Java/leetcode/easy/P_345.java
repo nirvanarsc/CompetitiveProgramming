e8dbe049f0be571e1af64de1edcae427340a89a6
nirvanarsc <nirvana_rsc@yahoo.com>
2022-11-04 23:49:46 +0100

    Running on empty - food review! part 860

--- Java/leetcode/easy/P_345.java
+++ Java/leetcode/easy/P_345.java
@@ -3,22 +3,24 @@ package leetcode.easy;
 public class P_345 {
 
     public String reverseVowels(String s) {
-        int start = 0, end = s.length() - 1;
+        final int n = s.length();
+        int l = 0;
+        int r = n - 1;
         final String vowels = "aeiouAEIOU";
-        final char[] string = s.toCharArray();
-        while (start < end) {
-            if (vowels.indexOf(string[start]) == -1) {
-                start++;
-            } else if (vowels.indexOf(string[end]) == -1) {
-                end--;
+        final char[] w = s.toCharArray();
+        while (l < r) {
+            if (vowels.indexOf(w[l]) == -1) {
+                l++;
+            } else if (vowels.indexOf(w[r]) == -1) {
+                r--;
             } else {
-                final char t = string[start];
-                string[start] = string[end];
-                string[end] = t;
-                start++;
-                end--;
+                final char t = w[l];
+                w[l] = w[r];
+                w[r] = t;
+                l++;
+                r--;
             }
         }
-        return new String(string);
+        return new String(w);
     }
 }
