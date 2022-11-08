59acc4d989879f6a904f5550f0559f16a5465cf2
nirvanarsc <nirvana_rsc@yahoo.com>
2022-11-08 23:47:22 +0100

    Running on empty - food review! part 864

--- Java/leetcode/weekly_contests/weekly_200_299/weekly_201/P_1544.java
+++ Java/leetcode/weekly_contests/weekly_200_299/weekly_201/P_1544.java
@@ -6,18 +6,20 @@ import java.util.Deque;
 public class P_1544 {
 
     public String makeGood(String s) {
-        final Deque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (!stack.isEmpty() && Math.abs(stack.peek() - s.charAt(i)) == 32) {
-                stack.removeFirst();
+        final Deque<Character> dq = new ArrayDeque<>();
+        final int n = s.length();
+        final char[] w = s.toCharArray();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && Math.abs(dq.getFirst() - w[i]) == 32) {
+                dq.removeFirst();
             } else {
-                stack.addFirst(s.charAt(i));
+                dq.addFirst(w[i]);
             }
         }
-        final char[] res = new char[stack.size()];
-        int index = stack.size() - 1;
-        while (!stack.isEmpty()) {
-            res[index--] = stack.removeFirst();
+        final char[] res = new char[dq.size()];
+        int index = dq.size() - 1;
+        while (!dq.isEmpty()) {
+            res[index--] = dq.removeFirst();
         }
         return new String(res);
     }
