ab7b5beadf36a9a75b3bf4258b2d97086198f931
nirvanarsc <nirvana_rsc@yahoo.com>
2022-11-11 22:00:25 +0100

    Running on empty - food review! part 867

--- Java/leetcode/easy/P_26.java
+++ Java/leetcode/easy/P_26.java
@@ -3,23 +3,14 @@ package leetcode.easy;
 public class P_26 {
 
     public int removeDuplicates(int[] nums) {
+        final int n = nums.length;
         int res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+        for (int i = 0; i < n; i++) {
+            while (i + 1 < n && nums[i] == nums[i + 1]) {
                 i++;
             }
             nums[res++] = nums[i];
         }
         return res;
-    }
-
-    public int removeDuplicatesOld(int[] nums) {
-        int k = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[k]) {
-                nums[++k] = nums[i];
-            }
-        }
-        return k + 1;
     }
 }
