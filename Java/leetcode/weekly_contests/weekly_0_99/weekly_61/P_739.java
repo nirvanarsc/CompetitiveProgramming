ab88334ae4a38e00cac0bff64fb30fe50071b954
nirvanarsc <nirvana_rsc@yahoo.com>
2022-12-18 23:52:55 +0100

    Running on empty - food review! part 905

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_61/P_739.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_61/P_739.java
@@ -4,15 +4,15 @@ public class P_739 {
 
     public int[] dailyTemperatures(int[] temperatures) {
         final int n = temperatures.length;
-        final int[] stack = new int[n];
+        final int[] stack = new int[n + 1];
         final int[] res = new int[n];
-        int stackIdx = 0;
+        int idx = 1;
         for (int i = n - 1; i >= 0; i--) {
-            while (stackIdx > 0 && temperatures[i] >= temperatures[stack[stackIdx - 1]]) {
-                stackIdx--;
+            while (idx > 1 && temperatures[i] >= temperatures[stack[idx - 1]]) {
+                idx--;
             }
-            res[i] = stackIdx == 0 ? 0 : stack[stackIdx - 1] - i;
-            stack[stackIdx++] = i;
+            res[i] = Math.max(0, stack[idx - 1] - i);
+            stack[idx++] = i;
         }
         return res;
     }
