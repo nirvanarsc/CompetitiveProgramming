5c23428f799d01a620ca0425d8f2782a58033c1a
nirvanarsc <nirvana_rsc@yahoo.com>
2022-12-20 22:35:09 +0100

    Running on empty - food review! part 907

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_86/P_841.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_86/P_841.java
@@ -1,38 +1,18 @@
 package leetcode.weekly_contests.weekly_0_99.weekly_86;
 
-import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.Deque;
 import java.util.List;
 
 public class P_841 {
 
-    public boolean canVisitAllRoomsBFS(List<List<Integer>> rooms) {
-        final int n = rooms.size();
-        final Deque<Integer> q = new ArrayDeque<>(Collections.singleton(0));
-        final boolean[] seen = new boolean[n];
-        while (!q.isEmpty()) {
-            final int curr = q.removeFirst();
-            if (seen[curr]) {
-                continue;
-            }
-            seen[curr] = true;
-            for (int key : rooms.get(curr)) {
-                q.offerLast(key);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (!seen[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+    static int n;
+    static List<List<Integer>> g;
+    static boolean[] seen;
 
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        final int n = rooms.size();
-        final boolean[] seen = new boolean[n];
-        dfs(0, rooms, seen);
+        n = rooms.size();
+        g = rooms;
+        seen = new boolean[n];
+        dfs(0);
         for (int i = 0; i < n; i++) {
             if (!seen[i]) {
                 return false;
@@ -41,13 +21,13 @@ public class P_841 {
         return true;
     }
 
-    private static void dfs(int u, List<List<Integer>> g, boolean[] seen) {
+    private static void dfs(int u) {
         if (seen[u]) {
             return;
         }
         seen[u] = true;
         for (int v : g.get(u)) {
-            dfs(v, g, seen);
+            dfs(v);
         }
     }
 }
