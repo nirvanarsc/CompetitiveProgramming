ad9e3fbb3a3fe85f154b98294c0f879d30ba705c
nirvanarsc <nirvana_rsc@yahoo.com>
2023-01-14 18:18:22 +0100

    Running on empty - food review! part 932

--- Java/leetcode/medium/P_1061.java
+++ Java/leetcode/medium/P_1061.java
@@ -1,28 +1,77 @@
 package leetcode.medium;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.TreeSet;
-
-import utils.DataStructures.UnionFind;
 
 public class P_1061 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public String smallestEquivalentString(String A, String B, String S) {
-        final Map<Character, TreeSet<Character>> map = new HashMap<>();
+    private static final class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+        private int count;
+
+        private UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int p) {
+            // path compression
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int p, int q) {
+            final int rootP = find(p);
+            final int rootQ = find(q);
+            if (rootP == rootQ) {
+                return;
+            }
+            // union by size
+            if (size[rootP] > size[rootQ]) {
+                parent[rootQ] = rootP;
+                size[rootP] += size[rootQ];
+                size[rootQ] = 0;
+            } else {
+                parent[rootP] = rootQ;
+                size[rootQ] += size[rootP];
+                size[rootP] = 0;
+            }
+            count--;
+        }
+
+        public int count() { return count; }
+
+        public int[] size() { return size; }
+    }
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        final char[] l = s1.toCharArray();
+        final char[] r = s2.toCharArray();
+        final int n = s1.length();
+        @SuppressWarnings("unchecked")
+        final TreeSet<Character>[] ts = new TreeSet[26];
         final UnionFind uf = new UnionFind(26);
-        for (int i = 0; i < A.length(); i++) {
-            uf.union(A.charAt(i) - 'a', B.charAt(i) - 'a');
-            map.computeIfAbsent((char) (uf.find(A.charAt(i) - 'a') + 'a'),
-                                v -> new TreeSet<>()).addAll(Arrays.asList(A.charAt(i), B.charAt(i)));
+        for (int i = 0; i < 26; i++) {
+            ts[i] = new TreeSet<>();
+        }
+        for (int i = 0; i < n; i++) {
+            uf.union(l[i] - 'a', r[i] - 'a');
+            final int root = uf.find(l[i] - 'a');
+            ts[root].add(l[i]);
+            ts[root].add(r[i]);
         }
         final StringBuilder sb = new StringBuilder();
-        for (char c : S.toCharArray()) {
-            sb.append(map.getOrDefault((char)(uf.find(c-'a')+'a'),
-                                       new TreeSet<>(Collections.singletonList(c))).first());
+        for (char c : baseStr.toCharArray()) {
+            final int root = uf.find(c - 'a');
+            sb.append(ts[root].isEmpty() ? c : ts[root].first());
         }
         return sb.toString();
     }
--- Java/leetcode/weekly_contests/weekly_300_399/weekly_327/P_4.java
+++ Java/leetcode/weekly_contests/weekly_300_399/weekly_327/P_4.java
@@ -1,4 +1,60 @@
 package leetcode.weekly_contests.weekly_300_399.weekly_327;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class P_4 {
+
+    public int findCrossingTime(int n, int k, int[][] time) {
+        int res = 0;
+        int free = 0;
+        final PriorityQueue<int[]> l = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        final PriorityQueue<int[]> r = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        final PriorityQueue<int[]> ll = new PriorityQueue<>((a, b) -> a[0] != b[0]
+                                                                      ? Integer.compare(b[0], a[0])
+                                                                      : Integer.compare(b[1], a[1]));
+        final PriorityQueue<int[]> rr = new PriorityQueue<>((a, b) -> a[0] != b[0]
+                                                                      ? Integer.compare(b[0], a[0])
+                                                                      : Integer.compare(b[1], a[1]));
+        for (int i = 0; i < k; i++) {
+            ll.add(new int[] { time[i][0] + time[i][2], i });
+        }
+        while (n > 0 || !r.isEmpty() || !rr.isEmpty()) {
+            if (rr.isEmpty()
+                && (r.isEmpty() || r.element()[0] > free)
+                && (n == 0 || ll.isEmpty() && (l.isEmpty() || l.element()[0] > free))) {
+                int curr = (int) 2e9;
+                if (n > 0 && !l.isEmpty()) {
+                    curr = Math.min(curr, l.element()[0]);
+                }
+                if (!r.isEmpty()) {
+                    curr = Math.min(curr, r.element()[0]);
+                }
+                free = curr;
+            }
+            while (!l.isEmpty() && l.element()[0] <= free) {
+                final int idx = l.remove()[1];
+                ll.add(new int[] { time[idx][0] + time[idx][2], idx });
+            }
+            while (!r.isEmpty() && r.element()[0] <= free) {
+                final int idx = r.remove()[1];
+                rr.add(new int[] { time[idx][0] + time[idx][2], idx });
+            }
+            if (!rr.isEmpty()) {
+                final int idx = rr.remove()[1];
+                free += time[idx][2];
+                if (n > 0) {
+                    l.add(new int[] { free + time[idx][3], idx });
+                } else {
+                    res = Math.max(res, free);
+                }
+            } else {
+                final int idx = ll.remove()[1];
+                free += time[idx][0];
+                r.add(new int[] { free + time[idx][1], idx });
+                n--;
+            }
+        }
+        return res;
+    }
 }
