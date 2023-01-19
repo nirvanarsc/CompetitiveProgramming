98ae7b45a63f3ce755a661804384cede40d380c8
nirvanarsc <nirvana_rsc@yahoo.com>
2023-01-19 22:11:38 +0100

    Running on empty - food review! part 937

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_119/P_974.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_119/P_974.java
@@ -1,32 +1,15 @@
 package leetcode.weekly_contests.weekly_100_199.weekly_119;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public class P_974 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public int subarraysDivByK(int[] A, int K) {
-        final int[] remainders = new int[K];
-        remainders[0] = 1;
+    public int subarraysDivByK(int[] nums, int k) {
+        final int[] f = new int[k];
+        f[0] = 1;
         int res = 0, sum = 0;
-        for (int num : A) {
-            sum += num;
-            final int r = ((sum % K) + K) % K;
-            res += remainders[r];
-            remainders[r]++;
-        }
-        return res;
-    }
-
-    public int subarraysDivByKMap(int[] nums, int k) {
-        final Map<Integer, Integer> map = new HashMap<>(Collections.singletonMap(0, 1));
-        int sum = 0, res = 0;
         for (int num : nums) {
-            sum += num;
-            res += map.getOrDefault(Math.floorMod(sum, k), 0);
-            map.merge(Math.floorMod(sum, k), 1, Integer::sum);
+            sum = (sum + num) % k;
+            sum = (sum + k) % k;
+            res += f[sum]++;
         }
         return res;
     }
