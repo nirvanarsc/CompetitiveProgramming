ab0ef436d40575a3dfbdd0afc845c6f15aeaecf7
nirvanarsc <nirvana_rsc@yahoo.com>
2023-02-07 23:47:01 +0100

    Running on empty - food review! part 955

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_102/P_904.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_102/P_904.java
@@ -3,14 +3,15 @@ package leetcode.weekly_contests.weekly_100_199.weekly_102;
 public class P_904 {
 
     public int totalFruit(int[] tree) {
+        final int n = tree.length;
+        final int[] seen = new int[n];
         int j = 0;
         int res = 0;
-        final int[] seen = new int[40000];
-        int window = 2;
-        for (int i = 0; i < tree.length; i++) {
-            window -= seen[tree[i]]++ == 0 ? 1 : 0;
-            while (window < 0) {
-                window += --seen[tree[j++]] == 0 ? 1 : 0;
+        int curr = 2;
+        for (int i = 0; i < n; i++) {
+            curr -= seen[tree[i]]++ == 0 ? 1 : 0;
+            while (curr < 0) {
+                curr += --seen[tree[j++]] == 0 ? 1 : 0;
             }
             res = Math.max(res, i - j + 1);
         }
