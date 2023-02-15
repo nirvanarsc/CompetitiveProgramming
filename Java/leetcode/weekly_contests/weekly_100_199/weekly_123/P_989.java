d2f117e4e690531dbf4ea434b647e3d95f29cb93
nirvanarsc <nirvana_rsc@yahoo.com>
2023-02-15 23:29:27 +0100

    Running on empty - food review! part 964

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_123/P_989.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_123/P_989.java
@@ -1,18 +1,22 @@
 package leetcode.weekly_contests.weekly_100_199.weekly_123;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class P_989 {
 
-    @SuppressWarnings("MethodParameterNamingConvention")
-    public List<Integer> addToArrayForm(int[] A, int K) {
-        final LinkedList<Integer> ans = new LinkedList<>();
-        for (int i = A.length - 1; K > 0 || i >= 0; i--, K /= 10) {
-            if (i >= 0) { K += A[i]; }
-            ans.offerFirst(K % 10);
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        final int n = num.length;
+        final List<Integer> res = new ArrayList<>();
+        for (int i = n - 1; k > 0 || i >= 0; i--, k /= 10) {
+            if (i >= 0) {
+                k += num[i];
+            }
+            res.add(k % 10);
         }
-        return ans;
+        Collections.reverse(res);
+        return res;
     }
 }
 
