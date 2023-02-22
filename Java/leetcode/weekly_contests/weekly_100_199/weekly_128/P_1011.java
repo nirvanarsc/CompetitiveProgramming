41b9df421833edeb41591e9678db2215d7e71498
nirvanarsc <nirvana_rsc@yahoo.com>
2023-02-22 23:56:22 +0100

    Running on empty - food review! part 971

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_128/P_1011.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_128/P_1011.java
@@ -1,13 +1,13 @@
 package leetcode.weekly_contests.weekly_100_199.weekly_128;
 
-@SuppressWarnings("MethodParameterNamingConvention")
 public class P_1011 {
 
-    public int shipWithinDays(int[] weights, int D) {
-        int lo = 0, hi = (int) 1e9;
+    public int shipWithinDays(int[] weights, int days) {
+        int lo = 0;
+        int hi = (int) 1e9;
         while (lo < hi) {
             final int mid = lo + hi >>> 1;
-            if (!f(weights, mid, D)) {
+            if (f(weights, mid) > days) {
                 lo = mid + 1;
             } else {
                 hi = mid;
@@ -16,23 +16,19 @@ public class P_1011 {
         return lo;
     }
 
-    private static boolean f(int[] weights, int mid, int D) {
+    private static int f(int[] weights, int mid) {
         int curr = 0;
-        int days = 0;
+        int res = 0;
         for (int w : weights) {
             if (w > mid) {
-                return false;
+                return (int) 1e9;
             }
-            if (curr + w > mid) {
+            curr += w;
+            if (curr > mid) {
                 curr = w;
-                days++;
-            } else {
-                curr += w;
+                res++;
             }
         }
-        if (curr > 0) {
-            days++;
-        }
-        return days <= D;
+        return res + (curr > 0 ? 1 : 0);
     }
 }
