28a04b2b5968c2ad816a49ebebdb7a21bd4ea14e
nirvanarsc <nirvana_rsc@yahoo.com>
2023-03-02 23:33:00 +0100

    Running on empty - food review! part 979

--- Java/leetcode/weekly_contests/weekly_0_99/weekly_56/P_443.java
+++ Java/leetcode/weekly_contests/weekly_0_99/weekly_56/P_443.java
@@ -3,22 +3,21 @@ package leetcode.weekly_contests.weekly_0_99.weekly_56;
 public class P_443 {
 
     public int compress(char[] chars) {
+        final int n = chars.length;
         int j = 0;
-        for (int i = 0; i < chars.length; i++) {
-            int count = 0;
+        for (int i = 0; i < n; i++) {
             int k = i;
-            while (k < chars.length && chars[i] == chars[k]) {
-                count++;
+            while (k < n && chars[i] == chars[k]) {
                 k++;
             }
             chars[j++] = chars[i];
-            if (count > 1) {
-                final String c = String.valueOf(count);
+            if (k - i > 1) {
+                final String c = String.valueOf(k - i);
                 for (int l = 0; l < c.length(); l++) {
                     chars[j++] = c.charAt(l);
                 }
-                i = k - 1;
             }
+            i = k - 1;
         }
         return j;
     }
