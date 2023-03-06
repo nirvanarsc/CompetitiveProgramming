15e7fdbb2fb160def1b1d40cd7b6e0fef6361b60
nirvanarsc <nirvana_rsc@yahoo.com>
2023-03-06 22:31:05 +0100

    Running on empty - food review! part 983

--- Java/leetcode/biweekly_contests/biweekly_32/P_1539.java
+++ Java/leetcode/biweekly_contests/biweekly_32/P_1539.java
@@ -2,23 +2,31 @@ package leetcode.biweekly_contests.biweekly_32;
 
 public class P_1539 {
 
-    // lowerBound
     public int findKthPositive(int[] arr, int k) {
+        int lo = 1;
+        int hi = 2000;
+        while (lo < hi) {
+            final int mid = lo + hi + 1 >>> 1;
+            final int missing = mid - lowerBound(arr, mid);
+            if (missing > k) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
+            }
+        }
+        return lo;
+    }
+
+    private static int lowerBound(int[] arr, int target) {
         int lo = 0, hi = arr.length;
         while (lo < hi) {
             final int mid = lo + hi >>> 1;
-            final int missing = getMissing(arr, mid);
-            if (missing < k) {
+            if (arr[mid] < target) {
                 lo = mid + 1;
             } else {
                 hi = mid;
             }
         }
-        // lo + k = (arr[lo-1] - (arr[lo-1] - (lo-1 + 1))) + k
-        return lo == 0 ? k : (arr[lo - 1] - getMissing(arr, lo - 1) + k);
-    }
-
-    private static int getMissing(int[] arr, int mid) {
-        return arr[mid] - (mid + 1);
+        return lo;
     }
 }
