5b1d8f1b560bab81a7f41f7472164a0710523f5e
nirvanarsc <nirvana_rsc@yahoo.com>
2023-03-24 22:39:59 +0200

    Running on empty - food review! part 1001

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_191/P_1466.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_191/P_1466.java
@@ -1,39 +1,47 @@
 package leetcode.weekly_contests.weekly_100_199.weekly_191;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public class P_1466 {
 
+    static int n;
+    static int[][] edges;
+    static int[][][] g;
+
     public int minReorder(int n, int[][] connections) {
-        final Set<String> set = new HashSet<>();
-        final Map<Integer, List<Integer>> g = new HashMap<>();
-        for (int[] c : connections) {
-            g.computeIfAbsent(c[0], v -> new ArrayList<>()).add(c[1]);
-            g.computeIfAbsent(c[1], v -> new ArrayList<>()).add(c[0]);
-            set.add(c[0] + " " + c[1]);
+        //noinspection AccessStaticViaInstance
+        this.n = n;
+        edges = new int[2 * (n - 1)][3];
+        for (int i = 0; i < (n - 1); i++) {
+            final int u = connections[i][0];
+            final int v = connections[i][1];
+            edges[2 * i] = new int[] { u, v, 1 };
+            edges[2 * i + 1] = new int[] { v, u, 0 };
         }
-        final Deque<int[]> q = new ArrayDeque<>(Collections.singleton(new int[] { 0, -1 }));
-        final Set<Integer> visited = new HashSet<>(Collections.singleton(0));
+        g = packG();
+        return dfs(0, -1);
+    }
+
+    private static int dfs(int u, int p) {
         int res = 0;
-        while (!q.isEmpty()) {
-            final int[] ints = q.removeFirst();
-            if (ints[1] != -1 && !set.contains(ints[0] + " " + ints[1])) {
-                res++;
-            }
-            for (int next : g.getOrDefault(ints[0], Collections.emptyList())) {
-                if (visited.add(next)) {
-                    q.offerLast(new int[] { next, ints[0] });
-                }
+        for (int[] v : g[u]) {
+            if (v[0] != p) {
+                res += v[1] + dfs(v[0], u);
             }
         }
         return res;
+    }
+
+    private static int[][][] packG() {
+        final int[][][] g = new int[n][][];
+        final int[] size = new int[n];
+        for (int[] edge : edges) {
+            ++size[edge[0]];
+        }
+        for (int i = 0; i < n; i++) {
+            g[i] = new int[size[i]][2];
+        }
+        for (int[] edge : edges) {
+            g[edge[0]][--size[edge[0]]] = new int[] { edge[1], edge[2] };
+        }
+        return g;
     }
 }
