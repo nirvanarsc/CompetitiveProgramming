484f66665d755064972488c4c5f2be4242c7666b
nirvanarsc <nirvana_rsc@yahoo.com>
2023-04-07 23:05:56 +0200

    Running on empty - food review! part 1009

--- Java/leetcode/weekly_contests/weekly_100_199/weekly_130/P_1020.java
+++ Java/leetcode/weekly_contests/weekly_100_199/weekly_130/P_1020.java
@@ -4,31 +4,33 @@ public class P_1020 {
 
     private static final int[][] DIRS = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
-    public int numEnclaves(int[][] A) {
-        final int n = A.length;
-        final int m = A[0].length;
+    public int numEnclaves(int[][] grid) {
+        final int n = grid.length;
+        final int m = grid[0].length;
         int res = 0;
         for (int i = 0; i < n; i++) {
-            if (A[i][0] == 1) { fill(A, i, 0); }
-            if (A[i][m - 1] == 1) { fill(A, i, m - 1); }
+            if (grid[i][0] == 1) { fill(grid, i, 0); }
+            if (grid[i][m - 1] == 1) { fill(grid, i, m - 1); }
         }
         for (int i = 0; i < m; i++) {
-            if (A[0][i] == 1) { fill(A, 0, i); }
-            if (A[n - 1][i] == 1) { fill(A, m - 1, i); }
+            if (grid[0][i] == 1) { fill(grid, 0, i); }
+            if (grid[n - 1][i] == 1) { fill(grid, n - 1, i); }
         }
-        for (int[] ints : A) {
-            for (int i = 0; i < m; i++) { res += ints[i]; }
+        for (int[] row : grid) {
+            for (int j = 0; j < m; j++) {
+                res += row[j];
+            }
         }
         return res;
     }
 
-    private static void fill(int[][] A, int r, int c) {
-        if (r < 0 || r >= A.length || c < 0 || c >= A[0].length || A[r][c] == 0) {
+    private static void fill(int[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) {
             return;
         }
-        A[r][c] = 0;
+        grid[r][c] = 0;
         for (int[] dir : DIRS) {
-            fill(A, r + dir[0], c + dir[1]);
+            fill(grid, r + dir[0], c + dir[1]);
         }
     }
 }
