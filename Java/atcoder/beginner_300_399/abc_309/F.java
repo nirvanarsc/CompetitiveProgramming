1d3dc4408636736a5bbfa52b1c97fa3a3eae6bdb
nirvanarsc <nirvana_rsc@yahoo.com>
2023-09-02 21:12:26 +0200

    Running on empty - food review! part 1037

---
+++ Java/atcoder/beginner_300_399/abc_309/F.java
@@ -0,0 +1,290 @@
+package atcoder.beginner_300_399.abc_309;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public final class F {
+
+    private static class SegTree {
+        int leftMost, rightMost;
+        SegTree left, right;
+        int max;
+
+        SegTree(int leftMost, int rightMost, int[] arr) {
+            this.leftMost = leftMost;
+            this.rightMost = rightMost;
+            if (leftMost == rightMost) {
+                max = arr[leftMost];
+            } else {
+                final int mid = leftMost + rightMost >>> 1;
+                left = new SegTree(leftMost, mid, arr);
+                right = new SegTree(mid + 1, rightMost, arr);
+                recalc();
+            }
+        }
+
+        private void recalc() {
+            if (leftMost == rightMost) {
+                return;
+            }
+            max = Math.max(left.max, right.max);
+        }
+
+        private int query(int l, int r) {
+            if (r < leftMost || l > rightMost) {
+                return (int) -1e9;
+            }
+            if (l <= leftMost && rightMost <= r) {
+                return max;
+            }
+            return Math.max(left.query(l, r), right.query(l, r));
+        }
+
+        private void update(int idx, int val) {
+            if (leftMost == rightMost) {
+                max = val;
+            } else {
+                final int mid = leftMost + rightMost >>> 1;
+                if (idx <= mid) {
+                    left.update(idx, val);
+                } else {
+                    right.update(idx, val);
+                }
+                recalc();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        final FastReader fs = new FastReader();
+        final int n = fs.nextInt();
+        final int[][] g = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            g[i] = fs.nextIntArray(3);
+            Arrays.sort(g[i]);
+        }
+        Arrays.sort(g, Comparator.comparingInt(a -> a[1]));
+        final Map<String, Integer> map = new HashMap<>();
+        final Map<Integer, Integer> r = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(f(g[i]), i);
+            r.put(g[i][1], i);
+        }
+        Arrays.sort(g, Comparator.comparingInt(a -> a[0]));
+        final SegTree st = new SegTree(0, n - 1, new int[n]);
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            st.update(map.get(f(g[i])), g[i][2]);
+        }
+        for (int i = 0; i < n; i++) {
+            while (j < n && g[i][0] == g[j][0]) {
+                st.update(map.get(f(g[j++])), 0);
+            }
+            if (g[i][2] < st.query(r.get(g[i][1]) + 1, n - 1)) {
+                System.out.println("Yes");
+                return;
+            }
+        }
+        System.out.println("No");
+    }
+
+    private static String f(int[] u) {
+        return u[0] + "," + u[1] + ',' + u[2];
+    }
+
+    static final class Utils {
+        private static class Shuffler {
+            private static void shuffle(int[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void shuffle(long[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void swap(int[] x, int i, int j) {
+                final int t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+
+            private static void swap(long[] x, int i, int j) {
+                final long t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+
+        public static void shuffleSort(int[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        private Utils() {}
+    }
+
+    static class FastReader {
+        private static final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            final byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int readSign() throws IOException {
+            byte c = read();
+            while ('+' != c && '-' != c) {
+                c = read();
+            }
+            return '+' == c ? 0 : 1;
+        }
+
+        private static boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+
+        private int skip() throws IOException {
+            int b;
+            //noinspection StatementWithEmptyBody
+            while ((b = read()) != -1 && isSpaceChar(b)) {}
+            return b;
+        }
+
+        public char nc() throws IOException {
+            return (char) skip();
+        }
+
+        public String next() throws IOException {
+            int b = skip();
+            final StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b)) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = read();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public int[] nextIntArray(int n) throws IOException {
+            final int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = nextInt();
+            }
+            return res;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public long[] nextLongArray(int n) throws IOException {
+            final long[] res = new long[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = nextLong();
+            }
+            return res;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) { buffer[0] = -1; }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) { fillBuffer(); }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
+        }
+    }
+}
