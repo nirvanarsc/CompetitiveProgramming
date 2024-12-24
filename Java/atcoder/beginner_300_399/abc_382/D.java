c6822c47bb9ec3a2235a7817b6d194b7a1569e0c
nirvanarsc <nirvana_rsc@yahoo.com>
2024-12-24 17:08:32 +0100

    Running on empty - food review! part 1070

---
+++ Java/atcoder/beginner_300_399/abc_382/D.java
@@ -0,0 +1,242 @@
+package atcoder.beginner_300_399.abc_382;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public final class D {
+
+    static int n;
+    static int m;
+    static StringBuilder res;
+    static int total;
+
+    public static void main(String[] args) throws IOException {
+        final FastReader fs = new FastReader();
+        n = fs.nextInt();
+        m = fs.nextInt();
+        res = new StringBuilder();
+        total = 0;
+        final List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.add(i);
+            dfs(i, list, 1);
+            list.remove(list.size() - 1);
+        }
+        System.out.println(total);
+        System.out.println(res);
+    }
+
+    private static void dfs(int u, List<Integer> list, int v) {
+        if (u + (n - v) * 10 > m) {
+            return;
+        }
+        if (v == n) {
+            total++;
+            for (int vv : list) {
+                res.append(vv).append(' ');
+            }
+            res.setCharAt(res.length() - 1, '\n');
+            return;
+        }
+        for (int k = 0; k < 10; k++) {
+            list.add(u + 10 + k);
+            dfs(u + 10 + k, list, v + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    static final class Utils {
+        private static class Shuffler {
+            private static void shuffle(int[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void shuffle(long[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void swap(int[] x, int i, int j) {
+                final int t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+
+            private static void swap(long[] x, int i, int j) {
+                final long t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+
+        public static void shuffleSort(int[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        private Utils() {}
+    }
+
+    static class FastReader {
+        private static final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            final byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int readSign() throws IOException {
+            byte c = read();
+            while ('+' != c && '-' != c) {
+                c = read();
+            }
+            return '+' == c ? 0 : 1;
+        }
+
+        private static boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+
+        private int skip() throws IOException {
+            int b;
+            //noinspection StatementWithEmptyBody
+            while ((b = read()) != -1 && isSpaceChar(b)) {}
+            return b;
+        }
+
+        public char nc() throws IOException {
+            return (char) skip();
+        }
+
+        public String next() throws IOException {
+            int b = skip();
+            final StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b)) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = read();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public int[] nextIntArray(int n) throws IOException {
+            final int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = nextInt();
+            }
+            return res;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public long[] nextLongArray(int n) throws IOException {
+            final long[] res = new long[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = nextLong();
+            }
+            return res;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) { buffer[0] = -1; }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) { fillBuffer(); }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
+        }
+    }
+}
---
+++ Java/leetcode/hard/P_3203.java
@@ -0,0 +1,58 @@
+package leetcode.hard;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class P_3203 {
+
+    public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
+        final int u = diameter(edges1);
+        final int v = diameter(edges2);
+        return Math.max(Math.max(u, v), 1 + ((u + 1) / 2) + ((v + 1) / 2));
+    }
+
+    private static int diameter(int[][] edges) {
+        final int[][] g = packG(edges, edges.length + 1);
+        final int[] bfs1 = bfs(0, g);
+        final int[] bfs2 = bfs(bfs1[0], g);
+        return bfs2[1];
+    }
+
+    private static int[] bfs(int u, int[][] g) {
+        final Deque<int[]> dq = new ArrayDeque<>();
+        final int[] res = { -1, -1 };
+        dq.offerLast(new int[] { u, -1 });
+        for (int level = 0; !dq.isEmpty(); level++) {
+            for (int size = dq.size(); size > 0; size--) {
+                final int[] curr = dq.removeFirst();
+                final int node = curr[0];
+                final int par = curr[1];
+                res[0] = node;
+                res[1] = level;
+                for (int next : g[node]) {
+                    if (next != par) {
+                        dq.offerLast(new int[] { next, node });
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private static int[][] packG(int[][] edges, int n) {
+        final int[][] g = new int[n][];
+        final int[] size = new int[n];
+        for (int[] edge : edges) {
+            ++size[edge[0]];
+            ++size[edge[1]];
+        }
+        for (int i = 0; i < n; i++) {
+            g[i] = new int[size[i]];
+        }
+        for (int[] edge : edges) {
+            g[edge[0]][--size[edge[0]]] = edge[1];
+            g[edge[1]][--size[edge[1]]] = edge[0];
+        }
+        return g;
+    }
+}
