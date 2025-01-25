4a26fe143bc505f9d79e26e850a677704e063c38
nirvanarsc <nirvana_rsc@yahoo.com>
2025-01-25 22:41:46 +0100

    Running on empty - food review! part 1073

---
+++ Java/atcoder/beginner_300_399/abc_385/D.java
@@ -0,0 +1,284 @@
+package atcoder.beginner_300_399.abc_385;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeSet;
+
+public final class D {
+
+    public static void main(String[] args) throws IOException {
+        final FastReader fs = new FastReader();
+        final int n = fs.nextInt();
+        final int m = fs.nextInt();
+        long sx = fs.nextInt();
+        long sy = fs.nextInt();
+        final Map<Long, TreeSet<Long>> gx = new HashMap<>();
+        final Map<Long, TreeSet<Long>> gy = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            final long x = fs.nextInt();
+            final long y = fs.nextInt();
+            gx.computeIfAbsent(x, val -> new TreeSet<>()).add(y);
+            gy.computeIfAbsent(y, val -> new TreeSet<>()).add(x);
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            final String dir = fs.next();
+            final int c = fs.nextInt();
+            final TreeSet<Long> ts;
+            switch (dir) {
+                case "L":
+                    ts = gy.getOrDefault(sy, new TreeSet<>());
+                    while (true) {
+                        final Long lower = ts.lower(sx);
+                        if (lower != null && lower >= sx - c) {
+                            res++;
+                            ts.remove(lower);
+                            gx.get(lower).remove(sy);
+                        } else {
+                            break;
+                        }
+                    }
+                    sx -= c;
+                    break;
+                case "R":
+                    ts = gy.getOrDefault(sy, new TreeSet<>());
+                    while (true) {
+                        final Long higher = ts.higher(sx);
+                        if (higher != null && higher <= sx + c) {
+                            res++;
+                            ts.remove(higher);
+                            gx.get(higher).remove(sy);
+                        } else {
+                            break;
+                        }
+                    }
+                    sx += c;
+                    break;
+                case "U":
+                    ts = gx.getOrDefault(sx, new TreeSet<>());
+                    while (true) {
+                        final Long higher = ts.higher(sy);
+                        if (higher != null && higher <= sy + c) {
+                            res++;
+                            ts.remove(higher);
+                            gy.get(higher).remove(sx);
+                        } else {
+                            break;
+                        }
+                    }
+                    sy += c;
+                    break;
+                case "D":
+                    ts = gx.getOrDefault(sx, new TreeSet<>());
+                    while (true) {
+                        final Long lower = ts.lower(sy);
+                        if (lower != null && lower >= sy - c) {
+                            res++;
+                            ts.remove(lower);
+                            gy.get(lower).remove(sx);
+                        } else {
+                            break;
+                        }
+                    }
+                    sy -= c;
+                    break;
+            }
+        }
+        System.out.printf("%d %d %d\n", sx, sy, res);
+    }
+
+    static final class Utils {
+        private static class Shuffler {
+            private static void shuffle(int[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void shuffle(long[] x) {
+                final Random r = new Random();
+
+                for (int i = 0; i <= x.length - 2; i++) {
+                    final int j = i + r.nextInt(x.length - i);
+                    swap(x, i, j);
+                }
+            }
+
+            private static void swap(int[] x, int i, int j) {
+                final int t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+
+            private static void swap(long[] x, int i, int j) {
+                final long t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+
+        public static void shuffleSort(int[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        public static void shuffleSort(long[] arr) {
+            Shuffler.shuffle(arr);
+            Arrays.sort(arr);
+        }
+
+        private Utils() {}
+    }
+
+    static class FastReader {
+        private static final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        FastReader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        FastReader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            final byte[] buf = new byte[1024]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    break;
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int readSign() throws IOException {
+            byte c = read();
+            while ('+' != c && '-' != c) {
+                c = read();
+            }
+            return '+' == c ? 0 : 1;
+        }
+
+        private static boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+
+        private int skip() throws IOException {
+            int b;
+            //noinspection StatementWithEmptyBody
+            while ((b = read()) != -1 && isSpaceChar(b)) {}
+            return b;
+        }
+
+        public char nc() throws IOException {
+            return (char) skip();
+        }
+
+        public String next() throws IOException {
+            int b = skip();
+            final StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b)) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = read();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public int[] nextIntArray(int n) throws IOException {
+            final int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = nextInt();
+            }
+            return res;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        public long[] nextLongArray(int n) throws IOException {
+            final long[] res = new long[n];
+            for (int i = 0; i < n; i++) {
+                res[i] = nextLong();
+            }
+            return res;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') { c = read(); }
+            final boolean neg = c == '-';
+            if (neg) { c = read(); }
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) { return -ret; }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) { buffer[0] = -1; }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) { fillBuffer(); }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
+        }
+    }
+}
