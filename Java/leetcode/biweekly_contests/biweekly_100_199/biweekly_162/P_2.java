1d8f11c9b11a3714ab92264033fa8211eabcb412
nirvanarsc <nirvana_rsc@yahoo.com>
2025-08-03 22:53:57 +0200

    Running on empty - food review! part 1094

--- Java/leetcode/biweekly_contests/biweekly_100_199/biweekly_162/P_2.java
+++ Java/leetcode/biweekly_contests/biweekly_100_199/biweekly_162/P_2.java
@@ -1,4 +1,35 @@
 package leetcode.biweekly_contests.biweekly_100_199.biweekly_162;
 
+import java.util.Arrays;
+
 public class P_2 {
+
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        int res = (int) 1e9;
+        final int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            final int u = lowerBound(nums, (long) k * nums[i]);
+            res = Math.min(res, n - u + i);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            final int u = lowerBound(nums, (long) (nums[i] + k - 1) / k);
+            res = Math.min(res, n - 1 - i + u);
+        }
+        return res;
+    }
+
+    private static int lowerBound(int[] arr, long target) {
+        int lo = 0;
+        int hi = arr.length;
+        while (lo < hi) {
+            final int mid = lo + hi >>> 1;
+            if (arr[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
 }
--- Java/leetcode/biweekly_contests/biweekly_100_199/biweekly_162/P_3.java
+++ Java/leetcode/biweekly_contests/biweekly_100_199/biweekly_162/P_3.java
@@ -1,4 +1,28 @@
 package leetcode.biweekly_contests.biweekly_100_199.biweekly_162;
 
 public class P_3 {
+
+    public int earliestFinishTime(int[] landStartTime,
+                                  int[] landDuration,
+                                  int[] waterStartTime,
+                                  int[] waterDuration) {
+        int res = (int) 2e9;
+        final int n = landStartTime.length;
+        final int m = waterStartTime.length;
+        int minEnd = (int) 2e9;
+        for (int i = 0; i < n; i++) {
+            minEnd = Math.min(minEnd, landStartTime[i] + landDuration[i]);
+        }
+        for (int i = 0; i < m; i++) {
+            res = Math.min(res, waterDuration[i] + Math.max(minEnd, waterStartTime[i]));
+        }
+        minEnd = (int) 2e9;
+        for (int i = 0; i < m; i++) {
+            minEnd = Math.min(minEnd, waterStartTime[i] + waterDuration[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            res = Math.min(res, landDuration[i] + Math.max(minEnd, landStartTime[i]));
+        }
+        return res;
+    }
 }
